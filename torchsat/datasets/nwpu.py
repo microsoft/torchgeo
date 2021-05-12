34e524c3cf091ea3b309db44b0b554ac344cba0f
Adam J. Stewart <ajstewart426@gmail.com>
2021-05-11 21:33:57 -0500

    Add annotations file

--- torchsat/datasets/nwpu.py
+++ torchsat/datasets/nwpu.py
@@ -1,7 +1,11 @@
 import os
+from typing import Any, Callable, Optional
 
-import torch
-from torchvision.datasets.utils import download_file_from_google_drive, check_integrity
+from torchvision.datasets.utils import (
+    download_and_extract_archive,
+    download_file_from_google_drive,
+    check_integrity,
+)
 from torchvision.datasets.vision import VisionDataset
 
 
@@ -9,17 +13,29 @@ class VHR10(VisionDataset):
     """`NWPU VHR-10 <https://doi.org/10.1016/j.isprsjprs.2014.10.002>`_ Dataset."""
 
     base_folder = "vhr10"
-    meta = {
+    image_meta = {
         "file_id": "1--foZ3dV5OCsqXQXT84UeKtrAqc5CkAE",
         "filename": "NWPU VHR-10 dataset.rar",
         "md5": "d30a7ff99d92123ebb0b3a14d9102081",
     }
+    target_meta = {
+        "url": (
+            "https://raw.githubusercontent.com/chaozhong2010/VHR-10_dataset_coco/"
+            "master/NWPU%20VHR-10_dataset_coco/annotations.json"
+        ),
+        "filename": "annotations.json",
+        "md5": "7c76ec50c17a61bb0514050d20f22c08",
+    }
 
-    def __init__(self, root: str, download: bool = False) -> None:
+    def __init__(
+        self, root: str, transforms: Optional[Callable[[Any], Any]] = None, download: bool = False
+    ) -> None:
         """Initialize a new VHR-10 dataset instance.
 
         Parameters:
             root: root directory where dataset can be found
+            transforms: a function/transform that takes input sample and its target as
+                entry and returns a transformed version
             download: if True, download dataset and store it in the root directory
         """
         super().__init__(root)
@@ -37,30 +53,48 @@ class VHR10(VisionDataset):
         """Check integrity of dataset.
 
         Returns:
-            True if dataset MD5 matches, else False
+            True if dataset MD5s match, else False
         """
-        return check_integrity(
-            os.path.join(self.root, self.base_folder, self.meta["filename"])
+        image: bool = check_integrity(
+            os.path.join(self.root, self.base_folder, self.image_meta["filename"]),
+            self.image_meta["md5"],
         )
+        target: bool = check_integrity(
+            os.path.join(
+                self.root,
+                self.base_folder,
+                "NWPU VHR-10 dataset",
+                self.target_meta["filename"],
+            ),
+            self.target_meta["md5"],
+        )
+        return image and target
 
     def download(self) -> None:
-        """Download the dataset from Google Drive and extract it."""
+        """Download the dataset and extract it."""
 
         if self._check_integrity():
             print("Files already downloaded and verified")
             return
 
         download_file_from_google_drive(
-            self.meta["file_id"],
+            self.image_meta["file_id"],
             os.path.join(self.root, self.base_folder),
-            self.meta["filename"],
-            self.meta["md5"],
+            self.image_meta["filename"],
+            self.image_meta["md5"],
         )
 
         # Must be installed to extract RAR file
         import rarfile
 
         with rarfile.RarFile(
-            os.path.join(self.root, self.base_folder, self.meta["filename"])
+            os.path.join(self.root, self.base_folder, self.image_meta["filename"])
         ) as f:
             f.extractall(os.path.join(self.root, self.base_folder))
+
+        download_and_extract_archive(
+            self.target_meta["url"],
+            os.path.join(self.root, self.base_folder, "NWPU VHR-10 dataset"),
+            self.target_meta["filename"],
+            self.target_meta["md5"],
+        )
