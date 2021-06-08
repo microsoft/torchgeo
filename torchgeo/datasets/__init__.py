3582ca9fc0a5415de2df6e367806934846ba59fc
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-08 22:47:36 +0000

    Add COWC Dataset

--- torchgeo/datasets/__init__.py
+++ torchgeo/datasets/__init__.py
@@ -1,5 +1,6 @@
+from .cowc import COWCDetection
 from .cv4a_kenya_crop_type import CV4AKenyaCropType
 from .landcoverai import LandCoverAI
 from .nwpu import VHR10
 
-__all__ = ("CV4AKenyaCropType", "LandCoverAI", "VHR10")
+__all__ = ("COWCDetection", "CV4AKenyaCropType", "LandCoverAI", "VHR10")
