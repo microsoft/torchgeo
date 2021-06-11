c6a518bbb98e3fc01a2d01c6a76ad690e9f1ecc4
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-11 19:41:12 +0000

    Add VisionDataset base class

--- torchgeo/datasets/geo.py
+++ torchgeo/datasets/geo.py
@@ -53,3 +53,42 @@ class GeoDataset(Dataset[Dict[str, Any]], abc.ABC):
 {self.__class__.__name__} Dataset
     type: GeoDataset
     size: {len(self)}"""
+
+
+class VisionDataset(Dataset[Dict[str, Any]], abc.ABC):
+    """Abstract base class for datasets lacking geospatial information.
+
+    This base class is designed for datasets with pre-defined image chips.
+    """
+
+    @abc.abstractmethod
+    def __getitem__(self, index: int) -> Dict[str, Any]:
+        """Return an index within the dataset.
+
+        Parameters:
+            index: index to return
+
+        Returns:
+            data and labels at that index
+        """
+        pass
+
+    @abc.abstractmethod
+    def __len__(self) -> int:
+        """Return the length of the dataset.
+
+        Returns:
+            length of the dataset
+        """
+        pass
+
+    def __str__(self) -> str:
+        """Return the informal string representation of the object.
+
+        Returns:
+            informal string representation
+        """
+        return f"""\
+{self.__class__.__name__} Dataset
+    type: VisionDataset
+    size: {len(self)}"""
