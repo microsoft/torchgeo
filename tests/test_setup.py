f91ef032d359b70047255836298b8b1c75cd033d
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-18 17:07:22 +0000

    Fix PYTHONPATH

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,10 +1,12 @@
+import distutils.sysconfig
 import os
 import pathlib
 import subprocess
 
 
 def test_install(tmp_path: pathlib.Path) -> None:
-    os.environ['PYTHONPATH'] = str(tmp_path)
+    site_packages_dir = distutils.sysconfig.get_python_lib(prefix=str(tmp_path))
+    os.environ["PYTHONPATH"] = site_packages_dir
     subprocess.run(
         ["python3", "setup.py", "install", "--prefix", str(tmp_path)], check=True
     )
