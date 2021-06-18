eccd6ab5b4ff25548127769d5d3b192c3e630494
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-18 18:11:22 +0000

    Try specifying same Python command

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,12 +1,13 @@
-# import distutils.sysconfig
+import distutils.sysconfig
 import os
 import pathlib
 import subprocess
+import sys
 
 
 def test_install(tmp_path: pathlib.Path) -> None:
-    # site_packages_dir = distutils.sysconfig.get_python_lib(prefix=str(tmp_path))
-    # os.environ["PYTHONPATH"] = site_packages_dir
+    site_packages_dir = distutils.sysconfig.get_python_lib(prefix=str(tmp_path))
+    os.environ["PYTHONPATH"] = site_packages_dir
     subprocess.run(
-        ["python3", "setup.py", "install", "--prefix", str(tmp_path)], check=True
+        [sys.executable, "setup.py", "install", "--prefix", str(tmp_path)], check=True
     )
