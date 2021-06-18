422f3b8d903014a5f05e9daf0add65e0489ea80a
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-18 16:17:16 +0000

    Fix tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,8 +1,8 @@
 import pathlib
 import subprocess
 
-import pytest
-
 
 def test_install(tmp_path: pathlib.Path) -> None:
-    subprocess.run(["python3", "setup.py", "install", "--prefix", tmp_path], check=True)
+    subprocess.run(
+        ["python3", "setup.py", "install", "--prefix", str(tmp_path)], check=True
+    )
