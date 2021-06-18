f995952c936f41ada7b4891e7dba475008e8567d
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-18 16:33:21 +0000

    Fix tests on Windows

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,8 +1,10 @@
+import os
 import pathlib
 import subprocess
 
 
 def test_install(tmp_path: pathlib.Path) -> None:
+    os.environ['PYTHONPATH'] = str(tmp_path)
     subprocess.run(
         ["python3", "setup.py", "install", "--prefix", str(tmp_path)], check=True
     )
