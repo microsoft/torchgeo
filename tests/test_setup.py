80bd9c77d657d7bb36086151c30ef095de791826
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-21 22:19:32 +0000

    Debug failing windows tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -11,7 +11,9 @@ def test_install(
     monkeypatch: Generator[MonkeyPatch, None, None], tmp_path: Path
 ) -> None:
     site_packages_dir = distutils.sysconfig.get_python_lib(prefix=str(tmp_path))
-    monkeypatch.setenv("PYTHONPATH", site_packages_dir, prepend=True)
+    monkeypatch.setenv(  # type: ignore[attr-defined]
+        "PYTHONPATH", site_packages_dir, prepend=True
+    )
     subprocess.run(
         [sys.executable, "setup.py", "install", "--prefix", str(tmp_path)], check=True
     )
