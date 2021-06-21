af8dc4857c70b0921417efd386e2a3d417801250
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-21 21:22:23 +0000

    Clean up environment after test

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,13 +1,17 @@
 import distutils.sysconfig
-import os
 from pathlib import Path
 import subprocess
 import sys
+from typing import Generator
+
+from pytest import MonkeyPatch
 
 
-def test_install(tmp_path: Path) -> None:
+def test_install(
+    monkeypatch: Generator[MonkeyPatch, None, None], tmp_path: Path
+) -> None:
     site_packages_dir = distutils.sysconfig.get_python_lib(prefix=str(tmp_path))
-    os.environ["PYTHONPATH"] = site_packages_dir
+    monkeypatch.setenv("PYTHONPATH", site_packages_dir, prepend=True)
     subprocess.run(
         [sys.executable, "setup.py", "install", "--prefix", str(tmp_path)], check=True
     )
