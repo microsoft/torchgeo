90f8e7fd1b89c11c099d4d5384bfbdfae3c920dc
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-22 15:20:31 +0000

    Add VHR-10 Dataset tests

---
+++ tests/datasets/test_nwpu.py
@@ -0,0 +1,90 @@
+import os
+from pathlib import Path
+import shutil
+from typing import Generator
+
+from _pytest.fixtures import SubRequest
+import pytest
+from pytest import MonkeyPatch
+import torch
+from torch.utils.data import ConcatDataset
+
+from torchgeo.datasets import VHR10
+import torchgeo.datasets.nwpu
+from torchgeo.transforms import Identity
+
+
+pytest.importorskip("rarfile")
+pytest.importorskip("pycocotools")
+
+
+def download_file_from_google_drive(file_id: str, root: str, *args: str) -> None:
+    shutil.copy(file_id, root)
+
+
+def download_url(url: str, root: str, *args: str) -> None:
+    shutil.copy(url, root)
+
+
+class TestVHR10:
+    @pytest.fixture(params=["positive", "negative"])
+    def dataset(
+        self,
+        monkeypatch: Generator[MonkeyPatch, None, None],
+        tmp_path: Path,
+        request: SubRequest,
+    ) -> VHR10:
+        monkeypatch.setattr(  # type: ignore[attr-defined]
+            torchgeo.datasets.nwpu,
+            "download_file_from_google_drive",
+            download_file_from_google_drive,
+        )
+        monkeypatch.setattr(  # type: ignore[attr-defined]
+            torchgeo.datasets.nwpu, "download_url", download_url
+        )
+        file_id = os.path.join("tests", "data", "vhr10", "NWPU VHR-10 dataset.rar")
+        monkeypatch.setitem(  # type: ignore[attr-defined]
+            VHR10.image_meta, "file_id", file_id
+        )
+        md5 = "e5c38351bd948479fe35a71136aedbc4"
+        monkeypatch.setitem(VHR10.image_meta, "md5", md5)  # type: ignore[attr-defined]
+        url = os.path.join("tests", "data", "vhr10", "annotations.json")
+        monkeypatch.setitem(VHR10.target_meta, "url", url)  # type: ignore[attr-defined]
+        md5 = "16fc6aa597a19179dad84151cc221873"
+        monkeypatch.setitem(VHR10.target_meta, "md5", md5)  # type: ignore[attr-defined]
+        (tmp_path / "vhr10").mkdir()
+        root = str(tmp_path)
+        split = request.param
+        transforms = Identity()
+        return VHR10(root, split, transforms, download=True, checksum=True)
+
+    def test_getitem(self, dataset: VHR10) -> None:
+        x = dataset[0]
+        assert isinstance(x, dict)
+        assert isinstance(x["image"], torch.Tensor)
+        assert isinstance(x["label"], dict)
+
+    def test_len(self, dataset: VHR10) -> None:
+        if dataset.split == "positive":
+            assert len(dataset) == 650
+        elif dataset.split == "negative":
+            assert len(dataset) == 150
+
+    def test_add(self, dataset: VHR10) -> None:
+        ds = dataset + dataset
+        assert isinstance(ds, ConcatDataset)
+        if dataset.split == "positive":
+            assert len(ds) == 1300
+        elif dataset.split == "negative":
+            assert len(ds) == 300
+
+    def test_already_downloaded(self, dataset: VHR10) -> None:
+        VHR10(root=dataset.root, download=True)
+
+    def test_invalid_split(self) -> None:
+        with pytest.raises(AssertionError):
+            VHR10(split="train")
+
+    def test_not_downloaded(self, tmp_path: Path) -> None:
+        with pytest.raises(RuntimeError, match="Dataset not found or corrupted."):
+            VHR10(str(tmp_path))
