5283dde91cd8fb7374288313789ee88a4b24bf89
Caleb Robinson <calebrob6@gmail.com>
2021-06-23 21:02:40 +0000

    Adding the Benin Cashews dataset

--- torchgeo/datasets/__init__.py
+++ torchgeo/datasets/__init__.py
@@ -1,3 +1,4 @@
+from .benin_cashews import BeninSmallHolderCashews
 from .cowc import COWCCounting, COWCDetection
 from .cv4a_kenya_crop_type import CV4AKenyaCropType
 from .geo import GeoDataset, VisionDataset, ZipDataset
@@ -6,6 +7,7 @@ from .nwpu import VHR10
 from .sen12ms import SEN12MS
 
 __all__ = (
+    "BeninSmallHolderCashews",
     "COWCCounting",
     "COWCDetection",
     "CV4AKenyaCropType",
