6cda753ad5a2801e2b070ec01f5e23003f4269cc
Adam J. Stewart <ajstewart426@gmail.com>
2021-07-01 10:12:27 -0500

    Override __module__ during aliasing

--- torchgeo/datasets/__init__.py
+++ torchgeo/datasets/__init__.py
@@ -8,6 +8,21 @@ from .nwpu import VHR10
 from .sen12ms import SEN12MS
 
 
+# https://stackoverflow.com/questions/40018681
+BeninSmallHolderCashews.__module__ = "torchgeo.datasets"
+COWC.__module__ = "torchgeo.datasets"
+COWCCounting.__module__ = "torchgeo.datasets"
+COWCDetection.__module__ = "torchgeo.datasets"
+CV4AKenyaCropType.__module__ = "torchgeo.datasets"
+TropicalCycloneWindEstimation.__module__ = "torchgeo.datasets"
+GeoDataset.__module__ = "torchgeo.datasets"
+VisionDataset.__module__ = "torchgeo.datasets"
+ZipDataset.__module__ = "torchgeo.datasets"
+LandCoverAI.__module__ = "torchgeo.datasets"
+VHR10.__module__ = "torchgeo.datasets"
+SEN12MS.__module__ = "torchgeo.datasets"
+
+
 __all__ = (
     "BeninSmallHolderCashews",
     "COWC",
