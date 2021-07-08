e810d529ad575ac9657667022b11ae6f8b5cced4
Adam J. Stewart <ajstewart426@gmail.com>
2021-07-08 23:14:01 +0000

    Reduce verbosity of __module__ overrides

--- torchgeo/datasets/__init__.py
+++ torchgeo/datasets/__init__.py
@@ -8,21 +8,6 @@ from .nwpu import VHR10
 from .sen12ms import SEN12MS
 from .so2sat import So2Sat
 
-# https://stackoverflow.com/questions/40018681
-BeninSmallHolderCashews.__module__ = "torchgeo.datasets"
-COWC.__module__ = "torchgeo.datasets"
-COWCCounting.__module__ = "torchgeo.datasets"
-COWCDetection.__module__ = "torchgeo.datasets"
-CV4AKenyaCropType.__module__ = "torchgeo.datasets"
-TropicalCycloneWindEstimation.__module__ = "torchgeo.datasets"
-GeoDataset.__module__ = "torchgeo.datasets"
-VisionDataset.__module__ = "torchgeo.datasets"
-ZipDataset.__module__ = "torchgeo.datasets"
-LandCoverAI.__module__ = "torchgeo.datasets"
-VHR10.__module__ = "torchgeo.datasets"
-SEN12MS.__module__ = "torchgeo.datasets"
-So2Sat.__module__ = "torchgeo.datasets"
-
 __all__ = (
     "BeninSmallHolderCashews",
     "COWC",
@@ -38,3 +23,7 @@ __all__ = (
     "VisionDataset",
     "ZipDataset",
 )
+
+# https://stackoverflow.com/questions/40018681
+for module in __all__:
+    globals()[module].__module__ = "torchgeo.datasets"
--- torchgeo/samplers/__init__.py
+++ torchgeo/samplers/__init__.py
@@ -1,9 +1,7 @@
 from .samplers import GeoSampler, GridGeoSampler, RandomGeoSampler
 
-# https://stackoverflow.com/questions/40018681
-GeoSampler.__module__ = "torchgeo.samplers"
-GridGeoSampler.__module__ = "torchgeo.samplers"
-RandomGeoSampler.__module__ = "torchgeo.samplers"
-
-
 __all__ = ("GeoSampler", "GridGeoSampler", "RandomGeoSampler")
+
+# https://stackoverflow.com/questions/40018681
+for module in __all__:
+    globals()[module].__module__ = "torchgeo.samplers"
