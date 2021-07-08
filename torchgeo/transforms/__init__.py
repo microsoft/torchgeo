32416e97123bbb829f285228cee072c5868596fd
Adam J. Stewart <ajstewart426@gmail.com>
2021-07-08 23:14:01 +0000

    Reduce verbosity of __module__ overrides

--- torchgeo/transforms/__init__.py
+++ torchgeo/transforms/__init__.py
@@ -1,9 +1,7 @@
 from .transforms import Identity, RandomHorizontalFlip, RandomVerticalFlip
 
-# https://stackoverflow.com/questions/40018681
-Identity.__module__ = "torchgeo.transforms"
-RandomHorizontalFlip.__module__ = "torchgeo.transforms"
-RandomVerticalFlip.__module__ = "torchgeo.transforms"
-
-
 __all__ = ("Identity", "RandomHorizontalFlip", "RandomVerticalFlip")
+
+# https://stackoverflow.com/questions/40018681
+for module in __all__:
+    globals()[module].__module__ = "torchgeo.transforms"
