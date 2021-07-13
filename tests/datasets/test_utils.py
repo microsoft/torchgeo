18c6ddebbd130c25ab4b9a844242722b5dd858c5
Adam J. Stewart <ajstewart426@gmail.com>
2021-07-13 20:45:21 +0000

    Add tests for collate_dict

--- tests/datasets/test_utils.py
+++ tests/datasets/test_utils.py
@@ -1,6 +1,39 @@
 from pathlib import Path
 
+import torch
+
+from torchgeo.datasets import BoundingBox, collate_dict
 from torchgeo.datasets.utils import working_dir
+
+
+def test_bounding_box() -> None:
+    bbox = BoundingBox(0, 1, 2, 3, 4, 5)
+    assert bbox.minx == 0
+    assert bbox.maxx == 1
+    assert bbox.miny == 2
+    assert bbox.maxy == 3
+    assert bbox.mint == 4
+    assert bbox.maxt == 5
+
+
+def test_collate_dict() -> None:
+    samples = [
+        {
+            "foo": torch.tensor(1),  # type: ignore[attr-defined]
+            "bar": torch.tensor(2),  # type: ignore[attr-defined]
+        },
+        {
+            "foo": torch.tensor(3),  # type: ignore[attr-defined]
+            "bar": torch.tensor(4),  # type: ignore[attr-defined]
+        },
+    ]
+    sample = collate_dict(samples)
+    assert torch.allclose(  # type: ignore[attr-defined]
+        sample["foo"], torch.tensor([1, 3])  # type: ignore[attr-defined]
+    )
+    assert torch.allclose(  # type: ignore[attr-defined]
+        sample["bar"], torch.tensor([2, 4])  # type: ignore[attr-defined]
+    )
 
 
 def test_existing_directory(tmp_path: Path) -> None:
