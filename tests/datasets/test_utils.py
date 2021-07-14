fbc3e9e0db65a788bb6f7a2b6205f1426233317b
Adam J. Stewart <ajstewart426@gmail.com>
2021-07-14 17:48:49 +0000

    Greatly expand invalid BoundingBox checking

--- tests/datasets/test_utils.py
+++ tests/datasets/test_utils.py
@@ -1,3 +1,4 @@
+from typing import Tuple, Union
 from pathlib import Path
 
 import pytest
@@ -8,8 +9,9 @@ from torchgeo.datasets.utils import working_dir
 
 
 class TestBoundingBox:
-    def test_bounding_box(self) -> None:
+    def test_new_init(self) -> None:
         bbox = BoundingBox(0, 1, 2, 3, 4, 5)
+
         assert bbox.minx == 0
         assert bbox.maxx == 1
         assert bbox.miny == 2
@@ -17,19 +19,64 @@ class TestBoundingBox:
         assert bbox.mint == 4
         assert bbox.maxt == 5
 
-    @pytest.mark.xfail(reason="not yet implemented")
+        assert bbox[0] == 0
+        assert bbox[-1] == 5
+        assert bbox[1:3] == (1, 2)
+
+    def test_repr_str(self) -> None:
+        bbox = BoundingBox(0, 1, 2.0, 3.0, -5, -4)
+        expected = "BoundingBox(minx=0, maxx=1, miny=2.0, maxy=3.0, mint=-5, maxt=-4)"
+        assert repr(bbox) == expected
+        assert str(bbox) == expected
+
+    @pytest.mark.parametrize(
+        "test_input,expected",
+        [
+            # bbox1 strictly within bbox2
+            ((-1, 2, -1, 2, -1, 2), True),
+            # bbox2 strictly within bbox1
+            ((0.25, 0.75, 0.25, 0.75, 0.25, 0.75), True),
+            # One corner of bbox1 within bbox2
+            ((0.5, 1.5, 0.5, 1.5, 0.5, 1.5), True),
+            ((0.5, 1.5, -0.5, 0.5, 0.5, 1.5), True),
+            ((0.5, 1.5, 0.5, 1.5, -0.5, 0.5), True),
+            ((0.5, 1.5, -0.5, 0.5, -0.5, 0.5), True),
+            ((-0.5, 0.5, 0.5, 1.5, 0.5, 1.5), True),
+            ((-0.5, 0.5, -0.5, 0.5, 0.5, 1.5), True),
+            ((-0.5, 0.5, 0.5, 1.5, -0.5, 0.5), True),
+            ((-0.5, 0.5, -0.5, 0.5, -0.5, 0.5), True),
+            # No overlap
+            ((0.5, 1.5, 0.5, 1.5, 2, 3), False),
+            ((0.5, 1.5, 2, 3, 0.5, 1.5), False),
+            ((2, 3, 0.5, 1.5, 0.5, 1.5), False),
+            ((2, 3, 2, 3, 2, 3), False),
+        ],
+    )
+    def test_intersects(
+        self,
+        test_input: Tuple[float, float, float, float, float, float],
+        expected: bool,
+    ) -> None:
+        bbox1 = BoundingBox(0, 1, 0, 1, 0, 1)
+        bbox2 = BoundingBox(*test_input)
+        assert bbox1.intersects(bbox2) == bbox2.intersects(bbox1) == expected
+
     def test_invalid_x(self) -> None:
-        with pytest.raises(ValueError):
+        with pytest.raises(
+            ValueError, match="Bounding box is invalid: 'minx=1' > 'maxx=0'"
+        ):
             BoundingBox(1, 0, 2, 3, 4, 5)
 
-    @pytest.mark.xfail(reason="not yet implemented")
     def test_invalid_y(self) -> None:
-        with pytest.raises(ValueError):
+        with pytest.raises(
+            ValueError, match="Bounding box is invalid: 'miny=3' > 'maxy=2'"
+        ):
             BoundingBox(0, 1, 3, 2, 4, 5)
 
-    @pytest.mark.xfail(reason="not yet implemented")
     def test_invalid_t(self) -> None:
-        with pytest.raises(ValueError):
+        with pytest.raises(
+            ValueError, match="Bounding box is invalid: 'mint=5' > 'maxt=4'"
+        ):
             BoundingBox(0, 1, 2, 3, 5, 4)
 
 
