c36fcfbd449b4624a9b950e0e5f46e0051f98b5c
Adam J. Stewart <ajstewart426@gmail.com>
2021-07-21 01:14:38 +0000

    Add test for SEN12MS check integrity

--- tests/datasets/test_sen12ms.py
+++ tests/datasets/test_sen12ms.py
@@ -64,7 +64,10 @@ class TestSEN12MS:
 
     def test_not_downloaded(self, tmp_path: Path) -> None:
         with pytest.raises(RuntimeError, match="Dataset not found or corrupted."):
-            SEN12MS(str(tmp_path))
+            SEN12MS(str(tmp_path), checksum=True)
+
+        with pytest.raises(RuntimeError, match="Dataset not found or corrupted."):
+            SEN12MS(str(tmp_path), checksum=False)
 
     def test_check_integrity_light(self) -> None:
         root = os.path.join("tests", "data")
