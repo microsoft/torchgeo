cd6c3c5bd35de683287daed07ee3fc8c12aef2ac
Caleb Robinson <calebrob6@gmail.com>
2021-07-27 17:25:06 +0000

    Removing OrderedDict from model definitions

--- torchgeo/models/fcn.py
+++ torchgeo/models/fcn.py
@@ -1,7 +1,5 @@
 """Simple fully convolutional neural network (FCN) implementations."""
 
-from typing import OrderedDict
-
 import torch.nn as nn
 from torch import Tensor
 
@@ -35,22 +33,18 @@ class FCN(nn.modules.Module):
             num_filters, num_filters, kernel_size=3, stride=1, padding=1
         )
 
-        self.layers = OrderedDict(
-            [
-                ("conv1", conv1),
-                ("relu1", nn.modules.LeakyReLU(inplace=True)),
-                ("conv2", conv2),
-                ("relu2", nn.modules.LeakyReLU(inplace=True)),
-                ("conv3", conv3),
-                ("relu3", nn.modules.LeakyReLU(inplace=True)),
-                ("conv4", conv4),
-                ("relu4", nn.modules.LeakyReLU(inplace=True)),
-                ("conv5", conv5),
-                ("relu5", nn.modules.LeakyReLU(inplace=True)),
-            ]
+        self.backbone = nn.modules.Sequential(
+            conv1,
+            nn.modules.LeakyReLU(inplace=True),
+            conv2,
+            nn.modules.LeakyReLU(inplace=True),
+            conv3,
+            nn.modules.LeakyReLU(inplace=True),
+            conv4,
+            nn.modules.LeakyReLU(inplace=True),
+            conv5,
+            nn.modules.LeakyReLU(inplace=True),
         )
-
-        self.backbone = nn.modules.Sequential(self.layers)
 
         self.last = nn.modules.Conv2d(
             num_filters, classes, kernel_size=1, stride=1, padding=0
