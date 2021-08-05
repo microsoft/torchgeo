3ce1ebdfce3c272385c6f50bd525808bb3bec791
Adam J. Stewart <ajstewart426@gmail.com>
2021-08-05 21:16:30 +0000

    Refactor GeoDataset

--- torchgeo/datasets/landsat.py
+++ torchgeo/datasets/landsat.py
@@ -1,27 +1,14 @@
 """Landsat datasets."""
 
 import abc
-import glob
-import os
-from datetime import datetime
-from typing import Any, Callable, Dict, Optional, Sequence
+from typing import Any, Callable, Dict, List, Optional, Sequence
 
-import matplotlib.pyplot as plt
-import numpy as np
-import rasterio
-import torch
 from rasterio.crs import CRS
-from rasterio.vrt import WarpedVRT
-from rtree.index import Index, Property
-from torch import Tensor
 
-from .geo import GeoDataset
-from .utils import BoundingBox
-
-_crs = CRS.from_epsg(32616)
+from .geo import RasterDataset
 
 
-class Landsat(GeoDataset, abc.ABC):
+class Landsat(RasterDataset, abc.ABC):
     """Abstract base class for all Landsat datasets.
 
     `Landsat <https://landsat.gsfc.nasa.gov/>`_ is a joint NASA/USGS program,
@@ -32,27 +19,45 @@ class Landsat(GeoDataset, abc.ABC):
     * https://www.usgs.gov/centers/eros/data-citation
     """
 
-    @property
-    @abc.abstractmethod
-    def band_names(self) -> Sequence[str]:
-        """Spectral bands provided by a satellite.
+    # https://www.usgs.gov/faqs/what-naming-convention-landsat-collections-level-1-scenes
+    # https://www.usgs.gov/faqs/what-naming-convention-landsat-collection-2-level-1-and-level-2-scenes
+    filename_glob = ""
+    filename_regex = r"""
+        ^L
+        (?P<sensor>[COTEM])
+        (?P<satellite>\d{2})
+        _(?P<processing_correction_level>[A-Z0-9]{4})
+        _(?P<wrs_path>\d{3})
+        (?P<wrs_row>\d{3})
+        _(?P<date>\d{8})
+        _(?P<processing_date>\d{8})
+        _(?P<collection_number>\d{2})
+        _(?P<collection_category>[A-Z0-9]{2})
+        _SR
+        _(?P<band>[A-Z0-9]{2})
+        \..*$
+    """
 
-        See https://www.usgs.gov/faqs/what-are-band-designations-landsat-satellites
-        for more details.
-        """
+    # https://www.usgs.gov/faqs/what-are-band-designations-landsat-satellites
+    all_bands: List[str] = []
+    rgb_bands: List[str] = []
+
+    separate_files = True
+    stretch = True
 
     def __init__(
         self,
         root: str = "data",
-        crs: CRS = _crs,
+        crs: Optional[CRS] = None,
         bands: Sequence[str] = [],
         transforms: Optional[Callable[[Dict[str, Any]], Dict[str, Any]]] = None,
     ) -> None:
-        """Initialize a new Landsat Dataset.
+        """Initialize a new Dataset instance.
 
         Args:
             root: root directory where dataset can be found
-            crs: :term:`coordinate reference system (CRS)` to project to
+            crs: :term:`coordinate reference system (CRS)` to project to. Uses the CRS
+                of the files by default
             bands: bands to return (defaults to all bands)
             transforms: a function/transform that takes input sample and its target as
                 entry and returns a transformed version
@@ -60,106 +65,104 @@ class Landsat(GeoDataset, abc.ABC):
         Raises:
             FileNotFoundError: if no files are found in ``root``
         """
-        self.root = root
-        self.crs = crs
-        self.bands = bands if bands else self.band_names
-        self.transforms = transforms
+        self.bands = bands if bands else self.all_bands
 
-        # Create an R-tree to index the dataset
-        self.index = Index(interleaved=False, properties=Property(dimension=3))
-        fileglob = os.path.join(root, f"**_{self.bands[0]}.TIF")
-        for i, filename in enumerate(glob.iglob(fileglob, recursive=True)):
-            # https://www.usgs.gov/faqs/what-naming-convention-landsat-collections-level-1-scenes
-            # https://www.usgs.gov/faqs/what-naming-convention-landsat-collection-2-level-1-and-level-2-scenes
-            time = datetime.strptime(os.path.basename(filename).split("_")[3], "%Y%m%d")
-            timestamp = time.timestamp()
-            with rasterio.open(filename) as src:
-                with WarpedVRT(src, crs=self.crs) as vrt:
-                    minx, miny, maxx, maxy = vrt.bounds
-            coords = (minx, maxx, miny, maxy, timestamp, timestamp)
-            self.index.insert(i, coords, filename)
+        super().__init__(root, crs, transforms)
 
-        if "filename" not in locals():
-            raise FileNotFoundError(
-                f"No {self.__class__.__name__} data was found in '{root}'"
-            )
 
-    def __getitem__(self, query: BoundingBox) -> Dict[str, Any]:
-        """Retrieve image and metadata indexed by query.
+class Landsat1(Landsat):
+    """Landsat 1 Multispectral Scanner (MSS)."""
 
-        Args:
-            query: (minx, maxx, miny, maxy, mint, maxt) coordinates to index
+    filename_glob = "LM01_*.*"
 
-        Returns:
-            sample of data and metadata at that index
+    all_bands = [
+        "B4",
+        "B5",
+        "B6",
+        "B7",
+    ]
+    rgb_bands = ["B6", "B5", "B4"]
 
-        Raises:
-            IndexError: if query is not within bounds of the index
-        """
-        if not query.intersects(self.bounds):
-            raise IndexError(
-                f"query: {query} is not within bounds of the index: {self.bounds}"
-            )
 
-        hits = self.index.intersection(query, objects=True)
-        filename = next(hits).object  # TODO: this assumes there is only a single hit
-        data_list = []
-        for band in self.bands:
-            tokens = filename.split("_")
-            tokens[-1] = band + ".TIF"
-            filename = "_".join(tokens)
-            with rasterio.open(filename) as src:
-                with WarpedVRT(src, crs=self.crs) as vrt:
-                    window = rasterio.windows.from_bounds(
-                        query.minx,
-                        query.miny,
-                        query.maxx,
-                        query.maxy,
-                        transform=vrt.transform,
-                    )
-                    image = vrt.read(window=window)
-            data_list.append(image)
-        image = np.concatenate(data_list)  # type: ignore[no-untyped-call]
-        image = image.astype(np.int32)
-        sample = {
-            "image": torch.tensor(image),  # type: ignore[attr-defined]
-            "crs": self.crs,
-            "bbox": query,
-        }
+class Landsat2(Landsat1):
+    """Landsat 2 Multispectral Scanner (MSS)."""
 
-        if self.transforms is not None:
-            sample = self.transforms(sample)
+    filename_glob = "LM02_*.*"
 
-        return sample
 
-    def plot(self, image: Tensor) -> None:
-        """Plot an image on a map.
+class Landsat3(Landsat1):
+    """Landsat 3 Multispectral Scanner (MSS)."""
 
-        Args:
-            image: the image to plot
-        """
-        # Convert from CxHxW to HxWxC
-        image = image.permute((1, 2, 0))
-        array = image.numpy()
+    filename_glob = "LM03_*.*"
 
-        # Stretch to the range of 2nd to 98th percentile
-        per98 = np.percentile(array, 98)  # type: ignore[no-untyped-call]
-        per02 = np.percentile(array, 2)  # type: ignore[no-untyped-call]
-        array = (array - per02) / (per98 - per02)
-        array = np.clip(array, 0, 1)
 
-        # Plot the image
-        ax = plt.axes()
-        ax.imshow(array)
-        ax.axis("off")
-        plt.show()
-        plt.close()
+class Landsat4MSS(Landsat):
+    """Landsat 4 Multispectral Scanner (MSS)."""
+
+    filename_glob = "LM04_*.*"
+
+    all_bands = [
+        "B1",
+        "B2",
+        "B3",
+        "B4",
+    ]
+    rgb_bands = ["B3", "B2", "B1"]
+
+
+class Landsat4TM(Landsat):
+    """Landsat 4 Thematic Mapper (TM)."""
+
+    filename_glob = "LT04_*.*"
+
+    all_bands = [
+        "B1",
+        "B2",
+        "B3",
+        "B4",
+        "B5",
+        "B6",
+        "B7",
+    ]
+    rgb_bands = ["B3", "B2", "B1"]
+
+
+class Landsat5MSS(Landsat4MSS):
+    """Landsat 4 Multispectral Scanner (MSS)."""
+
+    filename_glob = "LM04_*.*"
+
+
+class Landsat5TM(Landsat4TM):
+    """Landsat 5 Thematic Mapper (TM)."""
+
+    filename_glob = "LT05_*.*"
+
+
+class Landsat7(Landsat):
+    """Landsat 7 Enhanced Thematic Mapper Plus (ETM+)."""
+
+    filename_glob = "LE07_*.*"
+
+    all_bands = [
+        "B1",
+        "B2",
+        "B3",
+        "B4",
+        "B5",
+        "B6",
+        "B7",
+        "B8",
+    ]
+    rgb_bands = ["B3", "B2", "B1"]
 
 
 class Landsat8(Landsat):
-    """Landsat 8-9 Operational Land Imager (OLI) and Thermal Infrared Sensor (TIRS)."""
+    """Landsat 8 Operational Land Imager (OLI) and Thermal Infrared Sensor (TIRS)."""
 
-    band_names = [
+    filename_glob = "LC08_*.*"
+
+    all_bands = [
         "B1",
         "B2",
         "B3",
@@ -172,67 +175,10 @@ class Landsat8(Landsat):
         "B10",
         "B11",
     ]
+    rgb_bands = ["B4", "B3", "B2"]
 
 
-Landsat9 = Landsat8
+class Landsat9(Landsat8):
+    """Landsat 9 Operational Land Imager (OLI) and Thermal Infrared Sensor (TIRS)."""
 
-
-class Landsat7(Landsat):
-    """Landsat 7 Enhanced Thematic Mapper Plus (ETM+)."""
-
-    band_names = [
-        "B1",
-        "B2",
-        "B3",
-        "B4",
-        "B5",
-        "B6",
-        "B7",
-        "B8",
-    ]
-
-
-class Landsat4TM(Landsat):
-    """Landsat 4-5 Thematic Mapper (TM)."""
-
-    band_names = [
-        "B1",
-        "B2",
-        "B3",
-        "B4",
-        "B5",
-        "B6",
-        "B7",
-    ]
-
-
-Landsat5TM = Landsat4TM
-
-
-class Landsat4MSS(Landsat):
-    """Landsat 4-5 Multispectral Scanner (MSS)."""
-
-    band_names = [
-        "B1",
-        "B2",
-        "B3",
-        "B4",
-    ]
-
-
-Landsat5MSS = Landsat4MSS
-
-
-class Landsat1(Landsat):
-    """Landsat 1-3 Multispectral Scanner (MSS)."""
-
-    band_names = [
-        "B4",
-        "B5",
-        "B6",
-        "B7",
-    ]
-
-
-Landsat2 = Landsat1
-Landsat3 = Landsat1
+    filename_glob = "LC09_*.*"
