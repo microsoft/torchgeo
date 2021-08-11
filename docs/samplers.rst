c9a1446a8bbb7da5bce24a8b01928278e8118b2f
Adam J. Stewart <ajstewart426@gmail.com>
2021-08-11 21:32:23 +0000

    More intelligent sampling

--- docs/samplers.rst
+++ docs/samplers.rst
@@ -1,4 +1,68 @@
 torchgeo.samplers
 =================
 
-.. automodule:: torchgeo.samplers
+.. module:: torchgeo.samplers
+
+Samplers
+--------
+
+Samplers are used to index a dataset, retrieving a single query at a time. For :class:`~torchgeo.datasets.VisionDataset`, dataset objects can be indexed with integers, and PyTorch's builtin samplers are sufficient. For :class:`~torchgeo.datasets.GeoDataset`, dataset objects require a bounding box for indexing. For this reason, we define our own :class:`GeoSampler` implementations below. These can be used like so:
+
+.. code-block:: python
+
+   from torch.utils.data import DataLoader
+
+   from torchgeo.datasets import Landsat
+   from torchgeo.samplers import RandomGeoSampler
+
+   dataset = Landsat(...)
+   sampler = RandomGeoSampler(dataset.index, size=1000, length=100)
+   dataloader = DataLoader(dataset, sampler=sampler)
+
+
+Random Geo Sampler
+^^^^^^^^^^^^^^^^^^
+
+.. autoclass:: RandomGeoSampler
+
+Grid Geo Sampler
+^^^^^^^^^^^^^^^^
+
+.. autoclass:: GridGeoSampler
+
+Batch Samplers
+--------------
+
+When working with large tile-based datasets, randomly sampling patches from each tile can be extremely time consuming. It's much more efficient to choose a tile, load it, warp it to the appropriate :term:`coordinate reference system (CRS)` and resolution, and then sample random patches from that tile to construct a mini-batch of data. For this reason, we define our own :class:`BatchGeoSampler` implementations below. These can be used like so:
+
+.. code-block:: python
+
+   from torch.utils.data import DataLoader
+
+   from torchgeo.datasets import Landsat
+   from torchgeo.samplers import RandomBatchGeoSampler
+
+   dataset = Landsat(...)
+   sampler = RandomBatchGeoSampler(dataset.index, size=1000, batch_size=10, length=100)
+   dataloader = DataLoader(dataset, batch_sampler=sampler)
+
+
+Random Batch Geo Sampler
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. autoclass:: RandomBatchGeoSampler
+
+Sampler Base Classes
+--------------------
+
+If you want to write your own custom sampler, you can extend one of these abstract base classes.
+
+Geo Sampler
+^^^^^^^^^^^
+
+.. autoclass:: GeoSampler
+
+Batch Geo Sampler
+^^^^^^^^^^^^^^^^^
+
+.. autoclass:: BatchGeoSampler
--- torchgeo/samplers/single.py
+++ torchgeo/samplers/single.py
@@ -2,33 +2,21 @@
 
 import abc
 import random
-from typing import Any, Iterator, Tuple, Union
+from typing import Iterator, Optional, Tuple, Union
 
+from rtree.index import Index
 from torch.utils.data import Sampler
 
 from torchgeo.datasets import BoundingBox
+
+from .utils import _to_tuple
 
 # https://github.com/pytorch/pytorch/issues/60979
 # https://github.com/pytorch/pytorch/pull/61045
 Sampler.__module__ = "torch.utils.data"
 
 
-def _to_tuple(value: Union[Tuple[Any, Any], Any]) -> Tuple[Any, Any]:
-    """Convert value to a tuple if it is not already a tuple.
-
-    Args:
-        value: input value
-
-    Returns:
-        value if value is a tuple, else (value, value)
-    """
-    if isinstance(value, (float, int)):
-        return (value, value)
-    else:
-        return value
-
-
-class GeoSampler(Sampler[Tuple[Any, ...]], abc.ABC):
+class GeoSampler(Sampler[BoundingBox], abc.ABC):
     """Abstract base class for sampling from :class:`~torchgeo.datasets.GeoDataset`.
 
     Unlike PyTorch's :class:`~torch.utils.data.Sampler`, :class:`GeoSampler`
@@ -45,26 +33,25 @@ class GeoSampler(Sampler[Tuple[Any, ...]], abc.ABC):
             (minx, maxx, miny, maxy, mint, maxt) coordinates to index a dataset
         """
 
-    @abc.abstractmethod
-    def __len__(self) -> int:
-        """Return the number of samples in a single epoch.
-
-        Returns:
-            length of the epoch
-        """
-
 
 class RandomGeoSampler(GeoSampler):
     """Samples elements from a region of interest randomly.
 
     This is particularly useful during training when you want to maximize the size of
     the dataset and return as many random :term:`chips <chip>` as possible.
+
+    This sampler is not recommended for use with tile-based datasets. Use
+    :class:`RandomBatchGeoSampler` instead.
     """
 
     def __init__(
-        self, roi: BoundingBox, size: Union[Tuple[float, float], float], length: int
+        self,
+        index: Index,
+        size: Union[Tuple[float, float], float],
+        length: int,
+        roi: Optional[BoundingBox] = None,
     ) -> None:
-        """Initialize a new RandomGeoSampler.
+        """Initialize a new Sampler instance.
 
         The ``size`` argument can either be:
 
@@ -74,13 +61,19 @@ class RandomGeoSampler(GeoSampler):
           height dimension, and the second *float* for the width dimension
 
         Args:
-            roi: region of interest to sample from (minx, maxx, miny, maxy, mint, maxt)
+            index: index of a :class:`~torchgeo.datasets.GeoDataset`
             size: dimensions of each :term:`patch` in units of CRS
             length: number of random samples to draw per epoch
+            roi: region of interest to sample from (minx, maxx, miny, maxy, mint, maxt)
+                (defaults to the bounds of ``index``)
         """
-        self.roi = roi
+        self.index = index
         self.size = _to_tuple(size)
         self.length = length
+        if roi is None:
+            roi = BoundingBox(*index.bounds)
+        self.roi = roi
+        self.hits = list(index.intersection(roi, objects=True))
 
     def __iter__(self) -> Iterator[BoundingBox]:
         """Return the index of a dataset.
@@ -89,15 +82,19 @@ class RandomGeoSampler(GeoSampler):
             (minx, maxx, miny, maxy, mint, maxt) coordinates to index a dataset
         """
         for _ in range(len(self)):
-            minx = random.uniform(self.roi.minx, self.roi.maxx - self.size[1])
+            # Choose a random tile
+            hit = random.choice(self.hits)
+            bounds = BoundingBox(*hit.bounds)
+
+            # Choose a random index within that tile
+            minx = random.uniform(bounds.minx, bounds.maxx - self.size[1])
             maxx = minx + self.size[1]
 
-            miny = random.uniform(self.roi.miny, self.roi.maxy - self.size[0])
+            miny = random.uniform(bounds.miny, bounds.maxy - self.size[0])
             maxy = miny + self.size[0]
 
-            # TODO: figure out how to handle time
-            mint = self.roi.mint
-            maxt = self.roi.maxt
+            mint = bounds.mint
+            maxt = bounds.maxt
 
             yield BoundingBox(minx, maxx, miny, maxy, mint, maxt)
 
@@ -123,15 +120,19 @@ class GridGeoSampler(GeoSampler):
     The overlap between each chip (``chip_size - stride``) should be approximately equal
     to the `receptive field <https://distill.pub/2019/computing-receptive-fields/>`_ of
     the CNN.
+
+    When sampling from :class:`~torchgeo.datasets.ZipDataset`, the ``index`` should come
+    from a non-tile-based dataset if possible.
     """
 
     def __init__(
         self,
-        roi: BoundingBox,
+        index: Index,
         size: Union[Tuple[float, float], float],
         stride: Union[Tuple[float, float], float],
+        roi: Optional[BoundingBox] = None,
     ) -> None:
-        """Initialize a new RandomGeoSampler.
+        """Initialize a new Sampler instance.
 
         The ``size`` and ``stride`` arguments can either be:
 
@@ -141,15 +142,18 @@ class GridGeoSampler(GeoSampler):
           height dimension, and the second *float* for the width dimension
 
         Args:
+            index: index of a :class:`~torchgeo.datasets.GeoDataset`
             roi: region of interest to sample from (minx, maxx, miny, maxy, mint, maxt)
             size: dimensions of each :term:`patch` in units of CRS
             stride: distance to skip between each patch
         """
-        self.roi = roi
+        self.index = index
         self.size = _to_tuple(size)
         self.stride = _to_tuple(stride)
-        self.rows = int((roi.maxy - roi.miny - self.size[0]) // self.stride[0]) + 1
-        self.cols = int((roi.maxx - roi.minx - self.size[1]) // self.stride[1]) + 1
+        if roi is None:
+            roi = BoundingBox(*index.bounds)
+        self.roi = roi
+        self.hits = index.intersection(roi, objects=True)
 
     def __iter__(self) -> Iterator[BoundingBox]:
         """Return the index of a dataset.
@@ -157,23 +161,24 @@ class GridGeoSampler(GeoSampler):
         Returns:
             (minx, maxx, miny, maxy, mint, maxt) coordinates to index a dataset
         """
-        for i in range(self.rows):
-            miny = self.roi.miny + i * self.stride[0]
-            maxy = miny + self.size[0]
-            for j in range(self.cols):
-                minx = self.roi.minx + j * self.stride[1]
-                maxx = minx + self.size[1]
+        # For each tile...
+        for hit in self.hits:
+            bounds = BoundingBox(*hit.bounds)
 
-                # TODO: figure out how to handle time
-                mint = self.roi.mint
-                maxt = self.roi.maxt
+            rows = int((bounds.maxy - bounds.miny - self.size[0]) // self.stride[0]) + 1
+            cols = int((bounds.maxx - bounds.minx - self.size[1]) // self.stride[1]) + 1
 
-                yield BoundingBox(minx, maxx, miny, maxy, mint, maxt)
+            mint = bounds.mint
+            maxt = bounds.maxt
 
-    def __len__(self) -> int:
-        """Return the number of samples in a single epoch.
+            # For each row...
+            for i in range(rows):
+                miny = bounds.miny + i * self.stride[0]
+                maxy = miny + self.size[0]
 
-        Returns:
-            length of the epoch
-        """
-        return self.rows * self.cols
+                # For each column...
+                for j in range(cols):
+                    minx = bounds.minx + j * self.stride[1]
+                    maxx = minx + self.size[1]
+
+                    yield BoundingBox(minx, maxx, miny, maxy, mint, maxt)
