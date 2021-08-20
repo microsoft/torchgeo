84e4b5462900cfc7188d06e30d28e0e76dfefda1
Adam J. Stewart <ajstewart426@gmail.com>
2021-08-20 16:38:37 -0500

    Skip slow tests by default

--- tests/test_train.py
+++ tests/test_train.py
@@ -7,6 +7,9 @@ from pathlib import Path
 import pytest
 
 
+pytestmark = pytest.mark.slow
+
+
 def test_required_args() -> None:
     args = [sys.executable, "train.py"]
     ps = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
