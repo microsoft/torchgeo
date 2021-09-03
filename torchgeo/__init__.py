f44c757c6716cc19289905063328ea0e9109f94c
Adam J. Stewart <ajstewart426@gmail.com>
2021-09-03 14:38:03 -0500

    Fix tests

--- torchgeo/__init__.py
+++ torchgeo/__init__.py
@@ -1,8 +1,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-"""This library is part of the `PyTorch <http://pytorch.org/>`_ project. PyTorch is an
-open source machine learning framework.
+"""TorchGeo: datasets, transforms, and models for geospatial data.
+
+This library is part of the `PyTorch <http://pytorch.org/>`_ project. PyTorch is an open
+source machine learning framework.
 
 The :mod:`torchgeo` package consists of popular datasets, model architectures, and
 common image transformations for geospatial data.
