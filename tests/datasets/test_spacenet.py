2cf68c09fef7f6f1a618778150d2e268aa1e58e8
Ashwin Nair <ash1995@gmail.com>
2021-09-21 14:12:29 +0400

    Add tests and test data for SpaceNet2

--- tests/datasets/test_spacenet.py
+++ tests/datasets/test_spacenet.py
@@ -12,24 +12,24 @@ import torch
 from _pytest.fixtures import SubRequest
 from _pytest.monkeypatch import MonkeyPatch
 
-from torchgeo.datasets import SpaceNet1
+from torchgeo.datasets import SpaceNet1, SpaceNet2
 from torchgeo.transforms import Identity
 
 TEST_DATA_DIR = "tests/data/spacenet"
 
 
-class Dataset:
-    def __init__(self, dataset_id: str) -> None:
-        self.dataset_id = dataset_id
+class Collection:
+    def __init__(self, collection_id: str) -> None:
+        self.collection_id = collection_id
 
     def download(self, output_dir: str, **kwargs: str) -> None:
-        glob_path = os.path.join(TEST_DATA_DIR, self.dataset_id, "*.tar.gz")
+        glob_path = os.path.join(TEST_DATA_DIR, "*.tar.gz")
         for tarball in glob.iglob(glob_path):
             shutil.copy(tarball, output_dir)
 
 
-def fetch_dataset(dataset_id: str, **kwargs: str) -> Dataset:
-    return Dataset(dataset_id)
+def fetch_collection(collection_id: str, **kwargs: str) -> Collection:
+    return Collection(collection_id)
 
 
 class TestSpaceNet1:
@@ -42,10 +42,14 @@ class TestSpaceNet1:
     ) -> SpaceNet1:
         radiant_mlhub = pytest.importorskip("radiant_mlhub", minversion="0.2.1")
         monkeypatch.setattr(  # type: ignore[attr-defined]
-            radiant_mlhub.Dataset, "fetch", fetch_dataset
+            radiant_mlhub.Collection, "fetch", fetch_collection
         )
-        test_md5 = "829652022c2df4511ee4ae05bc290250"
-        monkeypatch.setattr(SpaceNet1, "md5", test_md5)  # type: ignore[attr-defined]
+        test_md5 = {"sn1_AOI_1_RIO": "829652022c2df4511ee4ae05bc290250"}
+
+        # Refer https://github.com/python/mypy/issues/1032
+        monkeypatch.setattr(  # type: ignore[attr-defined]
+            SpaceNet1, "collection_md5_dict", test_md5
+        )
         root = str(tmp_path)
         transforms = Identity()
         return SpaceNet1(
@@ -75,3 +79,59 @@ class TestSpaceNet1:
     def test_not_downloaded(self, tmp_path: Path) -> None:
         with pytest.raises(RuntimeError, match="Dataset not found"):
             SpaceNet1(str(tmp_path))
+
+
+class TestSpaceNet2:
+    @pytest.fixture(params=["PAN", "MS", "PS-MS", "PS-RGB"])
+    def dataset(
+        self,
+        request: SubRequest,
+        monkeypatch: Generator[MonkeyPatch, None, None],
+        tmp_path: Path,
+    ) -> SpaceNet2:
+        radiant_mlhub = pytest.importorskip("radiant_mlhub", minversion="0.2.1")
+        monkeypatch.setattr(  # type: ignore[attr-defined]
+            radiant_mlhub.Collection, "fetch", fetch_collection
+        )
+        test_md5 = {
+            "sn2_AOI_2_Vegas": "48bb4cc0a17de21e36305fbdfa58d4a5",
+            "sn2_AOI_3_Paris": "811e6a26fdeb8be445fed99769fa52c5",
+            "sn2_AOI_4_Shanghai": "139d1627d184c74426a85ad0222f7355",
+            "sn2_AOI_5_Khartoum": "435535120414b74165aa87f051c3a2b3",
+        }
+
+        monkeypatch.setattr(  # type: ignore[attr-defined]
+            SpaceNet2, "collection_md5_dict", test_md5
+        )
+        root = str(tmp_path)
+        transforms = Identity()
+        return SpaceNet2(
+            root,
+            image=request.param,
+            collections=["sn2_AOI_2_Vegas", "sn2_AOI_5_Khartoum"],
+            transforms=transforms,
+            download=True,
+            api_key="",
+        )
+
+    def test_getitem(self, dataset: SpaceNet2) -> None:
+        x = dataset[0]
+        assert isinstance(x, dict)
+        assert isinstance(x["image"], torch.Tensor)
+        assert isinstance(x["mask"], torch.Tensor)
+        if dataset.image == "PS-RGB":
+            assert x["image"].shape[0] == 3
+        elif dataset.image in ["MS", "PS-MS"]:
+            assert x["image"].shape[0] == 8
+        else:
+            assert x["image"].shape[0] == 1
+
+    def test_len(self, dataset: SpaceNet2) -> None:
+        assert len(dataset) == 4
+
+    def test_already_downloaded(self, dataset: SpaceNet2) -> None:
+        SpaceNet2(root=dataset.root, download=True)
+
+    def test_not_downloaded(self, tmp_path: Path) -> None:
+        with pytest.raises(RuntimeError, match="Dataset not found"):
+            SpaceNet2(str(tmp_path))
