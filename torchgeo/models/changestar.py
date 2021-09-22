5d59bd3aa235de7604657a23fc78dd40cdf95b6d
Z-Zheng <zhengzhuo_git@163.com>
2021-09-22 23:26:26 +0800

    fixing mypy issues

--- torchgeo/models/changestar.py
+++ torchgeo/models/changestar.py
@@ -11,11 +11,11 @@ from einops import rearrange
 from torch import Tensor
 from torch.nn.modules import Module
 
+from .farseg import FarSeg
+
 # https://github.com/pytorch/pytorch/issues/60979
 # https://github.com/pytorch/pytorch/pull/61045
 Module.__module__ = "torch.nn"
-
-from .farseg import FarSeg
 
 
 class ChangeMixin(Module):
