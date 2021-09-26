44ea9ae00caea889eec1fd6100fc2f9ed9771491
Ashwin Nair <ash1995@gmail.com>
2021-09-27 01:19:46 +0400

    Fix regex pattern

--- torchgeo/datasets/spacenet.py
+++ torchgeo/datasets/spacenet.py
@@ -470,7 +470,7 @@ class SpaceNet2(SpaceNet):
             images = glob.glob(os.path.join(root, collection, "*", self.filename))
             images = sorted(images)
             for imgpath in images:
-                pat = re.compile("img1")
+                pat = re.compile(f"img1{os.sep}")
                 if collection == "sn2_AOI_2_Vegas" and pat.search(imgpath):
                     lbl_path = os.path.join(
                         os.path.dirname(os.path.dirname(imgpath)),
