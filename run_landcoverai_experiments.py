36883c3593b78527194903f647ef5619c27f0363
Caleb Robinson <calebrob6@gmail.com>
2021-09-27 20:13:26 +0000

    Experiments

---
+++ run_landcoverai_experiments.py
@@ -0,0 +1,85 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""Runs the train script with a grid of hyperparameters."""
+import itertools
+import os
+import subprocess
+from multiprocessing import Process, Queue
+
+# list of GPU IDs that we want to use, one job will be started for every ID in the list
+GPUS = [0, 1, 2, 3, 4, 5, 6, 7]
+TEST_MODE = False  # if False then print out the commands to be run, if True then run
+
+# Hyperparameter options
+model_options = ["unet"]
+encoder_options = ["resnet18", "resnet50"]
+lr_options = [1e-2, 1e-3, 1e-4]
+loss_options = ["ce", "jaccard"]
+weight_init_options = ["null", "imagenet"]
+
+
+def do_work(work: "Queue[str]", gpu_idx: int) -> bool:
+    """Process for each ID in GPUS."""
+    while not work.empty():
+        experiment = work.get()
+        experiment = experiment.replace("GPU", str(gpu_idx))
+        print(experiment)
+        if not TEST_MODE:
+            subprocess.call(experiment.split(" "))
+    return True
+
+
+def main() -> None:
+    """Main."""
+    work: Queue[str] = Queue()
+
+    for (model, encoder, lr, loss, weight_init) in itertools.product(
+        model_options,
+        encoder_options,
+        lr_options,
+        loss_options,
+        weight_init_options
+    ):
+
+        experiment_name = f"{model}_{encoder}_{lr}_{loss}_{weight_init}"
+
+        output_dir = "output/landcoverai_experiments/"
+
+        if not os.path.exists(os.path.join(output_dir, experiment_name)):
+
+            command = (
+                "python train.py"
+                + " config_file=conf/landcoverai.yaml"
+                + f" experiment.name={experiment_name}"
+                + f" experiment.module.segmentation_model={model}"
+                + f" experiment.module.learning_rate={lr}"
+                + f" experiment.module.loss={loss}"
+                + f" experiment.module.encoder_name={encoder}"
+                + f" experiment.module.encoder_weights={weight_init}"
+                + f" program.output_dir={output_dir}"
+                + f" program.log_dir={output_dir}/logs"
+                + " program.data_dir=/home/calebrobinson/ssdprivate/data/landcoverai/"
+                + " trainer.gpus=[GPU]"
+            )
+            command = command.strip()
+
+            work.put(command)
+
+    processes = []
+    for gpu_idx in GPUS:
+        p = Process(
+            target=do_work,
+            args=(
+                work,
+                gpu_idx,
+            ),
+        )
+        processes.append(p)
+        p.start()
+    for p in processes:
+        p.join()
+
+
+if __name__ == "__main__":
+    main()
