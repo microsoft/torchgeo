0387d3db7eea2d0d0c62213143960c05f35d18c8
Caleb Robinson <calebrob6@gmail.com>
2021-09-29 05:02:22 +0000

    BYOL trainer is not Chesapeake specific

--- torchgeo/trainers/byol.py
+++ torchgeo/trainers/byol.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-"""Trainers for the Chesapeake datasets."""
+"""Trainer task for BYOL."""
 import random
 from copy import deepcopy
 from typing import Any, Callable, Dict, Optional, Tuple, Union, cast
