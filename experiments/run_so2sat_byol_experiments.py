b0396550f74f7bcc62bf2d5e887ebd89f22c2896
isaac <22203655+isaaccorley@users.noreply.github.com>
2021-10-11 17:23:52 -0500

    Merge pull request #183 from isaaccorley/trainers/extract_encoder
    Load Pretrained Weights in Trainer

---
+++ experiments/run_so2sat_byol_experiments.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""Runs the train script with a grid of hyperparameters."""
+import itertools
+import os
+import subprocess
+from multiprocessing import Process, Queue
+from typing import List
+
+# list of GPU IDs that we want to use, one job will be started for every ID in the list
+GPUS = [0, 1, 2, 3, 3]
+DRY_RUN = False  # if False then print out the commands to be run, if True then run
+DATA_DIR = ""  # path to the So2Sat data directory
+
+# Hyperparameter options
+model_options = ["resnet50"]
+lr_options = [1e-4]
+loss_options = ["ce"]
+weight_options: List[str] = []  # set paths to checkpoint files
+bands_options = ["s2"]
+
+
+def do_work(work: "Queue[str]", gpu_idx: int) -> bool:
+    """Process for each ID in GPUS."""
+    while not work.empty():
+        experiment = work.get()
+        experiment = experiment.replace("GPU", str(gpu_idx))
+        print(experiment)
+        if not DRY_RUN:
+            subprocess.call(experiment.split(" "))
+    return True
+
+
+if __name__ == "__main__":
+    work: "Queue[str]" = Queue()
+
+    for (model, lr, loss, weights, bands) in itertools.product(
+        model_options,
+        lr_options,
+        loss_options,
+        weight_options,
+        bands_options,
+    ):
+
+        experiment_name = f"{model}_{lr}_{loss}_byol_{bands}-{weights.split('/')[-2]}"
+
+        output_dir = os.path.join("output", "so2sat_experiments")
+        log_dir = os.path.join(output_dir, "logs")
+        config_file = os.path.join("conf", "so2sat.yaml")
+
+        if not os.path.exists(os.path.join(output_dir, experiment_name)):
+
+            command = (
+                "python train.py"
+                + f" config_file={config_file}"
+                + f" experiment.name={experiment_name}"
+                + f" experiment.module.classification_model={model}"
+                + f" experiment.module.learning_rate={lr}"
+                + f" experiment.module.loss={loss}"
+                + f" experiment.module.weights={weights}"
+                + " experiment.module.in_channels=10"
+                + f" experiment.datamodule.bands={bands}"
+                + f" program.output_dir={output_dir}"
+                + f" program.log_dir={log_dir}"
+                + f" program.data_dir={DATA_DIR}"
+                + " trainer.gpus=[GPU]"
+            )
+            command = command.strip()
+
+            work.put(command)
+
+    processes = []
+    for gpu_idx in GPUS:
+        p = Process(
+            target=do_work,
+            args=(
+                work,
+                gpu_idx,
+            ),
+        )
+        processes.append(p)
+        p.start()
+    for p in processes:
+        p.join()
---
+++ torchgeo/trainers/utils.py
@@ -0,0 +1,97 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""Common trainer utilities."""
+
+import warnings
+from collections import OrderedDict
+from typing import Dict, Tuple
+
+import torch
+from torch import Tensor
+from torch.nn.modules import Module
+
+# https://github.com/pytorch/pytorch/issues/60979
+# https://github.com/pytorch/pytorch/pull/61045
+Module.__module__ = "nn.Module"
+
+
+def extract_encoder(path: str) -> Tuple[str, Dict[str, Tensor]]:
+    """Extracts an encoder from a pytorch lightning checkpoint file.
+
+    Args:
+        path: path to checkpoint file (.ckpt)
+
+    Returns:
+        name: str representing model name (generally a torchvision resnet model)
+        state_dict: dict of layer names and weight tensors
+
+    Raises:
+        ValueError: if 'classification_model' or 'encoder' not in
+            checkpoint['hyper_parameters']
+    """
+    checkpoint = torch.load(  # type: ignore[no-untyped-call]
+        path, map_location=torch.device("cpu")  # type: ignore[attr-defined]
+    )
+
+    if "classification_model" in checkpoint["hyper_parameters"]:
+        name = checkpoint["hyper_parameters"]["classification_model"]
+        state_dict = checkpoint["state_dict"]
+        state_dict = OrderedDict({k: v for k, v in state_dict.items() if "model." in k})
+        state_dict = OrderedDict(
+            {k.replace("model.", ""): v for k, v in state_dict.items()}
+        )
+    elif "encoder" in checkpoint["hyper_parameters"]:
+        name = checkpoint["hyper_parameters"]["encoder"]
+        state_dict = checkpoint["state_dict"]
+        state_dict = OrderedDict(
+            {k: v for k, v in state_dict.items() if "model.encoder.model" in k}
+        )
+        state_dict = OrderedDict(
+            {k.replace("model.encoder.model.", ""): v for k, v in state_dict.items()}
+        )
+    else:
+        raise ValueError(
+            """Unknown checkpoint task. Only encoder or classification_model"""
+            """extraction is supported"""
+        )
+
+    return name, state_dict
+
+
+def load_state_dict(model: Module, state_dict: Dict[str, Tensor]) -> Module:
+    """Load pretrained resnet weights to a model.
+
+    Args:
+        model: model to load the pretrained weights to
+        state_dict: dict containing tensor parameters
+
+    Returns:
+        the model with pretrained weights
+
+    Warns:
+        If input channels in model != pretrained model input channels
+        If num output classes in model != pretrained model num classes
+    """
+    in_channels = model.conv1.in_channels  # type: ignore[union-attr]
+    expected_in_channels = state_dict["conv1.weight"].shape[1]
+    num_classes = model.fc.out_features  # type: ignore[union-attr]
+    expected_num_classes = state_dict["fc.weight"].shape[0]
+
+    if in_channels != expected_in_channels:
+        warnings.warn(
+            f"""input channels {in_channels} != input channels in pretrained"""
+            """model {expected_in_channels}. Overriding with new input channels"""
+        )
+        del state_dict["conv1.weight"]
+
+    if num_classes != expected_num_classes:
+        warnings.warn(
+            f"""num classes {num_classes} != num classes in pretrained model"""
+            """{expected_num_classes}. Overriding with new num classes"""
+        )
+        del state_dict["fc.weight"], state_dict["fc.bias"]
+
+    model.load_state_dict(state_dict, strict=False)  # type: ignore[arg-type]
+
+    return model
