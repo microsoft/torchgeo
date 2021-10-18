f6e6e7d95f257a2806ddf1e8e0afecd434b674ad
Esther Rolf <esther_rolf@berkeley.edu>
2021-10-18 18:24:30 +0000

    adding QR loss functions for learning on the prior

---
+++ torchgeo/losses/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""TorchGeo specific losses."""
+
+from .qr_losses import loss_on_prior_simple, loss_on_prior_reversed_kl_simple
+
+__all__ = (
+    "loss_on_prior_simple",
+    "loss_on_prior_reversed_kl_simple",
+)
+
+# https://stackoverflow.com/questions/40018681
+for module in __all__:
+    globals()[module].__module__ = "torchgeo.losses"
---
+++ torchgeo/losses/qr_losses.py
@@ -0,0 +1,42 @@
+import torch as T
+
+
+def loss_on_prior_simple(logged_output, 
+                         target):
+    '''QR (forwards) loss on prior.
+    
+    Args:
+        logged_output (torch.Tensor): Log-probabilities of predictions
+        target (torch.Tensor): Prior probabilities
+    Returns:
+        loss (torch.Tensor): Computed loss
+    '''
+   
+    q = T.exp(logged_output)
+    q_bar = q.mean(axis=(0,2,3))
+    qbar_log_S = (q_bar * T.log(q_bar) ).sum()
+        
+    q_log_p = T.einsum('bcxy,bcxy->bxy', q, T.log(target)).mean()
+
+    loss = qbar_log_S - q_log_p
+    return loss
+   
+def loss_on_prior_reversed_kl_simple(logged_output, 
+                                      target):
+    '''RQ (backwards) loss on prior.
+    
+    Args:
+        logged_output (torch.Tensor): Log-probabilities of predictions
+        target (torch.Tensor): Prior probabilities
+    Returns:
+        loss (torch.Tensor): Computed loss
+    '''
+
+    q = T.exp(logged_output)
+
+    # they're in batches 
+    z = T.nn.functional.normalize(q,p=1,dim=(0,2,3))
+    r = T.nn.functional.normalize(z * target, p=1,dim=1)
+        
+    loss = T.einsum('bcxy,bcxy->bxy', r, T.log(r) - T.log(q)).mean() 
+    return loss
