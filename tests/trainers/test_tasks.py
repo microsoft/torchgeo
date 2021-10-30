58a09bd89d2ce6eba8bde46f83e8d4c53c34374c
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2021-10-30 15:21:47 -0500

    clean up fixtures in test_tasks.py

--- tests/trainers/test_tasks.py
+++ tests/trainers/test_tasks.py
@@ -19,65 +19,35 @@ from torchgeo.trainers import (
 from .test_utils import mocked_log
 
 
-@pytest.fixture(scope="module", params=[("rgb", 3), ("s2", 10)])
-def bands_so2sat(request: SubRequest) -> Tuple[str, int]:
-    return cast(Tuple[str, int], request.param)
-
-
-@pytest.fixture(scope="module", params=[("s1", 2), ("s2", 12), ("all", 14)])
-def bands_bigearthnet(request: SubRequest) -> Tuple[str, int]:
-    return cast(Tuple[str, int], request.param)
-
-
-@pytest.fixture(scope="module", params=[True, False])
-def datamodule_classification(
-    bands_so2sat: Tuple[str, int], request: SubRequest
-) -> So2SatDataModule:
-    band_set = bands_so2sat[0]
-    unsupervised_mode = request.param
-    root = os.path.join("tests", "data", "so2sat")
-    batch_size = 2
-    num_workers = 0
-    dm = So2SatDataModule(root, batch_size, num_workers, band_set, unsupervised_mode)
-    dm.prepare_data()
-    dm.setup()
-    return dm
-
-
-@pytest.fixture(scope="module", params=[True, False])
-def datamodule_multilabel(
-    bands_bigearthnet: Tuple[str, int], request: SubRequest
-) -> BigEarthNetDataModule:
-    band_set = bands_bigearthnet[0]
-    unsupervised_mode = request.param
-    root = os.path.join("tests", "data", "bigearthnet")
-    batch_size = 1
-    num_workers = 0
-    dm = BigEarthNetDataModule(
-        root,
-        band_set,
-        batch_size,
-        num_workers,
-        unsupervised_mode,
-        val_split_pct=0.3,
-        test_split_pct=0.3,
-    )
-    dm.prepare_data()
-    dm.setup()
-    return dm
-
-
 class TestClassificationTask:
+    @pytest.fixture(params=[("rgb", 3), ("s2", 10)])
+    def bands(self, request: SubRequest) -> Tuple[str, int]:
+        return cast(Tuple[str, int], request.param)
+
+    @pytest.fixture(params=[True, False])
+    def datamodule(
+        self, bands: Tuple[str, int], request: SubRequest
+    ) -> So2SatDataModule:
+        band_set = bands[0]
+        unsupervised_mode = request.param
+        root = os.path.join("tests", "data", "so2sat")
+        batch_size = 2
+        num_workers = 0
+        dm = So2SatDataModule(
+            root, batch_size, num_workers, band_set, unsupervised_mode
+        )
+        dm.prepare_data()
+        dm.setup()
+        return dm
+
     @pytest.fixture(
         params=zip(["ce", "jaccard", "focal"], ["imagenet", "random", "random"])
     )
-    def config(
-        self, request: SubRequest, bands_so2sat: Tuple[str, int]
-    ) -> Dict[str, Any]:
+    def config(self, request: SubRequest, bands: Tuple[str, int]) -> Dict[str, Any]:
         task_conf = OmegaConf.load(os.path.join("conf", "task_defaults", "so2sat.yaml"))
         task_args = OmegaConf.to_object(task_conf.experiment.module)
         task_args = cast(Dict[str, Any], task_args)
-        task_args["in_channels"] = bands_so2sat[1]
+        task_args["in_channels"] = bands[1]
         loss, weights = request.param
         task_args["loss"] = loss
         task_args["weights"] = weights
@@ -97,23 +67,21 @@ class TestClassificationTask:
         assert "lr_scheduler" in out
 
     def test_training(
-        self, datamodule_classification: So2SatDataModule, task: ClassificationTask
+        self, datamodule: So2SatDataModule, task: ClassificationTask
     ) -> None:
-        batch = next(iter(datamodule_classification.train_dataloader()))
+        batch = next(iter(datamodule.train_dataloader()))
         task.training_step(batch, 0)
         task.training_epoch_end(0)
 
     def test_validation(
-        self, datamodule_classification: So2SatDataModule, task: ClassificationTask
+        self, datamodule: So2SatDataModule, task: ClassificationTask
     ) -> None:
-        batch = next(iter(datamodule_classification.val_dataloader()))
+        batch = next(iter(datamodule.val_dataloader()))
         task.validation_step(batch, 0)
         task.validation_epoch_end(0)
 
-    def test_test(
-        self, datamodule_classification: So2SatDataModule, task: ClassificationTask
-    ) -> None:
-        batch = next(iter(datamodule_classification.test_dataloader()))
+    def test_test(self, datamodule: So2SatDataModule, task: ClassificationTask) -> None:
+        batch = next(iter(datamodule.test_dataloader()))
         task.test_step(batch, 0)
         task.test_epoch_end(0)
 
@@ -152,16 +120,40 @@ class TestClassificationTask:
 
 
 class TestMultiLabelClassificationTask:
+    @pytest.fixture(params=[("s1", 2), ("s2", 12), ("all", 14)])
+    def bands(self, request: SubRequest) -> Tuple[str, int]:
+        return cast(Tuple[str, int], request.param)
+
+    @pytest.fixture(params=[True, False])
+    def datamodule(
+        self, bands: Tuple[str, int], request: SubRequest
+    ) -> BigEarthNetDataModule:
+        band_set = bands[0]
+        unsupervised_mode = request.param
+        root = os.path.join("tests", "data", "bigearthnet")
+        batch_size = 1
+        num_workers = 0
+        dm = BigEarthNetDataModule(
+            root,
+            band_set,
+            batch_size,
+            num_workers,
+            unsupervised_mode,
+            val_split_pct=0.3,
+            test_split_pct=0.3,
+        )
+        dm.prepare_data()
+        dm.setup()
+        return dm
+
     @pytest.fixture(params=zip(["bce", "bce"], ["imagenet", "random"]))
-    def config(
-        self, request: SubRequest, bands_bigearthnet: Tuple[str, int]
-    ) -> Dict[str, Any]:
+    def config(self, request: SubRequest, bands: Tuple[str, int]) -> Dict[str, Any]:
         task_conf = OmegaConf.load(
             os.path.join("conf", "task_defaults", "bigearthnet.yaml")
         )
         task_args = OmegaConf.to_object(task_conf.experiment.module)
         task_args = cast(Dict[str, Any], task_args)
-        task_args["in_channels"] = bands_bigearthnet[1]
+        task_args["in_channels"] = bands[1]
         loss, weights = request.param
         task_args["loss"] = loss
         task_args["weights"] = weights
@@ -177,28 +169,28 @@ class TestMultiLabelClassificationTask:
 
     def test_training(
         self,
-        datamodule_multilabel: BigEarthNetDataModule,
+        datamodule: BigEarthNetDataModule,
         task: MultiLabelClassificationTask,
     ) -> None:
-        batch = next(iter(datamodule_multilabel.train_dataloader()))
+        batch = next(iter(datamodule.train_dataloader()))
         task.training_step(batch, 0)
         task.training_epoch_end(0)
 
     def test_validation(
         self,
-        datamodule_multilabel: BigEarthNetDataModule,
+        datamodule: BigEarthNetDataModule,
         task: MultiLabelClassificationTask,
     ) -> None:
-        batch = next(iter(datamodule_multilabel.val_dataloader()))
+        batch = next(iter(datamodule.val_dataloader()))
         task.validation_step(batch, 0)
         task.validation_epoch_end(0)
 
     def test_test(
         self,
-        datamodule_multilabel: BigEarthNetDataModule,
+        datamodule: BigEarthNetDataModule,
         task: MultiLabelClassificationTask,
     ) -> None:
-        batch = next(iter(datamodule_multilabel.test_dataloader()))
+        batch = next(iter(datamodule.test_dataloader()))
         task.test_step(batch, 0)
         task.test_epoch_end(0)
 
