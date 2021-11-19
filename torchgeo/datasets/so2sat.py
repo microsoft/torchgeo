0b2082c0a183bc10ad5d3dda8d8fb4a06370e966
Caleb Robinson <calebrob6@gmail.com>
2021-11-19 19:08:18 +0000

    Fixed So2Sat doc

--- torchgeo/datasets/so2sat.py
+++ torchgeo/datasets/so2sat.py
@@ -214,7 +214,7 @@ class So2Sat(VisionDataset):
         """Plot a sample from the dataset.
 
         Args:
-            sample: a sample returned by :meth:`VisionClassificationDataset.__getitem__`
+            sample: a sample returned by :meth:`__getitem__`
             show_titles: flag indicating whether to show titles above each panel
             suptitle: optional string to use as a suptitle
 
