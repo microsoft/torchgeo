2839a1669d990637772b55e52fafe3c5dc870ae6
Ashwin Nair <ash1995@gmail.com>
2021-11-26 12:56:26 +0400

    Fix docs

--- torchgeo/datasets/spacenet.py
+++ torchgeo/datasets/spacenet.py
@@ -934,7 +934,7 @@ class SpaceNet5(SpaceNet):
         """Plot a sample from the dataset.
 
         Args:
-            sample: a sample returned by :meth:`__getitem__`
+            sample: a sample returned by :meth:`SpaceNet.__getitem__`
             show_titles: flag indicating whether to show titles above each panel
             suptitle: optional string to use as a suptitle
 
