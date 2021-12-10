303c7190d83a28ab5cebc7e8824e635b41e88a19
Caleb Robinson <calebrob6@gmail.com>
2021-12-09 17:38:30 -0800

    Fixing network calls in unit tests (#265)
    * Fixing network call in test of pretrained resnet
    
    * Enable loading a pretrained model from a local path
    
    * Fix filename
    
    * monkeypatch the load_state_dict_from_url method instead of going around it
    
    * Make default configs use random weights instead of imagenet pre-trained weights
    
    * Removing internet access requirement from tests
    
    * Deleted So2Sat trainer

--- tests/models/test_resnet.py
+++ tests/models/test_resnet.py
@@ -1,11 +1,30 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+import os
+from pathlib import Path
+from typing import Any, Generator, Optional
+
 import pytest
 import torch
+from _pytest.monkeypatch import MonkeyPatch
 from torch.nn.modules import Module
 
+import torchgeo.models.resnet
+from torchgeo.datasets.utils import extract_archive
 from torchgeo.models import resnet50
+
+
+def load_state_dict_from_file(
+    file: str,
+    model_dir: Optional[str] = None,
+    map_location: Optional[Any] = None,
+    progress: Optional[bool] = True,
+    check_hash: Optional[bool] = False,
+    file_name: Optional[str] = None,
+) -> Any:
+    """Mockup of ``torch.hub.load_state_dict_from_url``."""
+    return torch.load(file)  # type: ignore[no-untyped-call]
 
 
 @pytest.mark.parametrize(
@@ -13,8 +32,30 @@ from torchgeo.models import resnet50
     [(resnet50, "sentinel2", "all", 10, 17)],
 )
 def test_resnet(
-    model_class: Module, sensor: str, bands: str, in_channels: int, num_classes: int
+    monkeypatch: Generator[MonkeyPatch, None, None],
+    tmp_path: Path,
+    model_class: Module,
+    sensor: str,
+    bands: str,
+    in_channels: int,
+    num_classes: int,
 ) -> None:
+    extract_archive(
+        os.path.join("tests", "data", "models", "resnet50-sentinel2-2.pt.zip"),
+        str(tmp_path),
+    )
+
+    new_model_urls = {
+        "sentinel2": {"all": {"resnet50": str(tmp_path / "resnet50-sentinel2-2.pt")}}
+    }
+
+    monkeypatch.setattr(  # type: ignore[attr-defined]
+        torchgeo.models.resnet, "MODEL_URLS", new_model_urls
+    )
+    monkeypatch.setattr(  # type: ignore[attr-defined]
+        torchgeo.models.resnet, "load_state_dict_from_url", load_state_dict_from_file
+    )
+
     model = model_class(sensor, bands, pretrained=True)
     x = torch.zeros(1, in_channels, 256, 256)  # type: ignore[attr-defined]
     y = model(x)
