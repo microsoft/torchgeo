3393f335998a9fc3531462339d8ca0b5bce110db
isaac <22203655+isaaccorley@users.noreply.github.com>
2021-12-10 18:09:08 -0600

    Add FAIR1M Dataset (#232)

---
+++ tests/datasets/test_fair1m.py
@@ -0,0 +1,97 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+import shutil
+from pathlib import Path
+from typing import Generator
+
+import matplotlib.pyplot as plt
+import pytest
+import torch
+import torch.nn as nn
+from _pytest.monkeypatch import MonkeyPatch
+
+from torchgeo.datasets import FAIR1M, FAIR1MDataModule
+
+
+class TestFAIR1M:
+    @pytest.fixture
+    def dataset(self, monkeypatch: Generator[MonkeyPatch, None, None]) -> FAIR1M:
+        md5s = ["f278aba757de9079225db42107e09e30", "aca59017207141951b53e91795d8179e"]
+        monkeypatch.setattr(FAIR1M, "md5s", md5s)  # type: ignore[attr-defined]
+        root = os.path.join("tests", "data", "fair1m")
+        transforms = nn.Identity()  # type: ignore[attr-defined]
+        return FAIR1M(root, transforms)
+
+    def test_getitem(self, dataset: FAIR1M) -> None:
+        x = dataset[0]
+        assert isinstance(x, dict)
+        assert isinstance(x["image"], torch.Tensor)
+        assert isinstance(x["boxes"], torch.Tensor)
+        assert isinstance(x["label"], torch.Tensor)
+        assert x["image"].shape[0] == 3
+        assert x["boxes"].shape[-2:] == (5, 2)
+        assert x["label"].ndim == 1
+
+    def test_len(self, dataset: FAIR1M) -> None:
+        assert len(dataset) == 4
+
+    def test_already_downloaded(self, dataset: FAIR1M, tmp_path: Path) -> None:
+        shutil.rmtree(str(tmp_path))
+        shutil.copytree(dataset.root, str(tmp_path))
+        FAIR1M(root=str(tmp_path))
+
+    def test_already_downloaded_not_extracted(
+        self, dataset: FAIR1M, tmp_path: Path
+    ) -> None:
+        for filename in dataset.filenames:
+            filepath = os.path.join("tests", "data", "fair1m", filename)
+            shutil.copy(filepath, str(tmp_path))
+        FAIR1M(root=str(tmp_path), checksum=True)
+
+    def test_corrupted(self, tmp_path: Path) -> None:
+        filenames = ["images.zip", "labelXmls.zip"]
+        for filename in filenames:
+            with open(os.path.join(tmp_path, filename), "w") as f:
+                f.write("bad")
+        with pytest.raises(RuntimeError, match="Dataset found, but corrupted."):
+            FAIR1M(root=str(tmp_path), checksum=True)
+
+    def test_not_downloaded(self, tmp_path: Path) -> None:
+        err = "Dataset not found in `root` directory, "
+        "specify a different `root` directory."
+        with pytest.raises(RuntimeError, match=err):
+            FAIR1M(str(tmp_path))
+
+    def test_plot(self, dataset: FAIR1M) -> None:
+        x = dataset[0].copy()
+        dataset.plot(x, suptitle="Test")
+        plt.close()
+        dataset.plot(x, show_titles=False)
+        plt.close()
+        x["prediction_boxes"] = x["boxes"].clone()
+        dataset.plot(x)
+        plt.close()
+
+
+class TestFAIR1MDataModule:
+    @pytest.fixture(scope="class", params=[True, False])
+    def datamodule(self) -> FAIR1MDataModule:
+        root = os.path.join("tests", "data", "fair1m")
+        batch_size = 2
+        num_workers = 0
+        dm = FAIR1MDataModule(
+            root, batch_size, num_workers, val_split_pct=0.33, test_split_pct=0.33
+        )
+        dm.setup()
+        return dm
+
+    def test_train_dataloader(self, datamodule: FAIR1MDataModule) -> None:
+        next(iter(datamodule.train_dataloader()))
+
+    def test_val_dataloader(self, datamodule: FAIR1MDataModule) -> None:
+        next(iter(datamodule.val_dataloader()))
+
+    def test_test_dataloader(self, datamodule: FAIR1MDataModule) -> None:
+        next(iter(datamodule.test_dataloader()))
---
+++ torchgeo/datasets/fair1m.py
@@ -0,0 +1,451 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""FAIR1M dataset."""
+
+import glob
+import os
+from typing import Any, Callable, Dict, List, Optional, Tuple, cast
+from xml.etree import ElementTree
+
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from PIL import Image
+from torch import Tensor
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
+
+from ..datasets.utils import check_integrity, dataset_split, extract_archive
+from .geo import VisionDataset
+
+# https://github.com/pytorch/pytorch/issues/60979
+# https://github.com/pytorch/pytorch/pull/61045
+DataLoader.__module__ = "torch.utils.data"
+
+
+def collate_fn(batch: List[Dict[str, Tensor]]) -> Dict[str, Any]:
+    """Custom object detection collate fn to handle variable number of boxes.
+
+    Args:
+        batch: list of sample dicts return by dataset
+    Returns:
+        batch dict output
+    """
+    output: Dict[str, Any] = {}
+    output["image"] = torch.stack([sample["image"] for sample in batch])
+    output["boxes"] = [sample["boxes"] for sample in batch]
+    return output
+
+
+def parse_pascal_voc(path: str) -> Dict[str, Any]:
+    """Read a PASCAL VOC annotation file.
+
+    Args:
+        path: path to xml file
+
+    Returns:
+        dict of image filename, points, and class labels
+    """
+    et = ElementTree.parse(path)
+    element = et.getroot()
+    filename = element.find("source").find("filename").text  # type: ignore[union-attr]
+    labels, points = [], []
+    for obj in element.find("objects").findall("object"):  # type: ignore[union-attr]
+        obj_points = [
+            p for p in obj.find("points").findall("point")  # type: ignore[union-attr]
+        ]
+        obj_points = [p.text.split(",") for p in obj_points]  # type: ignore[union-attr]
+        obj_points = [
+            (float(p1), float(p2)) for p1, p2 in obj_points  # type: ignore[arg-type]
+        ]
+        label = obj.find("possibleresult").find("name").text  # type: ignore[union-attr]
+        labels.append(label)
+        points.append(obj_points)
+    return dict(filename=filename, points=points, labels=labels)
+
+
+class FAIR1M(VisionDataset):
+    """FAIR1M dataset.
+
+    The `FAIR1M <http://gaofen-challenge.com/benchmark>`_
+    dataset is a dataset for remote sensing fine-grained oriented object detection.
+
+    Dataset features:
+
+    * 15,000+ images with 0.3-0.8 m per pixel resolution (1,000-10,000 px)
+    * 1 million object instances
+    * 5 object categories, 37 object sub-categories
+    * three spectral bands - RGB
+    * images taken by Gaofen satellites and Google Earth
+
+    Dataset format:
+
+    * images are three-channel tiffs
+    * labels are xml files with PASCAL VOC like annotations
+
+    Dataset classes:
+
+    0. Passenger Ship
+    1. Motorboat
+    2. Fishing Boat
+    3. Tugboat
+    4. other-ship
+    5. Engineering Ship
+    6. Liquid Cargo Ship
+    7. Dry Cargo Ship
+    8. Warship
+    9. Small Car
+    10. Bus
+    11. Cargo Truck
+    12. Dump Truck
+    13. other-vehicle
+    14. Van
+    15. Trailer
+    16. Tractor
+    17. Excavator
+    18. Truck Tractor
+    19. Boeing737
+    20. Boeing747
+    21. Boeing777
+    22. Boeing787
+    23. ARJ21
+    24. C919
+    25. A220
+    26. A321
+    27. A330
+    28. A350
+    29. other-airplane
+    30. Baseball Field
+    31. Basketball Court
+    32. Football Field
+    33. Tennis Court
+    34. Roundabout
+    35. Intersection
+    36. Bridge
+
+    If you use this dataset in your research, please cite the following paper:
+
+    * https://arxiv.org/abs/2103.05569
+
+    .. versionadded:: 0.2
+    """
+
+    classes = {
+        "Passenger Ship": {"id": 0, "category": "Ship"},
+        "Motorboat": {"id": 1, "category": "Ship"},
+        "Fishing Boat": {"id": 2, "category": "Ship"},
+        "Tugboat": {"id": 3, "category": "Ship"},
+        "other-ship": {"id": 4, "category": "Ship"},
+        "Engineering Ship": {"id": 5, "category": "Ship"},
+        "Liquid Cargo Ship": {"id": 6, "category": "Ship"},
+        "Dry Cargo Ship": {"id": 7, "category": "Ship"},
+        "Warship": {"id": 8, "category": "Ship"},
+        "Small Car": {"id": 9, "category": "Vehicle"},
+        "Bus": {"id": 10, "category": "Vehicle"},
+        "Cargo Truck": {"id": 11, "category": "Vehicle"},
+        "Dump Truck": {"id": 12, "category": "Vehicle"},
+        "other-vehicle": {"id": 13, "category": "Vehicle"},
+        "Van": {"id": 14, "category": "Vehicle"},
+        "Trailer": {"id": 15, "category": "Vehicle"},
+        "Tractor": {"id": 16, "category": "Vehicle"},
+        "Excavator": {"id": 17, "category": "Vehicle"},
+        "Truck Tractor": {"id": 18, "category": "Vehicle"},
+        "Boeing737": {"id": 19, "category": "Airplane"},
+        "Boeing747": {"id": 20, "category": "Airplane"},
+        "Boeing777": {"id": 21, "category": "Airplane"},
+        "Boeing787": {"id": 22, "category": "Airplane"},
+        "ARJ21": {"id": 23, "category": "Airplane"},
+        "C919": {"id": 24, "category": "Airplane"},
+        "A220": {"id": 25, "category": "Airplane"},
+        "A321": {"id": 26, "category": "Airplane"},
+        "A330": {"id": 27, "category": "Airplane"},
+        "A350": {"id": 28, "category": "Airplane"},
+        "other-airplane": {"id": 29, "category": "Airplane"},
+        "Baseball Field": {"id": 30, "category": "Court"},
+        "Basketball Court": {"id": 31, "category": "Court"},
+        "Football Field": {"id": 32, "category": "Court"},
+        "Tennis Court": {"id": 33, "category": "Court"},
+        "Roundabout": {"id": 34, "category": "Road"},
+        "Intersection": {"id": 35, "category": "Road"},
+        "Bridge": {"id": 36, "category": "Road"},
+    }
+
+    image_root: str = "images"
+    labels_root: str = "labelXmls"
+    filenames = ["images.zip", "labelXmls.zip"]
+    md5s = ["a460fe6b1b5b276bf856ce9ac72d6568", "ca8666dc43a553f8d65e5dc671a8ac3c"]
+
+    def __init__(
+        self,
+        root: str = "data",
+        transforms: Optional[Callable[[Dict[str, Tensor]], Dict[str, Tensor]]] = None,
+        checksum: bool = False,
+    ) -> None:
+        """Initialize a new FAIR1M dataset instance.
+
+        Args:
+            root: root directory where dataset can be found
+            transforms: a function/transform that takes input sample and its target as
+                entry and returns a transformed version
+            checksum: if True, check the MD5 of the downloaded files (may be slow)
+        """
+        self.root = root
+        self.transforms = transforms
+        self.checksum = checksum
+        self._verify()
+        self.files = sorted(
+            glob.glob(os.path.join(self.root, self.labels_root, "*.xml"))
+        )
+
+    def __getitem__(self, index: int) -> Dict[str, Tensor]:
+        """Return an index within the dataset.
+
+        Args:
+            index: index to return
+
+        Returns:
+            data and label at that index
+        """
+        path = self.files[index]
+        parsed = parse_pascal_voc(path)
+        image = self._load_image(parsed["filename"])
+        boxes, labels = self._load_target(parsed["points"], parsed["labels"])
+        sample = {"image": image, "boxes": boxes, "label": labels}
+
+        if self.transforms is not None:
+            sample = self.transforms(sample)
+
+        return sample
+
+    def __len__(self) -> int:
+        """Return the number of data points in the dataset.
+
+        Returns:
+            length of the dataset
+        """
+        return len(self.files)
+
+    def _load_image(self, path: str) -> Tensor:
+        """Load a single image.
+
+        Args:
+            path: path to image
+
+        Returns:
+            the image
+        """
+        path = os.path.join(self.root, self.image_root, path)
+        with Image.open(path) as img:
+            array = np.array(img.convert("RGB"))
+            tensor: Tensor = torch.from_numpy(array)  # type: ignore[attr-defined]
+            # Convert from HxWxC to CxHxW
+            tensor = tensor.permute((2, 0, 1))
+            return tensor
+
+    def _load_target(
+        self, points: List[List[Tuple[float, float]]], labels: List[str]
+    ) -> Tuple[Tensor, Tensor]:
+        """Load the target mask for a single image.
+
+        Args:
+            points: list of point tuple lists
+            labels: list of class labels
+
+        Returns:
+            the target bounding boxes and labels
+        """
+        labels_list = [self.classes[label]["id"] for label in labels]
+        boxes = torch.tensor(points).to(torch.float)  # type: ignore[attr-defined]
+        labels = torch.tensor(labels_list)  # type: ignore[attr-defined]
+        return boxes, cast(Tensor, labels)
+
+    def _verify(self) -> None:
+        """Verify the integrity of the dataset.
+
+        Raises:
+            RuntimeError: if checksum fails or the dataset is not found
+        """
+        # Check if the files already exist
+        exists = []
+        for directory in [self.image_root, self.labels_root]:
+            exists.append(os.path.exists(os.path.join(self.root, directory)))
+        if all(exists):
+            return
+
+        # Check if .zip files already exists (if so extract)
+        exists = []
+        for filename, md5 in zip(self.filenames, self.md5s):
+            filepath = os.path.join(self.root, filename)
+            if os.path.isfile(filepath):
+                if self.checksum and not check_integrity(filepath, md5):
+                    raise RuntimeError("Dataset found, but corrupted.")
+                exists.append(True)
+                extract_archive(filepath)
+            else:
+                exists.append(False)
+
+        if all(exists):
+            return
+
+        raise RuntimeError(
+            "Dataset not found in `root` directory, "
+            "specify a different `root` directory."
+        )
+
+    def plot(
+        self,
+        sample: Dict[str, Tensor],
+        show_titles: bool = True,
+        suptitle: Optional[str] = None,
+    ) -> plt.Figure:
+        """Plot a sample from the dataset.
+
+        Args:
+            sample: a sample returned by :meth:`__getitem__`
+            show_titles: flag indicating whether to show titles above each panel
+            suptitle: optional string to use as a suptitle
+
+        Returns:
+            a matplotlib Figure with the rendered sample
+        """
+        image = sample["image"].permute((1, 2, 0)).numpy()
+
+        ncols = 1
+        if "prediction_boxes" in sample:
+            ncols += 1
+
+        fig, axs = plt.subplots(ncols=ncols, figsize=(ncols * 10, 10))
+        if ncols < 2:
+            axs = [axs]
+
+        axs[0].imshow(image)
+        axs[0].axis("off")
+        polygons = [
+            patches.Polygon(points, color="r", fill=False)
+            for points in sample["boxes"].numpy()
+        ]
+        for polygon in polygons:
+            axs[0].add_patch(polygon)
+
+        if show_titles:
+            axs[0].set_title("Ground Truth")
+
+        if ncols > 1:
+            axs[1].imshow(image)
+            axs[1].axis("off")
+            polygons = [
+                patches.Polygon(points, color="r", fill=False)
+                for points in sample["prediction_boxes"].numpy()
+            ]
+            for polygon in polygons:
+                axs[0].add_patch(polygon)
+
+            if show_titles:
+                axs[1].set_title("Predictions")
+
+        if suptitle is not None:
+            plt.suptitle(suptitle)
+
+        return fig
+
+
+class FAIR1MDataModule(pl.LightningDataModule):
+    """LightningDataModule implementation for the FAIR1M dataset."""
+
+    def __init__(
+        self,
+        root_dir: str,
+        batch_size: int = 64,
+        num_workers: int = 0,
+        val_split_pct: float = 0.2,
+        test_split_pct: float = 0.2,
+        **kwargs: Any,
+    ) -> None:
+        """Initialize a LightningDataModule for FAIR1M based DataLoaders.
+
+        Args:
+            root_dir: The ``root`` arugment to pass to the FAIR1M Dataset classes
+            batch_size: The batch size to use in all created DataLoaders
+            num_workers: The number of workers to use in all created DataLoaders
+            val_split_pct: What percentage of the dataset to use as a validation set
+            test_split_pct: What percentage of the dataset to use as a test set
+        """
+        super().__init__()  # type: ignore[no-untyped-call]
+        self.root_dir = root_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.val_split_pct = val_split_pct
+        self.test_split_pct = test_split_pct
+
+    def preprocess(self, sample: Dict[str, Any]) -> Dict[str, Any]:
+        """Transform a single sample from the Dataset.
+
+        Args:
+            sample: input image dictionary
+
+        Returns:
+            preprocessed sample
+        """
+        sample["image"] = sample["image"].float()
+        sample["image"] /= 255.0
+        return sample
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Initialize the main ``Dataset`` objects.
+
+        This method is called once per GPU per run.
+
+        Args:
+            stage: stage to set up
+        """
+        transforms = Compose([self.preprocess])
+
+        dataset = FAIR1M(self.root_dir, transforms=transforms)
+        self.train_dataset, self.val_dataset, self.test_dataset = dataset_split(
+            dataset, val_pct=self.val_split_pct, test_pct=self.test_split_pct
+        )
+
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Return a DataLoader for training.
+
+        Returns:
+            training data loader
+        """
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            collate_fn=collate_fn,
+        )
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Return a DataLoader for validation.
+
+        Returns:
+            validation data loader
+        """
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            collate_fn=collate_fn,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Return a DataLoader for testing.
+
+        Returns:
+            testing data loader
+        """
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            collate_fn=collate_fn,
+        )
