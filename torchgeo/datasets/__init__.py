7718690d40504e26a22eb5f160ad7e1ec6b85a1d
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2021-12-20 13:29:26 -0600

    draft

--- torchgeo/datasets/__init__.py
+++ torchgeo/datasets/__init__.py
@@ -193,6 +193,14 @@ __all__ = (
     "MillionAID",
     "NASAMarineDebris",
     "OSCD",
+<<<<<<< HEAD
+=======
+    "OSCDDataModule",
+    "PASTISInstanceSegmentation",
+    "PASTISInstanceSegmentationDataModule",
+    "PASTISSemanticSegmentation",
+    "PASTISSemanticSegmentationDataModule",
+>>>>>>> draft
     "PatternNet",
     "Potsdam2D",
     "RESISC45",
