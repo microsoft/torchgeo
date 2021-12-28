d9414ced8820b1ebf9f2fcbe35a5ea41dab512d8
Nils Lehmann <35272119+nilsleh@users.noreply.github.com>
2021-12-28 18:25:50 +0100

    Add plotting method for COWC dataset (#300)
    * plotting method cowc dataset
    
    * boolean label for detection
    
    * restructure plotting method
    
    * typo
    
    * label title not as variable
    
    * single plot method in super class
    
    Co-authored-by: Caleb Robinson <calebrob6@gmail.com>

--- torchgeo/datasets/cowc.py
+++ torchgeo/datasets/cowc.py
@@ -6,8 +6,9 @@
 import abc
 import csv
 import os
-from typing import Callable, Dict, List, Optional
+from typing import Callable, Dict, List, Optional, cast
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from PIL import Image
@@ -189,6 +190,48 @@ class COWC(VisionDataset, abc.ABC):
                 filename=filename,
                 md5=md5 if self.checksum else None,
             )
+
+    def plot(
+        self,
+        sample: Dict[str, Tensor],
+        show_titles: bool = True,
+        suptitle: Optional[str] = None,
+    ) -> plt.Figure:
+        """Plot a sample from the dataset.
+
+        Args:
+            sample: a sample returned by :meth:`VisionClassificationDataset.__getitem__`
+            show_titles: flag indicating whether to show titles above each panel
+            suptitle: optional string to use as a suptitle
+
+        Returns:
+            a matplotlib Figure with the rendered sample
+
+        .. versionadded:: 0.2
+        """
+        image = sample["image"]
+        label = cast(str, sample["label"].item())
+
+        showing_predictions = "prediction" in sample
+        if showing_predictions:
+            prediction = cast(str, sample["prediction"].item())
+        else:
+            prediction = None
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        ax.imshow(image.permute(1, 2, 0))
+        ax.axis("off")
+
+        if show_titles:
+            title = f"Label: {label}"
+            if prediction is not None:
+                title += f"\nPrediction: {prediction}"
+            ax.set_title(title)
+
+        if suptitle is not None:
+            plt.suptitle(suptitle)
+
+        return fig
 
 
 class COWCCounting(COWC):
