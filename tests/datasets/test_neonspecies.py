e14393917a89c4fc168bd91bec091f3ff7f95217
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-02-11 13:26:16 -0800

    flake8 passes

--- tests/datasets/test_neonspecies.py
+++ tests/datasets/test_neonspecies.py
@@ -2,19 +2,20 @@ from torchgeo.datasets import neonspecies
 from matplotlib import pyplot as plt
 import pytest
 
+
 @pytest.fixture()
 def dataset(tmpdir):
     dataset = neonspecies.NEONTreeSpecies(download=True, root=tmpdir)
-    
     return dataset
+
 
 def test_getitem(dataset):
     items = dataset.__getitem__(0)
     assert len(items) == 5
-    assert list(items.keys()) == ["image","hsi","chm","metadata","label"]
+    assert list(items.keys()) == ["image", "hsi", "chm", "metadata", "label"]
+
 
 def test_plot(dataset):
-    items = dataset.__getitem__(10)    
-    fig = dataset.plot(items)
+    items = dataset.__getitem__(10)
+    dataset.plot(items)
     plt.close()
-    
