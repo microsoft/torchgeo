bb1f48f76a75b5179987387f79c2e8faa6c29bff
Kaustav Mukherjee <oracler4284@gmail.com>
2022-02-24 02:28:15 +0530

    added test cases

--- tests/transforms/test_indices.py
+++ tests/transforms/test_indices.py
@@ -18,6 +18,7 @@ from torchgeo.transforms import (
     AppendNDWI,
     AppendNormalizedDifferenceIndex,
     AppendSWI,
+    AppendTriBandNormalizedDifferenceIndex,
 )
 
 
@@ -64,6 +65,20 @@ def test_append_index_batch(batch: Dict[str, Tensor]) -> None:
     assert output["image"].shape == (b, c + 1, h, w)
 
 
+def test_append_triband_index_sample(sample: Dict[str, Tensor]) -> None:
+    c, h, w = sample["image"].shape
+    tr = AppendTriBandNormalizedDifferenceIndex(index_a=0, index_b=0, index_c=0)
+    output = tr(sample)
+    assert output["image"].shape == (c + 1, h, w)
+
+
+def test_append_triband_index_batch(batch: Dict[str, Tensor]) -> None:
+    b, c, h, w = batch["image"].shape
+    tr = AppendTriBandNormalizedDifferenceIndex(index_a=0, index_b=0, index_c=0)
+    output = tr(batch)
+    assert output["image"].shape == (b, c + 1, h, w)
+
+
 @pytest.mark.parametrize(
     "index",
     [
@@ -80,6 +95,15 @@ def test_append_index_batch(batch: Dict[str, Tensor]) -> None:
 )
 def test_append_normalized_difference_indices(
     sample: Dict[str, Tensor], index: AppendNormalizedDifferenceIndex
+) -> None:
+    c, h, w = sample["image"].shape
+    tr = index(0, 0)
+    output = tr(sample)
+    assert output["image"].shape == (c + 1, h, w)
+
+
+def test_append_triband_normalized_difference_indices(
+    sample: Dict[str, Tensor], index: AppendTriBandNormalizedDifferenceIndex
 ) -> None:
     c, h, w = sample["image"].shape
     tr = index(0, 0)
