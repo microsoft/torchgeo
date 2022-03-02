28295d8c81d4df19d584fceb71c1d85d8f107081
Kaustav Mukherjee <oracler4284@gmail.com>
2022-03-03 01:31:56 +0530

    added GBNDVI and GRNDVI

--- torchgeo/transforms/indices.py
+++ torchgeo/transforms/indices.py
@@ -360,3 +360,57 @@ class AppendTriBandNormalizedDifferenceIndex(Module):
             sample["image"] = torch.cat([sample["image"], index], dim=self.dim)
 
         return sample
+
+    
+class AppendGRNDVI(AppendTriBandNormalizedDifferenceIndex):
+    r"""Green-Red Normalized Difference Vegetation Index (GRNDVI).
+
+    Computes the following index:
+
+    .. math::
+
+       \text{GRNDVI} = \frac{\text{NIR} - {\text{GREEN} + \text{RED}}}{\text{NIR} + {\text{GREEN} + \text{RED}}}
+
+    If you use this index in your research, please cite the following paper:
+
+    * https://doi.org/10.2134/agronj2001.933583x
+
+    .. versionadded:: 0.3
+    """
+
+    def __init__(self, index_nir: int, index_green: int, index_red: int) -> None:
+        """Initialize a new transform instance.
+
+        Args:
+            index_nir: index of the NIR band, e.g. B8 in Sentinel 2 imagery
+            index_green: index of the Green band, B3 in Sentinel 2 imagery
+            index_red: index of the Red band, B4 in Sentinel 2 imagery
+        """
+        super().__init__(index_a=index_nir, index_b=index_green, index_c=index_red)
+
+
+class AppendGBNDVI(AppendTriBandNormalizedDifferenceIndex):
+    r"""Green-Blue Normalized Difference Vegetation Index (GRNDVI).
+
+    Computes the following index:
+
+    .. math::
+
+       \text{GBNDVI} = \frac{\text{NIR} - {\text{GREEN} + \text{BLUE}}}{\text{NIR} + {\text{GREEN} + \text{BLUE}}}
+
+    If you use this index in your research, please cite the following paper:
+
+    * https://doi.org/10.2134/agronj2001.933583x
+
+    .. versionadded:: 0.3
+    """
+
+    def __init__(self, index_nir: int, index_green: int, index_blue: int) -> None:
+        """Initialize a new transform instance.
+
+        Args:
+            index_nir: index of the NIR band, e.g. B8 in Sentinel 2 imagery
+            index_green: index of the Green band, B3 in Sentinel 2 imagery
+            index_red: index of the Blue band, B2 in Sentinel 2 imagery
+        """
+        super().__init__(index_a=index_nir, index_b=index_green, index_c=index_blue)
