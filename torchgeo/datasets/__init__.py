20406b77f539347a3b452535ef1aaeef60525730
nilsleh <nils.lehmann24@gmail.com>
2022-03-09 22:44:27 +0100

    millionaid

--- torchgeo/datasets/__init__.py
+++ torchgeo/datasets/__init__.py
@@ -68,6 +68,7 @@ from .landsat import (
 )
 from .levircd import LEVIRCDPlus
 from .loveda import LoveDA
+from .millionaid import MillionAID
 from .naip import NAIP
 from .nasa_marine_debris import NASAMarineDebris
 from .nwpu import VHR10
@@ -162,6 +163,7 @@ __all__ = (
     "LandCoverAI",
     "LEVIRCDPlus",
     "LoveDA",
+    "MillionAID",
     "NASAMarineDebris",
     "OSCD",
     "PatternNet",
