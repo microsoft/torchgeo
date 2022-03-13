a93ef5f676568f1c8f0bf9f49d82c3f361176352
iejMac <kilianmaciej6@gmail.com>
2022-03-13 10:00:53 +0000

    working tests locally

--- torchgeo/datasets/usavars.py
+++ torchgeo/datasets/usavars.py
@@ -181,7 +181,7 @@ class USAVars(VisionDataset):
     def _load_files(self) -> List[str]:
         """Loads file names."""
         with open(os.path.join(self.root, f"{self.split}_split.txt"), "r") as f:
-            files = f.readlines()
+            files = f.read().splitlines()
         return files
 
     def _load_image(self, path: str) -> Tensor:
