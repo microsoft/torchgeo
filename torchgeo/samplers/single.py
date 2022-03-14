1a0236c83521aa5864cc0668b66cff263677c3dd
remtav <remitavon@gmail.com>
2022-03-14 15:41:52 -0400

    style and mypy fixes

--- torchgeo/samplers/single.py
+++ torchgeo/samplers/single.py
@@ -190,7 +190,7 @@ class GridGeoSampler(GeoSampler):
         .. versionchanged:: 0.3
            Added ``units`` parameter, changed default to pixel units
         """
-        super().__init__(dataset=dataset, roi=roi, stride=stride, size=size)
+        super().__init__(dataset=dataset, roi=roi)
         self.size = _to_tuple(size)
         self.stride = _to_tuple(stride)
 
@@ -211,8 +211,20 @@ class GridGeoSampler(GeoSampler):
         for hit in self.hits:
             bounds = BoundingBox(*hit.bounds)
 
-            rows = int((bounds.maxy - bounds.miny - self.size[0] + self.stride[0]) // self.stride[0]) + 1
-            cols = int((bounds.maxx - bounds.minx - self.size[1] + self.stride[1]) // self.stride[1]) + 1
+            rows = (
+                int(
+                    (bounds.maxy - bounds.miny - self.size[0] + self.stride[0])
+                    // self.stride[0]
+                )
+                + 1
+            )
+            cols = (
+                    int(
+                        (bounds.maxx - bounds.minx - self.size[1] + self.stride[1])
+                        // self.stride[1]
+                    )
+                    + 1
+            )
             self.length += rows * cols
 
     def __iter__(self) -> Iterator[BoundingBox]:
@@ -225,8 +237,20 @@ class GridGeoSampler(GeoSampler):
         for hit in self.hits:
             bounds = BoundingBox(*hit.bounds)
 
-            rows = int((bounds.maxy - bounds.miny - self.size[0] + self.stride[0]) // self.stride[0]) + 1
-            cols = int((bounds.maxx - bounds.minx - self.size[1] + self.stride[1]) // self.stride[1]) + 1
+            rows = (
+                    int(
+                        (bounds.maxy - bounds.miny - self.size[0] + self.stride[0])
+                        // self.stride[0]
+                    )
+                    + 1
+            )
+            cols = (
+                    int(
+                        (bounds.maxx - bounds.minx - self.size[1] + self.stride[1])
+                        // self.stride[1]
+                    )
+                    + 1
+            )
 
             mint = bounds.mint
             maxt = bounds.maxt
@@ -236,12 +260,16 @@ class GridGeoSampler(GeoSampler):
                 miny = bounds.miny + i * self.stride[0]
                 maxy = miny + self.size[0]
                 if maxy > bounds.maxy:
-                    last_stride_y = self.stride[0] - (miny - (bounds.maxy - self.size[0]))
+                    last_stride_y = self.stride[0] - (
+                            miny - (bounds.maxy - self.size[0])
+                    )
                     maxy = bounds.maxy
                     miny = bounds.maxy - self.size[0]
                     warnings.warn(
-                        f"Max y coordinate of bounding box reaches passed y bounds of source tile"
-                        f"Bounding box will be moved to set max y at source tile's max y. Stride will be adjusted"
+                        f"Max y coordinate of bounding box reaches passed y bounds of "
+                        f"source tile"
+                        f"Bounding box will be moved to set max y at source tile's max"
+                        f" y. Stride will be adjusted"
                         f"from {self.stride[0]:.2f} to {last_stride_y:.2f}"
                     )
 
@@ -250,12 +278,16 @@ class GridGeoSampler(GeoSampler):
                     minx = bounds.minx + j * self.stride[1]
                     maxx = minx + self.size[1]
                     if maxx > bounds.maxx:
-                        last_stride_x = self.stride[1] - (minx - (bounds.maxx - self.size[1]))
+                        last_stride_x = self.stride[1] - (
+                                minx - (bounds.maxx - self.size[1])
+                        )
                         maxx = bounds.maxx
                         minx = bounds.maxx - self.size[1]
                         warnings.warn(
-                            f"Max x coordinate of bounding box reaches passed x bounds of source tile"
-                            f"Bounding box will be moved to set max x at source tile's max x. Stride will be adjusted"
+                            f"Max x coordinate of bounding box reaches passed x bounds"
+                            f" of source tile"
+                            f"Bounding box will be moved to set max x at source tile's"
+                            f" max x. Stride will be adjusted"
                             f"from {self.stride[1]:.2f} to {last_stride_x:.2f}"
                         )
 
