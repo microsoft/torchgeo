d84e3178eafc2f20170cf008a83666bdc8d9c2a2
Adam J. Stewart <ajstewart426@gmail.com>
2022-03-29 21:39:19 -0500

    Fix PyTorch Lightning 1.6 type hints (#487)

--- torchgeo/trainers/segmentation.py
+++ torchgeo/trainers/segmentation.py
@@ -174,13 +174,13 @@ class SemanticSegmentationTask(LightningModule):
 
         if batch_idx < 10:
             try:
-                datamodule = self.trainer.datamodule  # type: ignore[attr-defined]
+                datamodule = self.trainer.datamodule  # type: ignore[union-attr]
                 batch["prediction"] = y_hat_hard
                 for key in ["image", "mask", "prediction"]:
                     batch[key] = batch[key].cpu()
                 sample = unbind_samples(batch)[0]
                 fig = datamodule.plot(sample)
-                summary_writer = self.logger.experiment
+                summary_writer = self.logger.experiment  # type: ignore[union-attr]
                 summary_writer.add_figure(
                     f"image/{batch_idx}", fig, global_step=self.global_step
                 )
