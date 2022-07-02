21721cc73ca2b60916f1e1544dac77206e993c6c
Caleb Robinson <calebrob6@gmail.com>
2022-07-02 16:37:49 +0000

    Remove datamodule tests

--- tests/trainers/test_segmentation.py
+++ tests/trainers/test_segmentation.py
@@ -13,6 +13,7 @@ from torch.nn.modules import Module
 
 from torchgeo.datamodules import (
     ChesapeakeCVPRDataModule,
+    DeepGlobeLandCoverDataModule,
     ETCI2021DataModule,
     InriaAerialImageLabelingDataModule,
     LandCoverAIDataModule,
@@ -34,6 +35,7 @@ class TestSemanticSegmentationTask:
         "name,classname",
         [
             ("chesapeake_cvpr_5", ChesapeakeCVPRDataModule),
+            ("deepglobelandcover", DeepGlobeLandCoverDataModule),
             ("etci2021", ETCI2021DataModule),
             ("inria", InriaAerialImageLabelingDataModule),
             ("landcoverai", LandCoverAIDataModule),
