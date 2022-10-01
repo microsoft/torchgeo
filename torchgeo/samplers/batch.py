90604e389386a06f6b9688643a6000e15d673b01
Adam J. Stewart <ajstewart426@gmail.com>
2022-10-01 16:35:25 -0500

    Random GeoSamplers: add default length (#755)
    * Random GeoSamplers: add default length
    
    * Document sampler utilities
    
    * Mypy fix
    
    * Fix docs and tests
    
    * Fix import order
    
    * Fix rST link
    
    * Document default length
    
    * More clarification

--- torchgeo/samplers/batch.py
+++ torchgeo/samplers/batch.py
@@ -12,7 +12,7 @@ from torch.utils.data import Sampler
 
 from ..datasets import BoundingBox, GeoDataset
 from .constants import Units
-from .utils import _to_tuple, get_random_bounding_box
+from .utils import _to_tuple, get_random_bounding_box, tile_to_chips
 
 # https://github.com/pytorch/pytorch/issues/60979
 # https://github.com/pytorch/pytorch/pull/61045
@@ -62,7 +62,8 @@ class RandomBatchGeoSampler(BatchGeoSampler):
     """Samples batches of elements from a region of interest randomly.
 
     This is particularly useful during training when you want to maximize the size of
-    the dataset and return as many random :term:`chips <chip>` as possible.
+    the dataset and return as many random :term:`chips <chip>` as possible. Note that
+    randomly sampled chips may overlap.
     """
 
     def __init__(
@@ -70,7 +71,7 @@ class RandomBatchGeoSampler(BatchGeoSampler):
         dataset: GeoDataset,
         size: Union[Tuple[float, float], float],
         batch_size: int,
-        length: int,
+        length: Optional[int] = None,
         roi: Optional[BoundingBox] = None,
         units: Units = Units.PIXELS,
     ) -> None:
@@ -88,12 +89,18 @@ class RandomBatchGeoSampler(BatchGeoSampler):
             size: dimensions of each :term:`patch`
             batch_size: number of samples per batch
             length: number of samples per epoch
+                (defaults to approximately the maximal number of non-overlapping
+                :term:`chips <chip>` of size ``size`` that could be sampled from
+                the dataset)
             roi: region of interest to sample from (minx, maxx, miny, maxy, mint, maxt)
                 (defaults to the bounds of ``dataset.index``)
             units: defines if ``size`` is in pixel or CRS units
 
         .. versionchanged:: 0.3
            Added ``units`` parameter, changed default to pixel units
+
+        .. versionchanged:: 0.4
+           ``length`` parameter is now optional, a reasonable default will be used
         """
         super().__init__(dataset, roi)
         self.size = _to_tuple(size)
@@ -102,7 +109,7 @@ class RandomBatchGeoSampler(BatchGeoSampler):
             self.size = (self.size[0] * self.res, self.size[1] * self.res)
 
         self.batch_size = batch_size
-        self.length = length
+        self.length = 0
         self.hits = []
         areas = []
         for hit in self.index.intersection(tuple(self.roi), objects=True):
@@ -111,8 +118,15 @@ class RandomBatchGeoSampler(BatchGeoSampler):
                 bounds.maxx - bounds.minx >= self.size[1]
                 and bounds.maxy - bounds.miny >= self.size[0]
             ):
+                if bounds.area > 0:
+                    rows, cols = tile_to_chips(bounds, self.size)
+                    self.length += rows * cols
+                else:
+                    self.length += 1
                 self.hits.append(hit)
                 areas.append(bounds.area)
+        if length is not None:
+            self.length = length
 
         # torch.multinomial requires float probabilities > 0
         self.areas = torch.tensor(areas, dtype=torch.float)
