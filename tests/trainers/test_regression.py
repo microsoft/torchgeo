6e4ef4ca12b0c8d6c999220979c38f1d4ac52c95
Nils Lehmann <35272119+nilsleh@users.noreply.github.com>
2022-10-14 13:59:22 -0400

    Datamodule naming TropicalCyclone (#846)
    * name change for consistency
    
    * name change for consistency
    
    * flake8 style correction
    
    * flake8 style correction
    
    * requested changes

--- tests/trainers/test_regression.py
+++ tests/trainers/test_regression.py
@@ -8,7 +8,7 @@ import pytest
 from omegaconf import OmegaConf
 from pytorch_lightning import LightningDataModule, Trainer
 
-from torchgeo.datamodules import COWCCountingDataModule, CycloneDataModule
+from torchgeo.datamodules import COWCCountingDataModule, TropicalCycloneDataModule
 from torchgeo.trainers import RegressionTask
 
 from .test_utils import RegressionTestModel
@@ -17,7 +17,10 @@ from .test_utils import RegressionTestModel
 class TestRegressionTask:
     @pytest.mark.parametrize(
         "name,classname",
-        [("cowc_counting", COWCCountingDataModule), ("cyclone", CycloneDataModule)],
+        [
+            ("cowc_counting", COWCCountingDataModule),
+            ("cyclone", TropicalCycloneDataModule),
+        ],
     )
     def test_trainer(self, name: str, classname: Type[LightningDataModule]) -> None:
         conf = OmegaConf.load(os.path.join("tests", "conf", name + ".yaml"))
@@ -47,7 +50,7 @@ class TestRegressionTask:
 
         # Instantiate datamodule
         datamodule_kwargs = conf_dict["datamodule"]
-        datamodule = CycloneDataModule(**datamodule_kwargs)
+        datamodule = TropicalCycloneDataModule(**datamodule_kwargs)
 
         # Instantiate model
         model_kwargs = conf_dict["module"]
