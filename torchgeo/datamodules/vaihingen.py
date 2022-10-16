ebcd1ae6da6e195b9422d50bda1a41c74eb1cd83
Nils Lehmann <nils.lehmann24@gmail.com>
2022-10-16 19:00:44 +0200

    requested changes to vaihingen

--- torchgeo/datamodules/vaihingen.py
+++ torchgeo/datamodules/vaihingen.py
@@ -3,16 +3,17 @@
 
 """Vaihingen datamodule."""
 
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import matplotlib.pyplot as plt
 import kornia.augmentation as K
 import pytorch_lightning as pl
 import torch
 from einops import repeat
-from torch.utils.data import DataLoader, Dataset
-from torch.utils.data._utils.collate import default_collate
+from torch.utils.data import DataLoader, Dataset, default_collate
 from torchvision.transforms import Compose
+
+from torchgeo.samplers.utils import _to_tuple
 
 from ..datasets import Vaihingen2D
 from .utils import dataset_split
@@ -30,7 +31,7 @@ class Vaihingen2DDataModule(pl.LightningDataModule):
         self,
         train_batch_size: int = 32,
         num_workers: int = 0,
-        patch_size: Tuple[int, int] = (64, 64),
+        patch_size: Union[Tuple[int, int], int] = (64, 64),
         num_patches_per_tile: int = 32,
         val_split_pct: float = 0.2,
         **kwargs: Any,
@@ -49,15 +50,16 @@ class Vaihingen2DDataModule(pl.LightningDataModule):
             **kwargs: Additional keyword arguments passed to
                 :class:`~torchgeo.datasets.Vaihingen2D`
 
-        :: versionchanged:: 0.4
+        .. versionchanged:: 0.4
             'batch_size' is renamed to 'train_batch_size', 'patch_size' and
             'num_patches_per_tile' introduced in order to randomly crop the
             variable size images during training
         """
         super().__init__()
+
         self.train_batch_size = train_batch_size
         self.num_workers = num_workers
-        self.patch_size = patch_size
+        self.patch_size = _to_tuple(patch_size)
         self.num_patches_per_tile = num_patches_per_tile
         self.val_split_pct = val_split_pct
         self.kwargs = kwargs
@@ -87,12 +89,20 @@ class Vaihingen2DDataModule(pl.LightningDataModule):
         Args:
             stage: stage to set up
 
-        :: versionchanged:: 0.4
+        .. versionchanged:: 0.4
             Add functionality to randomly crop patches from a tile during
             training and pad validation and test samples to next multiple of 32
         """
 
         def n_random_crop(sample: Dict[str, Any]) -> Dict[str, Any]:
+            """Construct 'num_patches_per_tile' random patches of input tile.
+
+            Args:
+                sample: contains image and mask tile from dataset
+
+            Returns:
+                stacked randomly cropped patches from input tile
+            """
             images, masks = [], []
             for i in range(self.num_patches_per_tile):
                 mask = repeat(sample["mask"], "h w -> t h w", t=2).float()
@@ -105,7 +115,14 @@ class Vaihingen2DDataModule(pl.LightningDataModule):
             return sample
 
         def pad_to(sample: Dict[str, Any]) -> Dict[str, Any]:
-            """Pad to next multiple of 32."""
+            """Pad image and mask to next multiple of 32.
+
+            Args:
+                sample: contains image and mask sample from dataset
+
+            Returns:
+                padded image and mask
+            """
             h, w = sample["image"].shape[1], sample["image"].shape[2]
             new_h = int(32 * ((h // 32) + 1))
             new_w = int(32 * ((w // 32) + 1))
@@ -145,12 +162,23 @@ class Vaihingen2DDataModule(pl.LightningDataModule):
         )
 
     def train_dataloader(self) -> DataLoader[Any]:
-        """Return a DataLoader for training."""
+        """Return a DataLoader for training.
+
+        Returns:
+            training dataloader
+        """
 
         def collate_wrapper(batch: List[Dict[str, Any]]) -> Dict[str, Any]:
-            r_batch: Dict[str, Any] = default_collate(  # type: ignore[no-untyped-call]
-                batch
-            )
+            """Define collate function to combine patches per tile and batch size.
+
+            Args:
+                batch: sample batch from dataloader containing image and mask
+
+            Returns:
+                sample batch where the batch dimension is
+                'train_batch_size' * 'num_patches_per_tile'
+            """
+            r_batch: Dict[str, Any] = default_collate(batch)
             r_batch["image"] = torch.flatten(r_batch["image"], 0, 1)
             r_batch["mask"] = torch.flatten(r_batch["mask"], 0, 1)
             return r_batch
@@ -164,13 +192,21 @@ class Vaihingen2DDataModule(pl.LightningDataModule):
         )
 
     def val_dataloader(self) -> DataLoader[Any]:
-        """Return a DataLoader for validation."""
+        """Return a DataLoader for validation.
+
+        Returns:
+            validation dataloader
+        """
         return DataLoader(
             self.val_dataset, batch_size=1, num_workers=self.num_workers, shuffle=False
         )
 
     def test_dataloader(self) -> DataLoader[Any]:
-        """Return a DataLoader for testing."""
+        """Return a DataLoader for testing.
+
+        Returns:
+            test dataloader
+        """
         return DataLoader(
             self.test_dataset, batch_size=1, num_workers=self.num_workers, shuffle=False
         )
