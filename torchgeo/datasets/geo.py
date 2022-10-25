4aedcec8f09b993d246eb8bf48414781ccaef246
Pablo Mandiola <pablo@spikelab.xyz>
2022-10-25 17:09:22 -0300

    add transforms to UnionDataset

--- torchgeo/datasets/geo.py
+++ torchgeo/datasets/geo.py
@@ -934,6 +934,7 @@ class UnionDataset(GeoDataset):
         collate_fn: Callable[
             [Sequence[Dict[str, Any]]], Dict[str, Any]
         ] = merge_samples,
+        transforms: Optional[Callable[[Dict[str, Any]], Dict[str, Any]]] = None,
     ) -> None:
         """Initialize a new Dataset instance.
 
@@ -941,11 +942,13 @@ class UnionDataset(GeoDataset):
             dataset1: the first dataset
             dataset2: the second dataset
             collate_fn: function used to collate samples
+            transforms: a function/transform that takes input sample and its target as
+                entry and returns a transformed version
 
         Raises:
             ValueError: if either dataset is not a :class:`GeoDataset`
         """
-        super().__init__()
+        super().__init__(transforms)
         self.datasets = [dataset1, dataset2]
         self.collate_fn = collate_fn
 
@@ -1003,7 +1006,10 @@ class UnionDataset(GeoDataset):
         samples = []
         for ds in self.datasets:
             if list(ds.index.intersection(tuple(query))):
-                samples.append(ds[query])
+                sample = ds[query]
+                if self.transforms is not None:
+                    sample = self.transforms(sample)
+                samples.append(sample)
 
         return self.collate_fn(samples)
 
