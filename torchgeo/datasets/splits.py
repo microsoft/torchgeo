6f694e8a21886db17fefbf1294be0cffde95a81c
Pablo Mandiola <pablo@spikelab.xyz>
2022-12-20 16:39:42 -0300

    refactor random_nongeo_split

--- torchgeo/datasets/splits.py
+++ torchgeo/datasets/splits.py
@@ -4,10 +4,11 @@
 """Dataset splitting utilities."""
 
 from copy import deepcopy
-from typing import Any, List, Optional, Tuple, Union
+from typing import Any, List, Optional, Sequence, Tuple, Union
 
 import numpy as np
 from rtree.index import Index, Property
+from torch import Generator, default_generator
 from torch.utils.data import Subset, TensorDataset, random_split
 
 from ..datasets import GeoDataset, NonGeoDataset
@@ -16,30 +17,20 @@ from .utils import BoundingBox
 
 def random_nongeo_split(
     dataset: Union[TensorDataset, NonGeoDataset],
-    val_pct: float,
-    test_pct: Optional[float] = None,
+    lengths: Sequence[Union[int, float]],
+    generator: Optional[Generator] = default_generator,
 ) -> List[Subset[Any]]:
-    """Split a torch Dataset into train/val/test sets.
-
-    If ``test_pct`` is not set then only train and validation splits are returned.
+    """Randomly split a NonGeoDataset into non-overlapping new NonGeoDatasets.
 
     Args:
-        dataset: dataset to be split into train/val or train/val/test subsets
-        val_pct: percentage of samples to be in validation set
-        test_pct: (Optional) percentage of samples to be in test set
+        dataset: dataset to be split
+        lengths: lengths or fractions of splits to be produced
+        generator: (optional) generator used for the random permutation
 
     Returns:
-        a list of the subset datasets. Either [train, val] or [train, val, test]
+        A list of the subset datasets.
     """
-    if test_pct is None:
-        val_length = round(len(dataset) * val_pct)
-        train_length = len(dataset) - val_length
-        return random_split(dataset, [train_length, val_length])
-    else:
-        val_length = round(len(dataset) * val_pct)
-        test_length = round(len(dataset) * test_pct)
-        train_length = len(dataset) - (val_length + test_length)
-        return random_split(dataset, [train_length, val_length, test_length])
+    return random_split(dataset, lengths, generator)
 
 
 def random_bbox_splitting(
