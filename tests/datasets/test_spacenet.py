06686efadcda1374fcf15a9efce760152ca1301a
Caleb Robinson <calebrob6@gmail.com>
2022-12-21 05:16:29 +0000

    Make sure percent normalization doesn't divide by zero

--- tests/datasets/test_spacenet.py
+++ tests/datasets/test_spacenet.py
@@ -63,7 +63,7 @@ class TestSpaceNet1:
         self, request: SubRequest, monkeypatch: MonkeyPatch, tmp_path: Path
     ) -> SpaceNet1:
         monkeypatch.setattr(radiant_mlhub.Collection, "fetch", fetch_collection)
-        test_md5 = {"sn1_AOI_1_RIO": "9b445f0bddb5de04f806119dc8ee7fbf"}
+        test_md5 = {"sn1_AOI_1_RIO": "127a523561987110f008e8c9815ce807"}
 
         # Refer https://github.com/python/mypy/issues/1032
         monkeypatch.setattr(SpaceNet1, "collection_md5_dict", test_md5)
