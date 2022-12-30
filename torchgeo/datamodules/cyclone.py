22f11b994f2344d45aba034678943a2337d11436
Adam J. Stewart <ajstewart426@gmail.com>
2022-12-30 14:42:55 -0600

    Update Cyclone

--- torchgeo/datamodules/cyclone.py
+++ torchgeo/datamodules/cyclone.py
@@ -5,12 +5,15 @@
 
 from typing import Any, Dict, Optional
 
+import matplotlib.pyplot as plt
 import pytorch_lightning as pl
-import torch
+from kornia.augmentation import Normalize
 from sklearn.model_selection import GroupShuffleSplit
+from torch import Tensor
 from torch.utils.data import DataLoader, Subset
 
 from ..datasets import TropicalCyclone
+from ..transforms import AugmentationSequential
 
 
 class TropicalCycloneDataModule(pl.LightningDataModule):
@@ -19,50 +22,33 @@ class TropicalCycloneDataModule(pl.LightningDataModule):
     Implements 80/20 train/val splits based on hurricane storm ids.
     See :func:`setup` for more details.
 
-    .. versionchanged:: 0.4.0
+    .. versionchanged:: 0.4
         Class name changed from CycloneDataModule to TropicalCycloneDataModule to be
         consistent with TropicalCyclone dataset.
     """
 
     def __init__(
-        self, seed: int = 0, batch_size: int = 64, num_workers: int = 0, **kwargs: Any
+        self, batch_size: int = 64, num_workers: int = 0, **kwargs: Any
     ) -> None:
         """Initialize a LightningDataModule for NASA Cyclone based DataLoaders.
 
         Args:
-            seed: The seed value to use when doing the sklearn based GroupShuffleSplit
             batch_size: The batch size to use in all created DataLoaders
             num_workers: The number of workers to use in all created DataLoaders
             **kwargs: Additional keyword arguments passed to
                 :class:`~torchgeo.datasets.TropicalCyclone`
         """
         super().__init__()
-        self.seed = seed
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.kwargs = kwargs
 
-    def preprocess(self, sample: Dict[str, Any]) -> Dict[str, Any]:
-        """Transform a single sample from the Dataset.
-
-        Args:
-            sample: dictionary containing image and target
-
-        Returns:
-            preprocessed sample
-        """
-        sample["image"] = sample["image"].float()
-        sample["image"] /= 255.0
-        sample["image"] = (
-            sample["image"].unsqueeze(0).repeat(3, 1, 1)
-        )  # convert from grayscale to 3 channel
-        if "label" in sample:
-            sample["label"] = torch.as_tensor(sample["label"]).float()
-
-        return sample
+        self.transform = AugmentationSequential(
+            Normalize(mean=0, std=255), data_keys=["image"]
+        )
 
     def prepare_data(self) -> None:
-        """Initialize the main ``Dataset`` objects for use in :func:`setup`.
+        """Initialize the main Dataset objects for use in :func:`setup`.
 
         This includes optionally downloading the dataset. This is done once per node,
         while :func:`setup` is done once per GPU.
@@ -73,8 +59,7 @@ class TropicalCycloneDataModule(pl.LightningDataModule):
     def setup(self, stage: Optional[str] = None) -> None:
         """Create the train/val/test splits based on the original Dataset objects.
 
-        The splits should be done here vs. in :func:`__init__` per the docs:
-        https://pytorch-lightning.readthedocs.io/en/latest/extensions/datamodules.html#setup.
+        This method is called once per GPU per run.
 
         We split samples between train/val by the ``storm_id`` property. I.e. all
         samples with the same ``storm_id`` value will be either in the train or the val
@@ -86,13 +71,7 @@ class TropicalCycloneDataModule(pl.LightningDataModule):
         Args:
             stage: stage to set up
         """
-        self.all_train_dataset = TropicalCyclone(
-            split="train", transforms=self.preprocess, **self.kwargs
-        )
-
-        self.all_test_dataset = TropicalCyclone(
-            split="test", transforms=self.preprocess, **self.kwargs
-        )
+        self.all_train_dataset = TropicalCyclone(split="train", **self.kwargs)
 
         storm_ids = []
         for item in self.all_train_dataset.collection:
@@ -100,18 +79,16 @@ class TropicalCycloneDataModule(pl.LightningDataModule):
             storm_ids.append(storm_id)
 
         train_indices, val_indices = next(
-            GroupShuffleSplit(test_size=0.2, n_splits=2, random_state=self.seed).split(
+            GroupShuffleSplit(test_size=0.2, n_splits=2).split(
                 storm_ids, groups=storm_ids
             )
         )
 
         self.train_dataset = Subset(self.all_train_dataset, train_indices)
         self.val_dataset = Subset(self.all_train_dataset, val_indices)
-        self.test_dataset = Subset(
-            self.all_test_dataset, range(len(self.all_test_dataset))
-        )
+        self.test_dataset = TropicalCyclone(split="test", **self.kwargs)
 
-    def train_dataloader(self) -> DataLoader[Any]:
+    def train_dataloader(self) -> DataLoader[Dict[str, Tensor]]:
         """Return a DataLoader for training.
 
         Returns:
@@ -124,7 +101,7 @@ class TropicalCycloneDataModule(pl.LightningDataModule):
             shuffle=True,
         )
 
-    def val_dataloader(self) -> DataLoader[Any]:
+    def val_dataloader(self) -> DataLoader[Dict[str, Tensor]]:
         """Return a DataLoader for validation.
 
         Returns:
@@ -137,7 +114,7 @@ class TropicalCycloneDataModule(pl.LightningDataModule):
             shuffle=False,
         )
 
-    def test_dataloader(self) -> DataLoader[Any]:
+    def test_dataloader(self) -> DataLoader[Dict[str, Tensor]]:
         """Return a DataLoader for testing.
 
         Returns:
@@ -149,3 +126,22 @@ class TropicalCycloneDataModule(pl.LightningDataModule):
             num_workers=self.num_workers,
             shuffle=False,
         )
+
+    def on_after_batch_transfer(
+        self, batch: Dict[str, Tensor], dataloader_idx: int
+    ) -> Dict[str, Tensor]:
+        """Apply augmentations to batch after transferring to GPU.
+
+        Args:
+            batch: A batch of data that needs to be altered or augmented
+            dataloader_idx: The index of the dataloader to which the batch belongs
+
+        Returns:
+            A batch of data
+        """
+        batch = self.transform(batch)
+        return batch
+
+    def plot(self, *args: Any, **kwargs: Any) -> plt.Figure:
+        """Run :meth:`torchgeo.datasets.TropicalCyclone.plot`."""
+        return self.test_dataset.plot(*args, **kwargs)
