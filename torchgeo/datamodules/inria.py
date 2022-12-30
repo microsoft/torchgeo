7e1d500fdca02b343b679a4ead3050436c8c7b6c
Adam J. Stewart <ajstewart426@gmail.com>
2022-12-30 16:18:48 -0600

    Update Inria

--- torchgeo/datamodules/inria.py
+++ torchgeo/datamodules/inria.py
@@ -3,31 +3,19 @@
 
 """InriaAerialImageLabeling datamodule."""
 
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, Optional, Tuple, Union
 
-import kornia.augmentation as K
 import matplotlib.pyplot as plt
 import pytorch_lightning as pl
-import torch
-import torchvision.transforms as T
-from einops import rearrange
-from kornia.contrib import compute_padding, extract_tensor_patches
-from torch.utils.data import DataLoader, Dataset
-from torch.utils.data._utils.collate import default_collate
+from kornia.augmentation import Normalize, RandomVerticalFlip, RandomHorizontalFlip
+from torch import Tensor
+from torch.utils.data import DataLoader
 
 from ..datasets import InriaAerialImageLabeling
 from ..samplers.utils import _to_tuple
+from ..transforms import AugmentationSequential
+from ..transforms.transforms import _ExtractTensorPatches, _RandomNCrop
 from .utils import dataset_split
-
-
-def collate_wrapper(batch: List[Dict[str, Any]]) -> Dict[str, Any]:
-    """Flatten wrapper."""
-    r_batch: Dict[str, Any] = default_collate(batch)  # type: ignore[no-untyped-call]
-    r_batch["image"] = torch.flatten(r_batch["image"], 0, 1)
-    if "mask" in r_batch:
-        r_batch["mask"] = torch.flatten(r_batch["mask"], 0, 1)
-
-    return r_batch
 
 
 class InriaAerialImageLabelingDataModule(pl.LightningDataModule):
@@ -39,209 +27,139 @@ class InriaAerialImageLabelingDataModule(pl.LightningDataModule):
     .. versionadded:: 0.3
     """
 
-    h, w = 5000, 5000
-
     def __init__(
         self,
-        batch_size: int = 32,
+        num_tiles_per_batch: int = 16,
+        num_patches_per_tile: int = 16,
+        patch_size: Union[Tuple[int, int], int] = 64,
         num_workers: int = 0,
         val_split_pct: float = 0.1,
         test_split_pct: float = 0.1,
-        patch_size: Union[int, Tuple[int, int]] = 512,
-        num_patches_per_tile: int = 32,
         **kwargs: Any,
     ) -> None:
-        """Initialize a LightningDataModule for InriaAerialImageLabeling.
+        """Initialize a new LightningDataModule instance.
+
+        The Inria Aerial Image Labeling dataset contains images that are too large to
+        pass directly through a model. Instead, we randomly sample patches from image
+        tiles during training and chop up image tiles into patch grids during
+        evaluation. During training, the effective batch size is equal to
+        ``num_tiles_per_batch`` x ``num_patches_per_tile``.
 
         Args:
-            batch_size: The batch size used in the train DataLoader
-                (val_batch_size == test_batch_size == 1)
+            num_tiles_per_batch: The number of image tiles to sample from during
+                training
+            num_patches_per_tile: The number of patches to randomly sample from each
+                image tile during training
+            patch_size: The size of each patch, either ``size`` or ``(height, width)``.
+                Should be a multiple of 32 for most segmentation architectures
             num_workers: The number of workers to use in all created DataLoaders
             val_split_pct: What percentage of the dataset to use as a validation set
             test_split_pct: What percentage of the dataset to use as a test set
-            patch_size: Size of random patch from image and mask (height, width)
-            num_patches_per_tile: Number of random patches per sample
             **kwargs: Additional keyword arguments passed to
                 :class:`~torchgeo.datasets.InriaAerialImageLabeling`
         """
         super().__init__()
-        self.batch_size = batch_size
+
+        self.num_tiles_per_batch = num_tiles_per_batch
+        self.num_patches_per_tile = num_patches_per_tile
+        self.patch_size = _to_tuple(patch_size)
         self.num_workers = num_workers
         self.val_split_pct = val_split_pct
         self.test_split_pct = test_split_pct
-        self.patch_size = _to_tuple(patch_size)
-        self.num_patches_per_tile = num_patches_per_tile
         self.kwargs = kwargs
 
-        self.augmentations = K.AugmentationSequential(
-            K.RandomHorizontalFlip(p=0.5),
-            K.RandomVerticalFlip(p=0.5),
-            data_keys=["input", "mask"],
+        self.train_transform = AugmentationSequential(
+            Normalize(mean=0, std=255),
+            RandomHorizontalFlip(p=0.5),
+            RandomVerticalFlip(p=0.5),
+            _RandomNCrop(self.patch_size, self.num_patches_per_tile),
+            data_keys=["image", "mask"],
         )
-        self.random_crop = K.AugmentationSequential(
-            K.RandomCrop(self.patch_size, p=1.0, keepdim=False),
-            data_keys=["input", "mask"],
+        self.test_transform = AugmentationSequential(
+            Normalize(mean=0, std=255),
+            _ExtractTensorPatches(self.patch_size),
+            data_keys=["image", "mask"],
         )
-
-    def patch_sample(self, sample: Dict[str, Any]) -> Dict[str, Any]:
-        """Extract patches from single sample."""
-        assert sample["image"].ndim == 3
-        _, h, w = sample["image"].shape
-
-        padding = compute_padding((h, w), self.patch_size)
-        sample["original_shape"] = (h, w)
-        sample["patch_shape"] = self.patch_size
-        sample["padding"] = padding
-        sample["image"] = extract_tensor_patches(
-            sample["image"].unsqueeze(0),
-            self.patch_size,
-            self.patch_size,
-            padding=padding,
-        )
-        # Needed for reconstruction of patches later
-        sample["num_patches"] = sample["image"].shape[1]
-        sample["image"] = rearrange(sample["image"], "b n c h w -> (b n) c h w")
-        return sample
-
-    def preprocess(self, sample: Dict[str, Any]) -> Dict[str, Any]:
-        """Transform a single sample from the Dataset.
-
-        Args:
-            sample: input image dictionary
-
-        Returns:
-            preprocessed sample
-        """
-        sample["image"] = sample["image"].float()
-        sample["image"] /= 255.0
-        sample["image"] = torch.clip(sample["image"], min=0.0, max=1.0)
-
-        if "mask" in sample:
-            sample["mask"] = rearrange(sample["mask"], "h w -> () h w")
-
-        return sample
-
-    def n_random_crop(self, sample: Dict[str, Any]) -> Dict[str, Any]:
-        """Get n random crops."""
-        images, masks = [], []
-        for _ in range(self.num_patches_per_tile):
-            image, mask = sample["image"], sample["mask"]
-            # RandomCrop needs image and mask to be in float
-            mask = mask.to(torch.float)
-            image, mask = self.random_crop(image, mask)
-            images.append(image.squeeze())
-            masks.append(mask.squeeze(0).long())
-        sample["image"] = torch.stack(images)  # (t,c,h,w)
-        sample["mask"] = torch.stack(masks)  # (t, 1, h, w)
-        return sample
 
     def setup(self, stage: Optional[str] = None) -> None:
         """Initialize the main ``Dataset`` objects.
 
         This method is called once per GPU per run.
         """
-        train_transforms = T.Compose([self.preprocess, self.n_random_crop])
-        test_transforms = T.Compose([self.preprocess, self.patch_sample])
-
-        self.dataset = InriaAerialImageLabeling(
-            split="train", transforms=train_transforms, **self.kwargs
+        dataset = InriaAerialImageLabeling(split="train", **self.kwargs)
+        self.train_dataset, self.val_dataset, self.test_dataset = dataset_split(
+            dataset, self.val_split_pct, self.test_split_pct
         )
+        self.predict_dataset = InriaAerialImageLabeling(split="test", **self.kwargs)
 
-        self.train_dataset: Dataset[Any]
-        self.val_dataset: Dataset[Any]
-        self.test_dataset: Dataset[Any]
+    def train_dataloader(self) -> DataLoader[Dict[str, Tensor]]:
+        """Return a DataLoader for training.
 
-        if self.val_split_pct > 0.0:
-            if self.test_split_pct > 0.0:
-                self.train_dataset, self.val_dataset, self.test_dataset = dataset_split(
-                    self.dataset,
-                    val_pct=self.val_split_pct,
-                    test_pct=self.test_split_pct,
-                )
-            else:
-                self.train_dataset, self.val_dataset = dataset_split(
-                    self.dataset, val_pct=self.val_split_pct
-                )
-                self.test_dataset = self.val_dataset
-        else:
-            self.train_dataset = self.dataset
-            self.val_dataset = self.dataset
-            self.test_dataset = self.dataset
-
-        self.predict_dataset = InriaAerialImageLabeling(
-            split="test", transforms=test_transforms, **self.kwargs
-        )
-
-    def train_dataloader(self) -> DataLoader[Any]:
-        """Return a DataLoader for training."""
+        Returns:
+            training data loader
+        """
         return DataLoader(
             self.train_dataset,
-            batch_size=self.batch_size,
+            batch_size=self.num_patches_per_tile,
             num_workers=self.num_workers,
-            collate_fn=collate_wrapper,
             shuffle=True,
         )
 
-    def val_dataloader(self) -> DataLoader[Any]:
-        """Return a DataLoader for validation."""
+    def val_dataloader(self) -> DataLoader[Dict[str, Tensor]]:
+        """Return a DataLoader for validation.
+
+        Returns:
+            validation data loader
+        """
         return DataLoader(
-            self.val_dataset,
-            batch_size=1,
-            num_workers=self.num_workers,
-            collate_fn=collate_wrapper,
-            shuffle=False,
+            self.val_dataset, batch_size=1, num_workers=self.num_workers, shuffle=False
         )
 
-    def test_dataloader(self) -> DataLoader[Any]:
-        """Return a DataLoader for testing."""
+    def test_dataloader(self) -> DataLoader[Dict[str, Tensor]]:
+        """Return a DataLoader for testing.
+
+        Returns:
+            testing data loader
+        """
         return DataLoader(
-            self.test_dataset,
-            batch_size=1,
-            num_workers=self.num_workers,
-            collate_fn=collate_wrapper,
-            shuffle=False,
+            self.test_dataset, batch_size=1, num_workers=self.num_workers, shuffle=False
         )
 
-    def predict_dataloader(self) -> DataLoader[Any]:
-        """Return a DataLoader for prediction."""
+    def predict_dataloader(self) -> DataLoader[Dict[str, Tensor]]:
+        """Return a DataLoader for prediction.
+
+        Returns:
+            prediction data loader
+        """
         return DataLoader(
             self.predict_dataset,
             batch_size=1,
             num_workers=self.num_workers,
-            collate_fn=collate_wrapper,
             shuffle=False,
         )
 
     def on_after_batch_transfer(
-        self, batch: Dict[str, Any], dataloader_idx: int
-    ) -> Dict[str, Any]:
+        self, batch: Dict[str, Tensor], dataloader_idx: int
+    ) -> Dict[str, Tensor]:
         """Apply augmentations to batch after transferring to GPU.
 
         Args:
-            batch (dict): A batch of data that needs to be altered or augmented.
-            dataloader_idx (int): The index of the dataloader to which the batch
-            belongs.
+            batch: A batch of data that needs to be altered or augmented
+            dataloader_idx: The index of the dataloader to which the batch belongs
 
         Returns:
-            dict: A batch of data
+            A batch of data
         """
-        # Training
-        if (
-            hasattr(self, "trainer")
-            and self.trainer is not None
-            and hasattr(self.trainer, "training")
-            and self.trainer.training
-            and self.augmentations is not None
-        ):
-            batch["mask"] = batch["mask"].to(torch.float)
-            batch["image"], batch["mask"] = self.augmentations(
-                batch["image"], batch["mask"]
-            )
-            batch["mask"] = batch["mask"].to(torch.long)
+        if self.trainer:
+            if self.trainer.training:
+                batch = self.train_transform(batch)
+            elif (
+                self.trainer.validating
+                or self.trainer.testing
+                or self.trainer.predicting
+            ):
+                batch = self.test_transform(batch)
 
-        # Validation
-        if "mask" in batch:
-            batch["mask"] = rearrange(batch["mask"], "b () h w -> b h w")
         return batch
 
     def plot(self, *args: Any, **kwargs: Any) -> plt.Figure:
