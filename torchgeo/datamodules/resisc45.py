128d27955d196d5bf8dd1f15d82c2838cdc43834
Adam J. Stewart <ajstewart426@gmail.com>
2022-12-30 20:59:14 -0600

    Update RESISC45

--- torchgeo/datamodules/resisc45.py
+++ torchgeo/datamodules/resisc45.py
@@ -8,11 +8,11 @@ from typing import Any, Dict, Optional
 import kornia.augmentation as K
 import matplotlib.pyplot as plt
 import pytorch_lightning as pl
-import torch
+from torch import Tensor
 from torch.utils.data import DataLoader
-from torchvision.transforms import Compose, Normalize
 
 from ..datasets import RESISC45
+from ..transforms import AugmentationSequential
 
 
 class RESISC45DataModule(pl.LightningDataModule):
@@ -21,13 +21,13 @@ class RESISC45DataModule(pl.LightningDataModule):
     Uses the train/val/test splits from the dataset.
     """
 
-    band_means = torch.tensor([0.36820969, 0.38083247, 0.34341029])
-    band_stds = torch.tensor([0.20339924, 0.18524736, 0.18455448])
+    band_means = [127.86820969, 127.88083247, 127.84341029]
+    band_stds = [51.8668062, 47.2380768, 47.0613924]
 
     def __init__(
         self, batch_size: int = 64, num_workers: int = 0, **kwargs: Any
     ) -> None:
-        """Initialize a LightningDataModule for RESISC45 based DataLoaders.
+        """Initialize a new LightningDataModule instance.
 
         Args:
             batch_size: The batch size to use in all created DataLoaders
@@ -40,63 +40,26 @@ class RESISC45DataModule(pl.LightningDataModule):
         self.num_workers = num_workers
         self.kwargs = kwargs
 
-        self.norm = Normalize(self.band_means, self.band_stds)
-
-    def on_after_batch_transfer(
-        self, batch: Dict[str, Any], batch_idx: int
-    ) -> Dict[str, Any]:
-        """Apply batch augmentations after batch is transferred to the device.
-
-        Args:
-            batch: mini-batch of data
-            batch_idx: batch index
-
-        Returns:
-            augmented mini-batch
-        """
-        if (
-            hasattr(self, "trainer")
-            and self.trainer is not None
-            and hasattr(self.trainer, "training")
-            and self.trainer.training
-        ):
-            x = batch["image"]
-
-            train_augmentations = K.AugmentationSequential(
-                K.RandomRotation(p=0.5, degrees=90),
-                K.RandomHorizontalFlip(p=0.5),
-                K.RandomVerticalFlip(p=0.5),
-                K.RandomSharpness(p=0.5),
-                K.RandomErasing(p=0.1),
-                K.ColorJitter(
-                    p=0.5,
-                    brightness=0.1,
-                    contrast=0.1,
-                    saturation=0.1,
-                    hue=0.1,
-                    silence_instantiation_warning=True,
-                ),
-                data_keys=["input"],
-            )
-            x = train_augmentations(x)
-
-            batch["image"] = x
-
-        return batch
-
-    def preprocess(self, sample: Dict[str, Any]) -> Dict[str, Any]:
-        """Transform a single sample from the Dataset.
-
-        Args:
-            sample: input image dictionary
-
-        Returns:
-            preprocessed sample
-        """
-        sample["image"] = sample["image"].float()
-        sample["image"] /= 255.0
-        sample["image"] = self.norm(sample["image"])
-        return sample
+        self.train_transform = AugmentationSequential(
+            K.Normalize(mean=self.band_means, std=self.band_stds),
+            K.RandomRotation(p=0.5, degrees=90),
+            K.RandomHorizontalFlip(p=0.5),
+            K.RandomVerticalFlip(p=0.5),
+            K.RandomSharpness(p=0.5),
+            K.RandomErasing(p=0.1),
+            K.ColorJitter(
+                p=0.5,
+                brightness=0.1,
+                contrast=0.1,
+                saturation=0.1,
+                hue=0.1,
+                silence_instantiation_warning=True,
+            ),
+            data_keys=["image"],
+        )
+        self.test_transform = AugmentationSequential(
+            K.Normalize(mean=self.band_means, std=self.band_stds), data_keys=["image"]
+        )
 
     def prepare_data(self) -> None:
         """Make sure that the dataset is downloaded.
@@ -107,20 +70,16 @@ class RESISC45DataModule(pl.LightningDataModule):
             RESISC45(**self.kwargs)
 
     def setup(self, stage: Optional[str] = None) -> None:
-        """Initialize the main ``Dataset`` objects.
+        """Initialize the main Dataset objects.
 
         This method is called once per GPU per run.
 
         Args:
             stage: stage to set up
         """
-        transforms = Compose([self.preprocess])
-
-        self.train_dataset = RESISC45(
-            split="train", transforms=transforms, **self.kwargs
-        )
-        self.val_dataset = RESISC45(split="val", transforms=transforms, **self.kwargs)
-        self.test_dataset = RESISC45(split="test", transforms=transforms, **self.kwargs)
+        self.train_dataset = RESISC45(split="train", **self.kwargs)
+        self.val_dataset = RESISC45(split="val", **self.kwargs)
+        self.test_dataset = RESISC45(split="test", **self.kwargs)
 
     def train_dataloader(self) -> DataLoader[Any]:
         """Return a DataLoader for training.
@@ -160,6 +119,26 @@ class RESISC45DataModule(pl.LightningDataModule):
             num_workers=self.num_workers,
             shuffle=False,
         )
+
+    def on_after_batch_transfer(
+        self, batch: Dict[str, Tensor], dataloader_idx: int
+    ) -> Dict[str, Tensor]:
+        """Apply augmentations to batch after transferring to GPU.
+
+        Args:
+            batch: A batch of data that needs to be altered or augmented
+            dataloader_idx: The index of the dataloader to which the batch belongs
+
+        Returns:
+            A batch of data
+        """
+        if self.trainer:
+            if self.trainer.training:
+                batch = self.train_transform(batch)
+            elif self.trainer.validating or self.trainer.testing:
+                batch = self.test_transform(batch)
+
+        return batch
 
     def plot(self, *args: Any, **kwargs: Any) -> plt.Figure:
         """Run :meth:`torchgeo.datasets.RESISC45.plot`.
