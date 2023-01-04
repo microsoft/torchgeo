56a3692b76a2e0dc363cd67291a517f6642b2515
Pablo Mandiola <pablo@spikelab.xyz>
2023-01-04 15:01:21 -0300

    better tests

--- tests/datasets/test_splits.py
+++ tests/datasets/test_splits.py
@@ -2,9 +2,11 @@
 # Licensed under the MIT License.
 
 from math import floor, isclose
+from typing import Dict, List, Sequence, Tuple, Union
 
 import pytest
 import torch
+from rasterio.crs import CRS
 from torch.utils.data import TensorDataset
 
 from torchgeo.datasets import GeoDataset
@@ -18,7 +20,24 @@ from torchgeo.datasets.splits import (
 )
 from torchgeo.datasets.utils import BoundingBox
 
-from .test_geo import CustomGeoDataset
+
+class CustomGeoDataset(GeoDataset):
+    def __init__(
+        self,
+        items: List[Tuple[BoundingBox, str]] = [(BoundingBox(0, 1, 0, 1, 0, 40), "")],
+        crs: CRS = CRS.from_epsg(3005),
+        res: float = 1,
+    ) -> None:
+        super().__init__()
+        for box, content in items:
+            self.index.insert(0, tuple(box), content)
+        self._crs = crs
+        self.res = res
+
+    def __getitem__(self, query: BoundingBox) -> Dict[str, BoundingBox]:
+        hits = self.index.intersection(tuple(query), objects=True)
+        hit = next(iter(hits))
+        return {"content": hit.object}
 
 
 def test_random_nongeo_split() -> None:
@@ -39,46 +58,58 @@ def test_random_nongeo_split() -> None:
     assert len(test_ds) == floor(num_samples / 3)
 
 
-def test_random_bbox_assignment() -> None:
-    ds = (
-        CustomGeoDataset(BoundingBox(0, 1, 0, 1, 0, 0))
-        | CustomGeoDataset(BoundingBox(1, 2, 0, 1, 0, 0))
-        | CustomGeoDataset(BoundingBox(2, 3, 0, 1, 0, 0))
-        | CustomGeoDataset(BoundingBox(3, 4, 0, 1, 0, 0))
+@pytest.mark.parametrize(
+    "lengths,expected_lengths",
+    [
+        # List of lengths
+        ([2, 1, 1], [2, 1, 1]),
+        # List of fractions (with remainder)
+        ([1 / 3, 1 / 3, 1 / 3], [2, 1, 1]),
+    ],
+)
+def test_random_bbox_assignment(
+    lengths: Sequence[Union[int, float]], expected_lengths: Sequence[int]
+) -> None:
+    ds = CustomGeoDataset(
+        [
+            (BoundingBox(0, 1, 0, 1, 0, 0), "a"),
+            (BoundingBox(1, 2, 0, 1, 0, 0), "b"),
+            (BoundingBox(2, 3, 0, 1, 0, 0), "c"),
+            (BoundingBox(3, 4, 0, 1, 0, 0), "d"),
+        ]
     )
 
-    # Test list of lengths
-    train_ds, val_ds, test_ds = random_bbox_assignment(ds, lengths=[2, 1, 1])
-    assert len(train_ds) == 2
-    assert len(val_ds) == 1
-    assert len(test_ds) == 1
+    train_ds, val_ds, test_ds = random_bbox_assignment(ds, lengths)
+
+    # Check datasets lengths
+    assert len(train_ds) == expected_lengths[0]
+    assert len(val_ds) == expected_lengths[1]
+    assert len(test_ds) == expected_lengths[2]
+
+    # No overlap
     assert len(train_ds & val_ds) == 0
     assert len(val_ds & test_ds) == 0
     assert len(test_ds & train_ds) == 0
+
+    # Union equals original
     assert (train_ds | val_ds | test_ds).bounds == ds.bounds
 
-    # Test list of fractions (with remainder)
-    train_ds, val_ds, test_ds = random_bbox_assignment(
-        ds, lengths=[1 / 3, 1 / 3, 1 / 3]
-    )
-    assert len(train_ds) == floor(len(ds) / 3) + 1
-    assert len(val_ds) == floor(len(ds) / 3)
-    assert len(test_ds) == floor(len(ds) / 3)
-    assert len(train_ds & val_ds) == 0
-    assert len(val_ds & test_ds) == 0
-    assert len(test_ds & train_ds) == 0
-    assert (train_ds | val_ds | test_ds).bounds == ds.bounds
+    # Test __get_item__
+    x = train_ds[train_ds.bounds]
+    assert isinstance(x, dict)
+    assert isinstance(x["content"], str)
 
-    # Test invalid input lengths
+
+def test_random_bbox_assignment_invalid_inputs() -> None:
     with pytest.raises(
         ValueError,
         match="Sum of input lengths must equal 1 or the length of dataset's index.",
     ):
-        random_bbox_assignment(ds, lengths=[2, 2, 1])
+        random_bbox_assignment(CustomGeoDataset(), lengths=[2, 2, 1])
     with pytest.raises(
         ValueError, match="All items in input lengths must be greater than 0."
     ):
-        random_bbox_assignment(ds, lengths=[1 / 2, 3 / 4, -1 / 4])
+        random_bbox_assignment(CustomGeoDataset(), lengths=[1 / 2, 3 / 4, -1 / 4])
 
 
 def _get_total_area(dataset: GeoDataset) -> float:
@@ -91,16 +122,17 @@ def _get_total_area(dataset: GeoDataset) -> float:
 
 
 def test_random_bbox_splitting() -> None:
-    ds = (
-        CustomGeoDataset(BoundingBox(0, 1, 0, 1, 0, 0))
-        | CustomGeoDataset(BoundingBox(1, 2, 0, 1, 0, 0))
-        | CustomGeoDataset(BoundingBox(2, 3, 0, 1, 0, 0))
-        | CustomGeoDataset(BoundingBox(3, 4, 0, 1, 0, 0))
+    ds = CustomGeoDataset(
+        [
+            (BoundingBox(0, 1, 0, 1, 0, 0), "a"),
+            (BoundingBox(1, 2, 0, 1, 0, 0), "b"),
+            (BoundingBox(2, 3, 0, 1, 0, 0), "c"),
+            (BoundingBox(3, 4, 0, 1, 0, 0), "d"),
+        ]
     )
 
     ds_area = _get_total_area(ds)
 
-    # Test list of fractions
     train_ds, val_ds, test_ds = random_bbox_splitting(
         ds, fractions=[1 / 2, 1 / 4, 1 / 4]
     )
@@ -108,14 +140,24 @@ def test_random_bbox_splitting() -> None:
     val_ds_area = _get_total_area(val_ds)
     test_ds_area = _get_total_area(test_ds)
 
+    # Check datasets areas
     assert train_ds_area == ds_area / 2
     assert val_ds_area == ds_area / 4
     assert test_ds_area == ds_area / 4
+
+    # No overlap
     assert len(train_ds & val_ds) == 0
     assert len(val_ds & test_ds) == 0
     assert len(test_ds & train_ds) == 0
-    assert isclose(_get_total_area(train_ds | val_ds | test_ds), ds_area)
+
+    # Union equals original
     assert (train_ds | val_ds | test_ds).bounds == ds.bounds
+    assert isclose(_get_total_area(train_ds | val_ds | test_ds), ds_area)
+
+    # Test __get_item__
+    x = train_ds[train_ds.bounds]
+    assert isinstance(x, dict)
+    assert isinstance(x["content"], str)
 
     # Test invalid input fractions
     with pytest.raises(ValueError, match="Sum of input fractions must equal 1."):
@@ -127,22 +169,35 @@ def test_random_bbox_splitting() -> None:
 
 
 def test_random_grid_cell_assignment() -> None:
-    ds = CustomGeoDataset(BoundingBox(0, 12, 0, 12, 0, 0)) | CustomGeoDataset(
-        BoundingBox(12, 24, 0, 12, 0, 0)
+    ds = CustomGeoDataset(
+        [
+            (BoundingBox(0, 12, 0, 12, 0, 0), "a"),
+            (BoundingBox(12, 24, 0, 12, 0, 0), "b"),
+        ]
     )
 
     train_ds, val_ds, test_ds = random_grid_cell_assignment(
         ds, fractions=[1 / 2, 1 / 4, 1 / 4], grid_size=5
     )
 
+    # Check datasets lengths
     assert len(train_ds) == 1 / 2 * 2 * 5**2 + 1
     assert len(val_ds) == floor(1 / 4 * 2 * 5**2)
     assert len(test_ds) == floor(1 / 4 * 2 * 5**2)
+
+    # No overlap
     assert len(train_ds & val_ds) == 0
     assert len(val_ds & test_ds) == 0
     assert len(test_ds & train_ds) == 0
-    assert isclose(_get_total_area(train_ds | val_ds | test_ds), _get_total_area(ds))
+
+    # Union equals original
     assert (train_ds | val_ds | test_ds).bounds == ds.bounds
+    assert isclose(_get_total_area(train_ds | val_ds | test_ds), _get_total_area(ds))
+
+    # Test __get_item__
+    x = train_ds[train_ds.bounds]
+    assert isinstance(x, dict)
+    assert isinstance(x["content"], str)
 
     # Test invalid input fractions
     with pytest.raises(ValueError, match="Sum of input fractions must equal 1."):
@@ -156,11 +211,13 @@ def test_random_grid_cell_assignment() -> None:
 
 
 def test_roi_split() -> None:
-    ds = (
-        CustomGeoDataset(BoundingBox(0, 1, 0, 1, 0, 0))
-        | CustomGeoDataset(BoundingBox(1, 2, 0, 1, 0, 0))
-        | CustomGeoDataset(BoundingBox(2, 3, 0, 1, 0, 0))
-        | CustomGeoDataset(BoundingBox(3, 4, 0, 1, 0, 0))
+    ds = CustomGeoDataset(
+        [
+            (BoundingBox(0, 1, 0, 1, 0, 0), "a"),
+            (BoundingBox(1, 2, 0, 1, 0, 0), "b"),
+            (BoundingBox(2, 3, 0, 1, 0, 0), "c"),
+            (BoundingBox(3, 4, 0, 1, 0, 0), "d"),
+        ]
     )
 
     train_ds, val_ds, test_ds = roi_split(
@@ -171,13 +228,25 @@ def test_roi_split() -> None:
             BoundingBox(3.5, 4, 0, 1, 0, 0),
         ],
     )
+
+    # Check datasets lengths
     assert len(train_ds) == 2
     assert len(val_ds) == 2
     assert len(test_ds) == 1
+
+    # No overlap
     assert len(train_ds & val_ds) == 0
     assert len(val_ds & test_ds) == 0
     assert len(test_ds & train_ds) == 0
+
+    # Union equals original
     assert (train_ds | val_ds | test_ds).bounds == ds.bounds
+    assert isclose(_get_total_area(train_ds | val_ds | test_ds), _get_total_area(ds))
+
+    # Test __get_item__
+    x = train_ds[train_ds.bounds]
+    assert isinstance(x, dict)
+    assert isinstance(x["content"], str)
 
     # Test invalid input rois
     with pytest.raises(ValueError, match="ROIs in input rois can't overlap."):
@@ -186,80 +255,82 @@ def test_roi_split() -> None:
         )
 
 
-def test_time_series_split() -> None:
-    ds = (
-        CustomGeoDataset(BoundingBox(0, 1, 0, 1, 0, 10))
-        | CustomGeoDataset(BoundingBox(0, 1, 0, 1, 10, 20))
-        | CustomGeoDataset(BoundingBox(0, 1, 0, 1, 20, 30))
-        | CustomGeoDataset(BoundingBox(0, 1, 0, 1, 30, 40))
+@pytest.mark.parametrize(
+    "lengths,expected_lengths",
+    [
+        # List of timestamps
+        ([(0, 20), (20, 35), (35, 40)], [2, 2, 1]),
+        # List of lengths
+        ([20, 15, 5], [2, 2, 1]),
+        # List of fractions (with remainder)
+        ([1 / 2, 3 / 8, 1 / 8], [2, 2, 1]),
+    ],
+)
+def test_time_series_split(
+    lengths: Sequence[Union[Tuple[int, int], int, float]],
+    expected_lengths: Sequence[int],
+) -> None:
+    ds = CustomGeoDataset(
+        [
+            (BoundingBox(0, 1, 0, 1, 0, 10), "a"),
+            (BoundingBox(0, 1, 0, 1, 10, 20), "b"),
+            (BoundingBox(0, 1, 0, 1, 20, 30), "c"),
+            (BoundingBox(0, 1, 0, 1, 30, 40), "d"),
+        ]
     )
 
-    # Test lengths input using timestamps
-    train_ds, val_ds, test_ds = time_series_split(
-        ds, lengths=[(0, 20), (20, 35), (35, 40)]
-    )
+    train_ds, val_ds, test_ds = time_series_split(ds, lengths)
 
-    assert len(train_ds) == 2
-    assert len(val_ds) == 2
-    assert len(test_ds) == 1
+    # Check datasets lengths
+    assert len(train_ds) == expected_lengths[0]
+    assert len(val_ds) == expected_lengths[1]
+    assert len(test_ds) == expected_lengths[2]
+
+    # No overlap
     assert len(train_ds & val_ds) == 0
     assert len(val_ds & test_ds) == 0
     assert len(test_ds & train_ds) == 0
+
+    # Union equals original
     assert (train_ds | val_ds | test_ds).bounds == ds.bounds
 
-    # Test lengths input using lengths
-    train_ds, val_ds, test_ds = time_series_split(ds, lengths=[20, 15, 5])
+    # Test __get_item__
+    x = train_ds[train_ds.bounds]
+    assert isinstance(x, dict)
+    assert isinstance(x["content"], str)
 
-    assert len(train_ds) == 2
-    assert len(val_ds) == 2
-    assert len(test_ds) == 1
-    assert len(train_ds & val_ds) == 0
-    assert len(val_ds & test_ds) == 0
-    assert len(test_ds & train_ds) == 0
-    assert (train_ds | val_ds | test_ds).bounds == ds.bounds
 
-    # Test lengths input using fractions
-    train_ds, val_ds, test_ds = time_series_split(ds, lengths=[1 / 2, 3 / 8, 1 / 8])
-
-    assert len(train_ds) == 2
-    assert len(val_ds) == 2
-    assert len(test_ds) == 1
-    assert len(train_ds & val_ds) == 0
-    assert len(val_ds & test_ds) == 0
-    assert len(test_ds & train_ds) == 0
-    assert (train_ds | val_ds | test_ds).bounds == ds.bounds
-
-    # Test invalid input lengths
+def test_time_series_split_invalid_input() -> None:
     with pytest.raises(
         ValueError,
         match="Pairs of timestamps in lengths must have end greater than start.",
     ):
-        time_series_split(ds, lengths=[(0, 20), (35, 20), (35, 40)])
+        time_series_split(CustomGeoDataset(), lengths=[(0, 20), (35, 20), (35, 40)])
 
     with pytest.raises(
         ValueError,
         match="Pairs of timestamps in lengths must cover dataset's time bounds.",
     ):
-        time_series_split(ds, lengths=[(0, 20), (20, 35)])
+        time_series_split(CustomGeoDataset(), lengths=[(0, 20), (20, 35)])
 
     with pytest.raises(
         ValueError,
         match="Pairs of timestamps in lengths can't be out of dataset's time bounds.",
     ):
-        time_series_split(ds, lengths=[(0, 20), (20, 45)])
+        time_series_split(CustomGeoDataset(), lengths=[(0, 20), (20, 45)])
 
     with pytest.raises(
         ValueError, match="Pairs of timestamps in lengths can't overlap."
     ):
-        time_series_split(ds, lengths=[(0, 10), (10, 20), (15, 40)])
+        time_series_split(CustomGeoDataset(), lengths=[(0, 10), (10, 20), (15, 40)])
 
     with pytest.raises(
         ValueError,
         match="Sum of input lengths must equal 1 or the dataset's time length.",
     ):
-        time_series_split(ds, lengths=[1 / 2, 1 / 2, 1 / 2])
+        time_series_split(CustomGeoDataset(), lengths=[1 / 2, 1 / 2, 1 / 2])
 
     with pytest.raises(
         ValueError, match="All items in input lengths must be greater than 0."
     ):
-        time_series_split(ds, lengths=[20, 25, -5])
+        time_series_split(CustomGeoDataset(), lengths=[20, 25, -5])
