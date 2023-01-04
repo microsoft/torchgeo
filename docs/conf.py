843d96b9834d0d77a8dd16e27186fcdc8ee2fab8
Adam J. Stewart <ajstewart426@gmail.com>
2023-01-04 13:39:16 -0600

    Fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,7 @@ needs_sphinx = "4.0"
 nitpicky = True
 nitpick_ignore = [
     # Undocumented classes
+    ("py:class", "kornia.augmentation._2d.intensity.base.IntensityAugmentationBase2D"),
     ("py:class", "segmentation_models_pytorch.base.model.SegmentationModel"),
     ("py:class", "timm.models.resnet.ResNet"),
     ("py:class", "timm.models.vision_transformer.VisionTransformer"),
