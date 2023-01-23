7c2f462a9c712c961f5bc1b75115eb64dbd25ced
Adam J. Stewart <ajstewart426@gmail.com>
2023-01-22 22:05:10 -0700

    Convert all index transforms to Kornia (#999)
    * Convert all index transforms to Kornia
    
    * Fix tests too
    
    * Fix docs build
    
    * Update tutorial
    
    * Update transforms tutorial
    
    * Kornia augs add new batch dimension
    
    * Expand dims
    
    * Fix one more set of transforms
    
    * Escape quotes
    
    * Remove unneeded transform
    
    * Kornia transforms require float images
    
    * Fix device problems
    
    Co-authored-by: Caleb Robinson <calebrob6@gmail.com>

--- tests/transforms/test_indices.py
+++ tests/transforms/test_indices.py
@@ -22,44 +22,53 @@ from torchgeo.transforms import (
     AppendRBNDVI,
     AppendSWI,
     AppendTriBandNormalizedDifferenceIndex,
+    AugmentationSequential,
 )
 
 
 @pytest.fixture
 def sample() -> Dict[str, Tensor]:
     return {
-        "image": torch.tensor([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]], dtype=torch.float),
-        "mask": torch.tensor([[0, 0, 1], [0, 1, 1], [1, 1, 1]], dtype=torch.long),
+        "image": torch.arange(3 * 4 * 4, dtype=torch.float).view(3, 4, 4),
+        "mask": torch.arange(4 * 4, dtype=torch.long).view(4, 4),
     }
 
 
 @pytest.fixture
 def batch() -> Dict[str, Tensor]:
     return {
-        "image": torch.tensor([[[[1, 2, 3], [4, 5, 6], [7, 8, 9]]]], dtype=torch.float),
-        "mask": torch.tensor([[[[0, 0, 1], [0, 1, 1], [1, 1, 1]]]], dtype=torch.long),
-        "labels": torch.tensor([[0, 1]]),
+        "image": torch.arange(2 * 3 * 4 * 4, dtype=torch.float).view(2, 3, 4, 4),
+        "mask": torch.arange(2 * 4 * 4, dtype=torch.long).view(2, 4, 4),
     }
 
 
 def test_append_index_sample(sample: Dict[str, Tensor]) -> None:
     c, h, w = sample["image"].shape
-    tr = AppendNormalizedDifferenceIndex(index_a=0, index_b=0)
-    output = tr(sample)
-    assert output["image"].shape == (c + 1, h, w)
+    aug = AugmentationSequential(
+        AppendNormalizedDifferenceIndex(index_a=0, index_b=1),
+        data_keys=["image", "mask"],
+    )
+    output = aug(sample)
+    assert output["image"].shape == (1, c + 1, h, w)
 
 
 def test_append_index_batch(batch: Dict[str, Tensor]) -> None:
     b, c, h, w = batch["image"].shape
-    tr = AppendNormalizedDifferenceIndex(index_a=0, index_b=0)
-    output = tr(batch)
+    aug = AugmentationSequential(
+        AppendNormalizedDifferenceIndex(index_a=0, index_b=1),
+        data_keys=["image", "mask"],
+    )
+    output = aug(batch)
     assert output["image"].shape == (b, c + 1, h, w)
 
 
 def test_append_triband_index_batch(batch: Dict[str, Tensor]) -> None:
     b, c, h, w = batch["image"].shape
-    tr = AppendTriBandNormalizedDifferenceIndex(index_a=0, index_b=0, index_c=0)
-    output = tr(batch)
+    aug = AugmentationSequential(
+        AppendTriBandNormalizedDifferenceIndex(index_a=0, index_b=1, index_c=2),
+        data_keys=["image", "mask"],
+    )
+    output = aug(batch)
     assert output["image"].shape == (b, c + 1, h, w)
 
 
@@ -81,9 +90,9 @@ def test_append_normalized_difference_indices(
     sample: Dict[str, Tensor], index: AppendNormalizedDifferenceIndex
 ) -> None:
     c, h, w = sample["image"].shape
-    tr = index(0, 0)
-    output = tr(sample)
-    assert output["image"].shape == (c + 1, h, w)
+    aug = AugmentationSequential(index(0, 1), data_keys=["image", "mask"])
+    output = aug(sample)
+    assert output["image"].shape == (1, c + 1, h, w)
 
 
 @pytest.mark.parametrize("index", [AppendGBNDVI, AppendGRNDVI, AppendRBNDVI])
@@ -91,6 +100,6 @@ def test_append_tri_band_normalized_difference_indices(
     sample: Dict[str, Tensor], index: AppendTriBandNormalizedDifferenceIndex
 ) -> None:
     c, h, w = sample["image"].shape
-    tr = index(0, 0, 0)
-    output = tr(sample)
-    assert output["image"].shape == (c + 1, h, w)
+    aug = AugmentationSequential(index(0, 1, 2), data_keys=["image", "mask"])
+    output = aug(sample)
+    assert output["image"].shape == (1, c + 1, h, w)
