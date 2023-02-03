f8be16c2740059b2a2d6d2cdca52fa7b4c1b098e
Julien Blanchon <julien@blanchon.cc>
2023-02-03 18:26:44 +0000

    Add gdal

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes install libgdal-dev
+echo CPLUS_INCLUDE_PATH=/usr/include/gdal >> ~/.bashrc
+echo C_INCLUDE_PATH=/usr/include/gdal >> ~/.bashrc
+pip install --upgrade pip
 pip install --user -r ./requirements/datasets.txt
 pip install --user -r ./requirements/docs.txt
 pip install --user -r ./requirements/required.txt
