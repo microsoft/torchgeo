ed3fef40d758a030947a041986158e46ef29cfca
Julien Blanchon <julien@blanchon.cc>
2023-02-03 10:54:18 +0000

    🐛 Add postCreateCommand.sh

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install --user -r ./requirements/datasets.txt
+pip install --user -r ./requirements/docs.txt
+pip install --user -r ./requirements/required.txt
+pip install --user -r ./requirements/style.txt
+pip install --user -r ./requirements/tests.txt
+pip install --user pre-commit
