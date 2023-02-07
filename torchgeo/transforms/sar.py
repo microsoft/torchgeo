1a76d9105b558ca3a47329586725c3750e8a47f5
Ritwik Gupta <ritwikg2004@live.com>
2023-02-07 01:54:49 -0800

    Add Lee and Enhanced Lee filters

--- torchgeo/transforms/sar.py
+++ torchgeo/transforms/sar.py
@@ -62,7 +62,7 @@ def _quadrature_phase_to_phase(iq: Tensor) -> Tensor:
 
 
 def slc_to_iq(src: Tensor) -> Tensor:
-    """Convert a Sentinel-1 SLC ComplexFloatTensor/Complex64 to In-phase/Quadrature components.
+    """Convert a Sentinel-1 SLC to In-phase/Quadrature components.
 
     .. versionadded:: 0.5
 
@@ -160,3 +160,96 @@ def enl(src: Tensor, window_size: int = 3) -> Tensor:
     enl_image = mean_image / torch.sqrt(var_image)
     histogram = torch.histogram(enl_image)
     return histogram.bin_edges[torch.argmax(histogram.hist)]
+
+
+def lee_filter(src: Tensor, window_size: int):
+    """Lee filter for radar speckle removal.
+
+    .. versionadded:: 0.5
+
+    Args:
+        src: input Tensor
+        window_size: kernel window size, must be odd
+
+    Returns:
+        filtered_output: the filtered SAR image in amplitude scale.
+    """
+    assert window_size % 2 == 1, "The window size must be odd"
+
+    pad_size = math.ceil((window_size / 2) - 1)
+    src_pad = F.pad(src, (pad_size, pad_size, pad_size, pad_size), mode="reflect")
+
+    mean_image = F.avg_pool2d(
+        src_pad, kernel_size=window_size, stride=1, count_include_pad=False
+    )
+    mean_sqr_image = F.avg_pool2d(
+        src_pad**2, kernel_size=window_size, stride=1, count_include_pad=False
+    )
+    var_image = mean_sqr_image - mean_image**2
+
+    variance = torch.var(src)
+
+    img_weights = var_image / (var_image + variance)
+    filtered_output = mean_image + img_weights * (src - mean_image)
+    return filtered_output
+
+
+def enhanced_lee_filter(src: Tensor, window_size: int, num_looks: int = 1):
+    """Enhanced Lee filter for radar speckle removal.
+
+    Implements the Enhanced Lee filter as defined in "Adaptive Speckle Filters
+    and Scene Heterogeneity" by Lopes, Touzi, and Nezry (1990).
+
+    .. versionadded:: 0.5
+
+    Args:
+        src: input Tensor
+        window_size: kernel window size, must be odd
+        num_looks: number of looks for the provided SAR image.
+
+    Returns:
+        filtered_output: the filtered SAR image in amplitude scale.
+    """
+    assert window_size % 2 == 1, "The window size must be odd"
+
+    Cu = 0.523 / math.sqrt(num_looks)  # 0.523 is a magic number from Lopes et. al.
+
+    pad_size = math.ceil((window_size / 2) - 1)
+    src_pad = F.pad(src, (pad_size, pad_size, pad_size, pad_size), mode="reflect")
+
+    mean_image = F.avg_pool2d(
+        src_pad, kernel_size=window_size, stride=1, count_include_pad=False
+    )
+    mean_sqr_image = F.avg_pool2d(
+        src_pad**2, kernel_size=window_size, stride=1, count_include_pad=False
+    )
+    var_image = mean_sqr_image - mean_image**2
+
+    # Lopes et. al. define C_max as the maximum coefficient of variation
+    # value over the moving window within a homogeneous area
+    Cmax = (torch.sqrt(var_image) / mean_image).max()
+
+    noise_coeff_image = (
+        torch.sqrt(var_image) / mean_image
+    )  # Using the amplitude image as input
+    thresh = noise_coeff_image.clone()
+
+    mask_le = thresh <= Cu
+    mask_ge = thresh >= Cmax
+    mask_intermediate = (Cu < thresh) & (thresh < Cmax)
+
+    # Set all values less than or equal to noise_var_coeff to 1.0
+    thresh[mask_le] = 1.0
+
+    # Set all values greater than or equal to var_max to 0.0
+    thresh[mask_ge] = 0.0
+
+    # Compute the threshold for values in between noise_var_coeff and var_max
+    intermediate = -num_looks * (thresh - Cu) / (Cmax - thresh)
+    thresh = (thresh * ~mask_intermediate) + (
+        torch.exp(intermediate) * mask_intermediate
+    )
+
+    filtered_output = (mean_image * thresh) + (src * (1.0 - thresh))
+
+    return filtered_output
