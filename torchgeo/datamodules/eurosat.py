3c37a32b98a65d9e325995e08c5c36daf00707b6
Adam J. Stewart <ajstewart426@gmail.com>
2023-02-23 14:42:47 -0600

    Add disclaimer, remove duplicate code

--- torchgeo/datamodules/eurosat.py
+++ torchgeo/datamodules/eurosat.py
@@ -10,6 +10,42 @@ import torch
 from ..datasets import EuroSAT, EuroSAT100
 from .geo import NonGeoDataModule
 
+MEAN = torch.tensor(
+    [
+        1354.40546513,
+        1118.24399958,
+        1042.92983953,
+        947.62620298,
+        1199.47283961,
+        1999.79090914,
+        2369.22292565,
+        2296.82608323,
+        732.08340178,
+        12.11327804,
+        1819.01027855,
+        1118.92391149,
+        2594.14080798,
+    ]
+)
+
+STD = torch.tensor(
+    [
+        245.71762908,
+        333.00778264,
+        395.09249139,
+        593.75055589,
+        566.4170017,
+        861.18399006,
+        1086.63139075,
+        1117.98170791,
+        404.91978886,
+        4.77584468,
+        1002.58768311,
+        761.30323499,
+        1231.58581042,
+    ]
+)
+
 
 class EuroSATDataModule(NonGeoDataModule):
     """LightningDataModule implementation for the EuroSAT dataset.
@@ -19,41 +55,8 @@ class EuroSATDataModule(NonGeoDataModule):
     .. versionadded:: 0.2
     """
 
-    mean = torch.tensor(
-        [
-            1354.40546513,
-            1118.24399958,
-            1042.92983953,
-            947.62620298,
-            1199.47283961,
-            1999.79090914,
-            2369.22292565,
-            2296.82608323,
-            732.08340178,
-            12.11327804,
-            1819.01027855,
-            1118.92391149,
-            2594.14080798,
-        ]
-    )
-
-    std = torch.tensor(
-        [
-            245.71762908,
-            333.00778264,
-            395.09249139,
-            593.75055589,
-            566.4170017,
-            861.18399006,
-            1086.63139075,
-            1117.98170791,
-            404.91978886,
-            4.77584468,
-            1002.58768311,
-            761.30323499,
-            1231.58581042,
-        ]
-    )
+    mean = MEAN
+    std = STD
 
     def __init__(
         self, batch_size: int = 64, num_workers: int = 0, **kwargs: Any
@@ -72,44 +75,13 @@ class EuroSATDataModule(NonGeoDataModule):
 class EuroSAT100DataModule(NonGeoDataModule):
     """LightningDataModule implementation for the EuroSAT100 dataset.
 
+    Intended for tutorials and demonstrations, not for benchmarking.
+
     .. versionadded:: 0.5
     """
 
-    mean = torch.tensor(
-        [
-            1354.40546513,
-            1118.24399958,
-            1042.92983953,
-            947.62620298,
-            1199.47283961,
-            1999.79090914,
-            2369.22292565,
-            2296.82608323,
-            732.08340178,
-            12.11327804,
-            1819.01027855,
-            1118.92391149,
-            2594.14080798,
-        ]
-    )
-
-    std = torch.tensor(
-        [
-            245.71762908,
-            333.00778264,
-            395.09249139,
-            593.75055589,
-            566.4170017,
-            861.18399006,
-            1086.63139075,
-            1117.98170791,
-            404.91978886,
-            4.77584468,
-            1002.58768311,
-            761.30323499,
-            1231.58581042,
-        ]
-    )
+    mean = MEAN
+    std = STD
 
     def __init__(
         self, batch_size: int = 64, num_workers: int = 0, **kwargs: Any
