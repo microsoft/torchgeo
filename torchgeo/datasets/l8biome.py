f375d84ae1c415fc4de220ce56507c45af951e63
shradhasehgal <shradha.sgl@gmail.com>
2023-03-30 00:43:49 -0500

    Fixed dataset verification and download; minor changes in variable declaration

--- torchgeo/datasets/l8biome.py
+++ torchgeo/datasets/l8biome.py
@@ -47,7 +47,7 @@ class L8Biome(RasterDataset):
     .. versionadded:: 0.5
     """
 
-    url = "https://huggingface.co/datasets/torchgeo/l8biome" # redistributed from https://landsat.usgs.gov/landsat-8-cloud-cover-assessment-validation-data
+    url = "https://huggingface.co/datasets/torchgeo/l8biome/blob/main/{}.tar.gz" # redistributed from https://landsat.usgs.gov/landsat-8-cloud-cover-assessment-validation-data
     filenames_to_md5 = {
         "barren": "bb446fda3f6af50930849bb135e99f9c",
         "forest": "21505d878abac830890ea84abddc3c46",
@@ -58,20 +58,18 @@ class L8Biome(RasterDataset):
         "water": "d143049ef64e6e681cea380dd84680e9",
         "wetlands": "bff0d51db84e26a2a8e776c83ab2d331"
     }
-
-    cmap = {
-        0: (0,0,0),
-        64: (64,64,64),
-        128: (128,128,128),
-        192: (192,192,192),
-        255: (255,255,255),
-    }
     
-    filename_glob = "LC8*_B2.TIF"
-    filename_regex = r"""
-        ^(?P<date>\d+)
-        _30m_cdls\..*$
-    """
+    cmap = {
+        0: (0, 0, 0, 0),
+        1: (97, 74, 74, 255),
+        2: (38, 115, 0, 255),
+        3: (0, 197, 255, 255),
+        4: (207, 207, 207, 255),
+    } 
+
+    
+    filename_glob = "LC*_B2.TIF"
+    targz_file_glob = "*.tar.gz"
 
     def __init__(
         self, 
@@ -103,43 +101,69 @@ class L8Biome(RasterDataset):
                 don't match
         """
         self.root = root
+        self.download = download
+        self.checksum = checksum
+
         self._verify()
 
         super().__init__(root, crs = crs, res=res, transforms = transforms, cache = cache)
     
+
     def _verify(self) -> None:
         """Verify the integrity of the dataset.
-
         Raises:
-            RuntimeError: if dataset is missing or checksum fails
+            RuntimeError: if ``download=False`` but dataset is missing or checksum fails
         """
+        # Check if the extracted files already exist
+        pathname = os.path.join(self.root, "**", self.filename_glob)
+        for fname in glob.iglob(pathname, recursive=True):
+            if not fname.endswith(".tar.gz"):
+                return
 
-        # Check if the all files of dataset exist and the MD5 matches
-        for filename in self.filenames_to_md5.keys():
-            gz_path = os.path.join(self.root, f'{filename}.tar.gz')
-            if os.path.exists(gz_path):
-                md5 = self.filenames_to_md5[filename]
-                integrity = check_integrity(gz_path, md5)
-                if not integrity:
-                    raise RuntimeError(f"Dataset not found or corrupted.")
-            else:
-                raise RuntimeError(f"Dataset not found or corrupted.")
+        # Check if the tar.gz files have already been downloaded
+        pathname = os.path.join(self.root, self.targz_file_glob)
+        if glob.glob(pathname):
+            self._extract()
+            return
+
+        # Check if the user requested to download the dataset
+        if not self.download:
+            raise RuntimeError(
+                f"Dataset not found in `root={self.root}` and `download=False`, "
+                "either specify a different `root` directory or use `download=True` "
+                "to automatically download the dataset."
+            )
+
+        # Download the dataset
+        self._download()
+        self._extract()
+
+    def _download(self) -> None:
+        """Download the dataset."""
+        for biome, md5 in self.filenames_to_md5.items():
+            download_url(
+                self.url.format(biome), self.root, md5=md5 if self.checksum else None
+            )
+
+    def _extract(self) -> None:
+        """Extract the dataset."""
+        pathname = os.path.join(self.root, self.targz_file_glob)
+        for tarfile in glob.iglob(pathname):
+            extract_archive(tarfile)
+
 
     def __getitem__(self, query: BoundingBox) -> Dict[str, Any]:
         """Retrieve image/mask and metadata indexed by query.
-        
         Args:
             query: (minx, maxx, miny, maxy, mint, maxt) coordinates to index
-        
         Returns:
             sample of image, mask and metadata at that index
-        
         Raises:
             IndexError: if query is not found in the index
         """
         hits = self.index.intersection(tuple(query), objects=True)
         img_filepaths = cast(List[str], [hit.object for hit in hits])
-        mask_filepaths = [path.replace("images", "masks") for path in img_filepaths] # need to change
+        mask_filepaths = [path.replace("images", "masks") for path in img_filepaths]
 
         if not img_filepaths:
             raise IndexError(
@@ -160,49 +184,52 @@ class L8Biome(RasterDataset):
 
         return sample
 
-    def plot(
-        self,
-        sample: Dict[str, Tensor],
-        show_titles: bool = True,
-        suptitle: Optional[str] = None,
-    ) -> plt.Figure:
-        """Plot a sample from the dataset.
+    
+    # Plotting code added as placeholder for now till I get it working. Using LandCoverAI plotting as reference.
 
-        Args:
-            sample: a sample returned by :meth:`__getitem__`
-            show_titles: flag indicating whether to show titles above each panel
-            suptitle: optional string to use as a suptitle
+    # def plot(
+    #     self,
+    #     sample: Dict[str, Tensor],
+    #     show_titles: bool = True,
+    #     suptitle: Optional[str] = None,
+    # ) -> plt.Figure:
+    #     """Plot a sample from the dataset.
 
-        Returns:
-            a matplotlib Figure with the rendered sample
-        """
-        image = np.rollaxis(sample["image"].numpy().astype("uint8").squeeze(), 0, 3)
-        mask = sample["mask"].numpy().astype("uint8").squeeze()
+    #     Args:
+    #         sample: a sample returned by :meth:`__getitem__`
+    #         show_titles: flag indicating whether to show titles above each panel
+    #         suptitle: optional string to use as a suptitle
 
-        num_panels = 2
-        showing_predictions = "prediction" in sample
-        if showing_predictions:
-            predictions = sample["prediction"].numpy()
-            num_panels += 1
+    #     Returns:
+    #         a matplotlib Figure with the rendered sample
+    #     """
+    #     image = np.rollaxis(sample["image"].numpy().astype("uint8").squeeze(), 0, 3)
+    #     mask = sample["mask"].numpy().astype("uint8").squeeze()
 
-        fig, axs = plt.subplots(1, num_panels, figsize=(num_panels * 4, 5))
-        axs[0].imshow(image)
-        axs[0].axis("off")
-        axs[1].imshow(mask, vmin=0, vmax=4, cmap=self.cmap, interpolation="none")
-        axs[1].axis("off")
-        if show_titles:
-            axs[0].set_title("Image")
-            axs[1].set_title("Mask")
+    #     num_panels = 2
+    #     showing_predictions = "prediction" in sample
+    #     if showing_predictions:
+    #         predictions = sample["prediction"].numpy()
+    #         num_panels += 1
 
-        if showing_predictions:
-            axs[2].imshow(
-                predictions, vmin=0, vmax=4, cmap=self.cmap, interpolation="none"
-            )
-            axs[2].axis("off")
-            if show_titles:
-                axs[2].set_title("Predictions")
+    #     fig, axs = plt.subplots(1, num_panels, figsize=(num_panels * 4, 5))
+    #     axs[0].imshow(image)
+    #     axs[0].axis("off")
+    #     axs[1].imshow(mask, vmin=0, vmax=4, cmap=self.cmap, interpolation="none")
+    #     axs[1].axis("off")
+    #     if show_titles:
+    #         axs[0].set_title("Image")
+    #         axs[1].set_title("Mask")
 
-        if suptitle is not None:
-            plt.suptitle(suptitle)
-        return fig
+    #     if showing_predictions:
+    #         axs[2].imshow(
+    #             predictions, vmin=0, vmax=4, cmap=self.cmap, interpolation="none"
+    #         )
+    #         axs[2].axis("off")
+    #         if show_titles:
+    #             axs[2].set_title("Predictions")
+
+    #     if suptitle is not None:
+    #         plt.suptitle(suptitle)
+    #     return fig
 
