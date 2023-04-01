38216b05c2fe390cb265d8507dae2db892aabf24
shradhasehgal <shradha.sgl@gmail.com>
2023-04-01 12:09:00 -0500

    Added script to generate artificial data in similar format as original files

---
+++ tests/data/l8biome/data.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import hashlib
+import os
+import shutil
+from typing import Dict, List, Union
+
+import numpy as np
+import rasterio
+from rasterio import Affine
+from rasterio.crs import CRS
+
+SIZE = 36
+
+np.random.seed(0)
+
+FILENAME_HIERARCHY = Union[Dict[str, "FILENAME_HIERARCHY"], List[str]]
+
+bands = [
+    "B1.TIF",
+    "B2.TIF",
+    "B3.TIF",
+    "B4.TIF",
+    "B5.TIF",
+    "B6.TIF",
+    "B7.TIF",
+    "B8.TIF",
+    "B8A.TIF",
+    "B9.TIF",
+    "B10.TIF",
+    "B11.TIF",
+    "B12.TIF",
+]
+
+barren_prefix = "LC80420082013220LGN00"
+forest_prefix = "LC80070662014234LGN00"
+
+filenames: FILENAME_HIERARCHY = {
+    "barren": {
+        barren_prefix: []
+    },
+    "forest": {
+        forest_prefix: []
+    }
+}
+
+for band in bands:
+    filenames["barren"][barren_prefix].append(f'{barren_prefix}_{band}')
+    filenames["forest"][forest_prefix].append(f'{forest_prefix}_{band}')
+
+ROOT = f"../../../../BC/{barren_prefix}"
+FILENAME = f"{barren_prefix}_B1.TIF"
+src = rasterio.open(os.path.join(ROOT, FILENAME))
+
+profile = {
+    "driver": src.driver,
+    "dtype": src.read().dtype,
+    "width": SIZE,
+    "height": SIZE,
+    "count": 1,
+    "crs": src.crs,
+    "transform": src.transform,
+}
+
+def create_file(path: str) -> None:
+    
+    if path.endswith("B8.TIF"):
+        profile["width"] = profile["height"] = SIZE * 2
+
+    Z = np.random.randn(SIZE, SIZE).astype(profile["dtype"])
+
+    with rasterio.open(path, "w", **profile) as src:
+        src.write(Z, 1)
+
+
+def create_directory(directory: str, hierarchy: FILENAME_HIERARCHY) -> None:
+    if isinstance(hierarchy, dict):
+        # Recursive case
+        for key, value in hierarchy.items():
+            path = os.path.join(directory, key)
+            os.makedirs(path, exist_ok=True)
+            create_directory(path, value)
+    else:
+        # Base case
+        for value in hierarchy:
+            path = os.path.join(directory, value)
+            create_file(path)
+
+
+if __name__ == "__main__":
+    create_directory(".", filenames)
+
+    directories = ["barren", "forest"]
+    for directory in directories:
+        filename = str(directory)
+        
+        # Create tarballs
+        shutil.make_archive(filename, "gztar", ".", directory)
+
+        # # Compute checksums
+        with open(f"{filename}.tar.gz", "rb") as f:
+            md5 = hashlib.md5(f.read()).hexdigest()
+            print(filename, md5)
---
+++ tests/datasets/test_l8biome.py
@@ -0,0 +1,70 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pytest
+import torch
+import torch.nn as nn
+from _pytest.fixtures import SubRequest
+from _pytest.monkeypatch import MonkeyPatch
+from rasterio.crs import CRS
+
+from torchgeo.datasets import BoundingBox, IntersectionDataset, L8Biome, UnionDataset
+
+class TestL8Biome:
+    @pytest.fixture(
+        params=[
+            ["SR_B1", "SR_B2", "SR_B3", "SR_B4", "SR_B5", "SR_B6", "SR_B7"],
+            ["SR_B4", "SR_B3", "SR_B2", "SR_QA_AEROSOL"],
+        ]
+    )
+    def dataset(self, monkeypatch: MonkeyPatch, request: SubRequest) -> L8Biome:
+        root = os.path.join("tests", "data", "l8biome")
+        bands = request.param
+        transforms = nn.Identity()
+        return L8Biome(root, bands=bands, transforms=transforms)
+
+    def test_separate_files(self, dataset: L8Biome) -> None:
+        assert dataset.index.count(dataset.index.bounds) == 1
+
+    def test_getitem(self, dataset: L8Biome) -> None:
+        x = dataset[dataset.bounds]
+        assert isinstance(x, dict)
+        assert isinstance(x["crs"], CRS)
+        assert isinstance(x["image"], torch.Tensor)
+
+    def test_and(self, dataset: L8Biome) -> None:
+        ds = dataset & dataset
+        assert isinstance(ds, IntersectionDataset)
+
+    def test_or(self, dataset: L8Biome) -> None:
+        ds = dataset | dataset
+        assert isinstance(ds, UnionDataset)
+
+    def test_plot(self, dataset: L8Biome) -> None:
+        x = dataset[dataset.bounds]
+        dataset.plot(x, suptitle="Test")
+        plt.close()
+
+    def test_plot_wrong_bands(self, dataset: L8Biome) -> None:
+        bands = ("SR_B1",)
+        ds = L8Biome(root=dataset.root, bands=bands)
+        x = dataset[dataset.bounds]
+        with pytest.raises(
+            ValueError, match="Dataset doesn't contain some of the RGB bands"
+        ):
+            ds.plot(x)
+
+    def test_no_data(self, tmp_path: Path) -> None:
+        with pytest.raises(FileNotFoundError, match="No L8Biome data was found in "):
+            L8Biome(str(tmp_path))
+
+    def test_invalid_query(self, dataset: L8Biome) -> None:
+        query = BoundingBox(0, 0, 0, 0, 0, 0)
+        with pytest.raises(
+            IndexError, match="query: .* not found in index with bounds:"
+        ):
+            dataset[query]
