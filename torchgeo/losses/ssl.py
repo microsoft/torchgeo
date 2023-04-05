def7b428879fe2b416ff13aa7931ab927c5ec648
Caleb Robinson <calebrob6@gmail.com>
2023-04-05 09:25:06 -0700

    Update torchgeo/losses/ssl.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- torchgeo/losses/ssl.py
+++ torchgeo/losses/ssl.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-"""Loss functions self-supervised learning."""
+"""Loss functions for self-supervised learning."""
 
 import torch
 import torch.nn.functional as F
