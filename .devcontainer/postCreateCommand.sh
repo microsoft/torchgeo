4017d2be6dacf74a1564a5cdd53d58c70d21d100
Julien Blanchon <julien@blanchon.cc>
2023-04-11 22:55:14 +0000

    Switch to .[dataset ...]

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,9 +5,5 @@ sudo apt-get -y --force-yes install libgdal-dev
 echo CPLUS_INCLUDE_PATH=/usr/include/gdal >> ~/.bashrc
 echo C_INCLUDE_PATH=/usr/include/gdal >> ~/.bashrc
 pip install --upgrade pip
-pip install --user -r ./requirements/datasets.txt
-pip install --user -r ./requirements/docs.txt
-pip install --user -r ./requirements/required.txt
-pip install --user -r ./requirements/style.txt
-pip install --user -r ./requirements/tests.txt
+yes | pip install .[datasets,docs,style,tests]
 pip install --user pre-commit
