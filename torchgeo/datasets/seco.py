7d3e16b2d8944e8b300a00530c22e54e5922c0a7
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2023-04-14 14:39:00 -0500

    SeCo newer version bug fix (#1235)

--- torchgeo/datasets/seco.py
+++ torchgeo/datasets/seco.py
@@ -126,7 +126,7 @@ class SeasonalContrastS2(NonGeoDataset):
         root = os.path.join(
             self.root, self.metadata[self.version]["directory"], f"{index:06}"
         )
-        subdirs = os.listdir(root)
+        subdirs = [f for f in os.listdir(root) if os.path.isdir(os.path.join(root, f))]
         subdirs = random.sample(subdirs, self.seasons)
 
         images = [self._load_patch(root, subdir) for subdir in subdirs]
