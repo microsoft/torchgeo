74aef3eb9ad5e57646c3dc90c949322d5c251f2a
Caleb Robinson <calebrob6@gmail.com>
2023-04-15 21:41:28 +0000

    Update to plotting script and data

--- experiments/plot_landsat_bands.py
+++ experiments/plot_landsat_bands.py
@@ -3,39 +3,62 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import json
-
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # https://www.usgs.gov/faqs/what-are-band-designations-landsat-satellites
-with open("landsat_non_thermal.json") as f:
-    non_thermal = json.load(f)
-with open("landsat_thermal.json") as f:
-    thermal = json.load(f)
+df = pd.read_csv("band_data.csv")
+
+bar_height = 5
+# This dictionary maps a sensor and resolution to a y location on the plot
+bar_to_height_map = {
+    ("tm", 120): 50,
+    ("etm", 60): 34,
+    ("tirs", 100): 12,
+    ("mss", 60): 60,
+    ("tm", 30): 44,
+    ("etm", 30): 28,
+    ("etm", 15): 22,
+    ("oli", 30): 6,
+    ("oli", 15): 0,
+}
 
 fig, (ax1, ax2) = plt.subplots(1, 2, gridspec_kw={"width_ratios": [4, 1]})
 fig.subplots_adjust(wspace=0.05)
 
-# Plot bands
-for sensor, value1 in non_thermal.items():
-    for res, value2 in value1.items():
-        ax1.broken_barh(
-            value2["xranges"],
-            value2["yrange"],
-            edgecolor="k",
-            facecolors=value2["facecolors"],
-            linewidth=0.5,
-        )
+for sensor_name, group1 in df.groupby("Sensor name"):
+    for resolution, group2 in group1.groupby("Resolution (m)"):
 
-for sensor, value1 in thermal.items():
-    for res, value2 in value1.items():
-        ax2.broken_barh(
-            value2["xranges"],
-            value2["yrange"],
-            edgecolor="k",
-            facecolors=value2["facecolors"],
-            linewidth=0.5,
-        )
+        # Plot one row of data
+        y_position = bar_to_height_map[(sensor_name, resolution)]
+
+        for i in range(group2.shape[0]):
+            row = group2.iloc[i]
+            wavelength_start = row["Wavelength start (μm)"]
+            wavelength_width = row["Wavelength width"]
+            color = row["Color"]
+            band_number = row["Band number"]
+
+            # We've split the plot into two parts as the thermal bands are > 10μm
+            # while the other bands are < 3μm
+            if wavelength_start < 10:
+                ax1.broken_barh(
+                    [[wavelength_start, wavelength_width]],
+                    [y_position, bar_height],
+                    edgecolor="k",
+                    facecolors=color,
+                    linewidth=0.5,
+                )
+                ax1.text(wavelength_start, y_position + 1.5, str(band_number))
+            else:
+                ax2.broken_barh(
+                    [[wavelength_start, wavelength_width]],
+                    [y_position, bar_height],
+                    edgecolor="k",
+                    facecolors=color,
+                    linewidth=0.5,
+                )
+                ax2.text(wavelength_start, y_position + 1.5, str(band_number))
 
 # Labels
 fig.supxlabel("Wavelength (μm)")
