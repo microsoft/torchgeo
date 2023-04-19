042172839bcf8dfcb43838046f28dd91b00b940b
Adam J. Stewart <ajstewart426@gmail.com>
2023-04-19 17:03:56 -0500

    Increase coverage

--- tests/trainers/test_simclr.py
+++ tests/trainers/test_simclr.py
@@ -83,6 +83,16 @@ class TestSimCLRTask:
         )
         trainer.fit(model=model, datamodule=datamodule)
 
+    def test_version_warnings(self):
+        with pytest.warnings(UserWarning, match="SimCLR v1 only uses 2 layers"):
+            SimCLRTask(version=1, layers=3)
+        with pytest.warnings(UserWarning, match="SimCLR v1 does not use a memory bank"):
+            SimCLRTask(version=1, memory_bank=10)
+        with pytest.warnings(UserWarning, match="SimCLR v2 uses 3+ layers"):
+            SimCLRTask(version=2, layers=2)
+        with pytest.warnings(UserWarning, match="SimCLR v2 uses a memory bank"):
+            SimCLRTask(version=2, memory_bank=0)
+
     @pytest.fixture(
         params=[
             weights for model in list_models() for weights in get_model_weights(model)
