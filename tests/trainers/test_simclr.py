1e90095e8e4333d3b43a7b1561659758e0b591c1
Adam J. Stewart <ajstewart426@gmail.com>
2023-04-19 17:28:07 -0500

    Escape regex

--- tests/trainers/test_simclr.py
+++ tests/trainers/test_simclr.py
@@ -88,7 +88,7 @@ class TestSimCLRTask:
             SimCLRTask(version=1, layers=3)
         with pytest.warns(UserWarning, match="SimCLR v1 does not use a memory bank"):
             SimCLRTask(version=1, memory_bank_size=10)
-        with pytest.warns(UserWarning, match="SimCLR v2 uses 3+ layers"):
+        with pytest.warns(UserWarning, match=r"SimCLR v2 uses 3\+ layers"):
             SimCLRTask(version=2, layers=2)
         with pytest.warns(UserWarning, match="SimCLR v2 uses a memory bank"):
             SimCLRTask(version=2, memory_bank_size=0)
