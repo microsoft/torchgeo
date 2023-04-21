0942fd2e1104860c0574af2726b39d2b681ad1f8
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2023-04-21 21:00:50 +0000

    rename

--- torchgeo/trainers/__init__.py
+++ torchgeo/trainers/__init__.py
@@ -6,15 +6,15 @@
 from .byol import BYOLTask
 from .classification import ClassificationTask, MultiLabelClassificationTask
 from .detection import ObjectDetectionTask
-from .regression import DenseRegressionTask, RegressionTask
+from .regression import PixelwiseRegressionTask, RegressionTask
 from .segmentation import SemanticSegmentationTask
 
 __all__ = (
     "BYOLTask",
     "ClassificationTask",
-    "DenseRegressionTask",
     "MultiLabelClassificationTask",
     "ObjectDetectionTask",
+    "PixelwiseRegressionTask",
     "RegressionTask",
     "SemanticSegmentationTask",
 )
