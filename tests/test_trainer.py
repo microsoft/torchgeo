0849400033b37a8c6b320569b8b420fde2247b09
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2023-04-23 03:31:46 +0000

    remove hydra recursive test

--- tests/test_trainer.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from hydra.utils import instantiate
-from omegaconf import OmegaConf
-
-
-def test_recursive_config() -> None:
-    conf = OmegaConf.load(os.path.join("tests", "conf", "trainer.yaml"))
-    instantiate(conf).trainer
