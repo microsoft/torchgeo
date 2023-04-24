09dccbda8ac83e3cec893795c20573ffe343a35e
Adam J. Stewart <ajstewart426@gmail.com>
2023-04-23 22:58:35 -0500

    pyvista 0.29 confirmed as min version

--- tests/datasets/test_idtrees.py
+++ tests/datasets/test_idtrees.py
@@ -140,7 +140,7 @@ class TestIDTReeS:
             plt.close()
 
     def test_plot_las(self, dataset: IDTReeS) -> None:
-        pyvista = pytest.importorskip("pyvista", minversion="0.25.2")
+        pyvista = pytest.importorskip("pyvista", minversion="0.29")
 
         # Test point cloud without colors
         point_cloud = dataset.plot_las(index=0)
