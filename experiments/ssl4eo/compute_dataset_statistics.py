11ffc676fbf3741db4b57280e35428ff163efa86
Adam J. Stewart <ajstewart426@gmail.com>
2023-05-04 16:19:19 -0500

    Add multithreading

--- experiments/ssl4eo/compute_dataset_statistics.py
+++ experiments/ssl4eo/compute_dataset_statistics.py
@@ -3,6 +3,7 @@
 import argparse
 import glob
 import os
+from multiprocessing.dummy import Pool
 
 import numpy as np
 import rasterio as rio
@@ -12,20 +13,46 @@ if __name__ == "__main__":
     parser.add_argument("directory", help="directory to recursively search for files")
     parser.add_argument("--ext", default="tif", help="file extension")
     parser.add_argument("--nan", type=float, default=0, help="fill value")
+    parser.add_argument(
+        "--num-workers", type=int, default=10, help="number of parallel threads"
+    )
     args = parser.parse_args()
 
-    s0 = 0
-    s1 = 0
-    s2 = 0
-    for path in glob.iglob(
-        os.path.join(args.directory, "**", f"*.{args.ext}"), recursive=True
-    ):
+    def calculate(path: str) -> tuple[int, float, float]:
+        """Compute the count, sum, and sum of squares of an image.
+
+        Args:
+            path: Path to an image file.
+
+        Returns:
+            Count, sum, and sum of squares.
+        """
         with rio.open(path) as f:
             x = f.read()
             x[x == args.nan] = np.NAN
-            s0 += np.count_nonzero(~np.isnan(x), axis=(1, 2))
-            s1 += np.nansum(x, axis=(1, 2))
-            s2 += np.nansum(x**2, axis=(1, 2))
+            s0 = np.count_nonzero(~np.isnan(x), axis=(1, 2))
+            s1 = np.nansum(x, axis=(1, 2))
+            s2 = np.nansum(x**2, axis=(1, 2))
+        return s0, s1, s2
+
+    paths = glob.iglob(
+        os.path.join(args.directory, "**", f"*.{args.ext}"), recursive=True
+    )
+
+    if args.num_workers > 0:
+        with Pool(args.num_workers) as p:
+            out = p.imap_unordered(calculate, paths, 10)
+            s0s, s1s, s2s = zip(*out)
+        s0 = sum(s0s)
+        s1 = sum(s1s)
+        s2 = sum(s2s)
+    else:
+        s0 = s1 = s2 = 0
+        for path in paths:
+            s = calculate(path)
+            s0 += s[0]
+            s1 += s[1]
+            s2 += s[2]
 
     # https://en.wikipedia.org/wiki/Standard_deviation#Rapid_calculation_methods
     m = s1 / s0
