29e6d377342ed60afa10a80f457ec9b8835cf72d
Yi-Chia Chang <yichia3@illinois.edu>
2023-05-08 14:46:33 -0500

    Test CPUs and Mem

--- test_ds.py
+++ test_ds.py
@@ -7,36 +7,32 @@ from torchgeo.datamodules.geo import GeoDataModule
 
 from torch.utils.data import DataLoader, Dataset, default_collate
 
-ds1 = L7Irish(
-    root = "/projects/dali/data/l7irish",
-    crs = "EPSG:3857"
-)
+#ds1 = L7Irish(
+#    root = "/projects/dali/data/l7irish",
+#    crs = "EPSG:3857"
+#)
 ds2 = L7Irish(
     root = "/projects/dali/data/l7irish_cog",
     crs = "EPSG:3857"
 )
-ds3 = L7Irish(
-    root = "/projects/dali/data/l7irish_trans",
-    crs = "EPSG:3857"
-)
-ds4 = L7Irish(
-    root = "/projects/dali/data/l7irish_trans2",
-    crs = "EPSG:3857"
-)
+#ds3 = L7Irish(
+#    root = "/projects/dali/data/l7irish_trans",
+#    crs = "EPSG:3857"
+#)
+#ds4 = L7Irish(
+#    root = "/projects/dali/data/l7irish_trans2",
+#    crs = "EPSG:3857"
+#)
 # dss = [ds1, ds2, ds3, ds4]
-dss = [ds2]
+#dss = [ds2]
 # avg_time = []
 
 if __name__ ==  '__main__':
-    for ds in dss:
-        print("start loading ds2...")
-        start_time = time.time()
-        # for i in range(5):
-        sampler = RandomBatchGeoSampler(ds, size=512, batch_size=32)
-        dl = DataLoader(ds, batch_sampler=sampler, num_workers=20, collate_fn=stack_samples)
-        for batch in tqdm(dl):
-            pass
-        # avg_time.append((time.time()-start_time)/5)
-    # print("Average time [original, cog, trans, trans2]: ", avg_time)
+    print("start loading COGs...")
+    start_time = time.time()
+    sampler = RandomBatchGeoSampler(ds2, size=512, batch_size=32)
+    dl = DataLoader(ds2, batch_sampler=sampler, num_workers=40, collate_fn=stack_samples)
+    for batch in tqdm(dl):
+        pass
     total_time = time.time() - start_time
     print("Dataloader for l7irish", total_time)
--- test_ds.sh
+++ test_ds.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 #SBATCH --time=02:00:00
-#SBATCH --mem=64G
+#SBATCH --mem=256G
 #SBATCH --job-name=testds
 #SBATCH --partition=dali
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=120
 #SBATCH --gres=gpu:A100:1
 
 . /projects/dali/spack/share/spack/setup-env.sh
