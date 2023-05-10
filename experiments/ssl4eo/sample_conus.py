61ba517fda16ab463a84224c71b21908078f292a
Nils Lehmann <nils.lehmann24@gmail.com>
2023-05-10 11:29:20 +0200

    adapt sampling script to suggestions

--- experiments/ssl4eo/sample_conus.py
+++ experiments/ssl4eo/sample_conus.py
@@ -11,14 +11,14 @@ import random
 import fiona
 from rtree import index
 from sample_ssl4eo import create_bbox, km2deg
-from shapely.geometry import Point, shape
+from shapely.geometry import MultiPolygon, Point, shape
 from torchvision.datasets.utils import download_and_extract_archive
+from tqdm import tqdm
 
 
-def get_uniform_points_within_conus(
-    download_root: str, num_samples: int, bbox_size_degree: float
-) -> list[tuple[float, float]]:
-    random.seed(0)
+def retrieve_rois_polygons(
+    download_root: str,
+) -> tuple[MultiPolygon, list[MultiPolygon]]:
     nation_url = (
         "https://www2.census.gov/geo/tiger/GENZ2022/shp/cb_2022_us_nation_5m.zip"
     )
@@ -29,7 +29,7 @@ def get_uniform_points_within_conus(
     state_filename = "cb_2018_us_state_5m.shp"
     download_and_extract_archive(state_url, download_root)
 
-    exclude_states = [
+    excluded_states = [
         "United States Virgin Islands",
         "Commonwealth of the Northern Mariana Island",
         "Puerto Rico",
@@ -42,37 +42,12 @@ def get_uniform_points_within_conus(
         excluded = []
         for feature in shapefile:
             name = feature["properties"]["NAME"]
-            if name in exclude_states:
+            if name in excluded_states:
                 excluded.append(shape(feature["geometry"]))
 
-    rtree_coords = index.Index()
     with fiona.open(os.path.join(download_root, nation_filename), "r") as shapefile:
         conus = shape(shapefile[0]["geometry"])
-        x_min, y_min, x_max, y_max = conus.bounds
-        points: list[tuple[float, float]] = []
-        while len(points) < num_samples:
-            x = random.uniform(x_min, x_max)
-            y = random.uniform(y_min, y_max)
-            point = Point(x, y)
-            if conus.contains(point) and not any(
-                [polygon.contains(point) for polygon in excluded]
-            ):
-                bbox = create_bbox((x, y), bbox_size_degree)
-                if list(rtree_coords.intersection(bbox)):
-                    continue
-                else:
-                    rtree_coords.insert(len(points), bbox)
-                    points.append((x, y))
-
-    return points
-
-
-def save_csv(points: list[tuple[float, float]], ext_path: str) -> None:
-    with open(ext_path, "w") as f:
-        writer = csv.writer(f)
-        for idx, (lng, lat) in enumerate(points):
-            data = [idx, lng, lat]
-            writer.writerow(data)
+    return conus, excluded
 
 
 if __name__ == "__main__":
@@ -81,13 +56,17 @@ if __name__ == "__main__":
         "--save-path", type=str, default="./data/", help="dir to save data"
     )
     parser.add_argument(
-        "--num-samples",
-        type=int,
-        default=1000,
-        help="number of uniform samples to draw from conus",
+        "--size", type=float, default=1320, help="half patch size in meters"
     )
     parser.add_argument(
-        "--size", type=float, default=1320, help="half patch size in meters"
+        "--indices-range",
+        type=int,
+        nargs=2,
+        default=[0, 500],
+        help="indices to download",
+    )
+    parser.add_argument(
+        "--resume", action="store_true", help="resume from a previous run"
     )
 
     args = parser.parse_args()
@@ -97,5 +76,46 @@ if __name__ == "__main__":
     bbox_size_degree = km2deg(bbox_size)
 
     root = os.path.join(args.save_path, "conus")
-    points = get_uniform_points_within_conus(root, args.num_samples, bbox_size_degree)
-    save_csv(points, os.path.join(root, "sampled_locations.csv"))
+    csv_path = os.path.join(args.save_path, "sampled_locations.csv")
+
+    # Populate R-tree if resuming
+    rtree_coords = index.Index()
+    if args.resume:
+        print("Loading existing locations...")
+        with open(csv_path) as csv_file:
+            reader = csv.reader(csv_file)
+            for i, row in enumerate(reader):
+                key = int(row[0])
+                val1 = float(row[1])
+                val2 = float(row[2])
+                bbox = create_bbox((val1, val2), bbox_size_degree)
+                rtree_coords.insert(i, bbox)
+        assert key < args.indices_range[0]
+    else:
+        if os.path.exists(csv_path):
+            os.remove(csv_path)
+
+    # Retrieve Area of interest and states to ignore
+    conus, excluded = retrieve_rois_polygons(root)
+    x_min, y_min, x_max, y_max = conus.bounds
+
+    with open(csv_path, "a") as f:
+        writer = csv.writer(f)
+        for idx in tqdm(range(*args.indices_range)):
+            count = 0
+            while count < 1:
+                x = random.uniform(x_min, x_max)
+                y = random.uniform(y_min, y_max)
+                point = Point(x, y)
+                if conus.contains(point) and not any(
+                    [polygon.contains(point) for polygon in excluded]
+                ):
+                    bbox = create_bbox((x, y), bbox_size_degree)
+                    if list(rtree_coords.intersection(bbox)):
+                        continue
+                    else:
+                        rtree_coords.insert(idx, bbox)
+                        count += 1
+                        # write to file
+                        data = [idx, x, y]
+                        writer.writerow(data)
