ee83aea96988eb9371d7299726613065e725874f
Adam J. Stewart <ajstewart426@gmail.com>
2023-05-11 18:27:58 -0500

    Add scale parameter

--- experiments/ssl4eo/compute_dataset_pca.py
+++ experiments/ssl4eo/compute_dataset_pca.py
@@ -4,7 +4,6 @@ import argparse
 import glob
 import os
 
-# import matplotlib.pyplot as plt
 import numpy as np
 import rasterio as rio
 from sklearn.decomposition import IncrementalPCA
@@ -14,12 +13,7 @@ if __name__ == "__main__":
     parser.add_argument("directory", help="directory to recursively search for files")
     parser.add_argument("--ext", default="tif", help="file extension")
     parser.add_argument("--nan", default=0, type=float, help="fill value")
-    # parser.add_argument(
-    #     "--mean", nargs="*", type=float, help="mean for normalization"
-    # )
-    # parser.add_argument(
-    #     "--std", nargs="*", type=float, help="std dev for normalization"
-    # )
+    parser.add_argument("--scale", default=255, type=float, help="scale factor")
     args = parser.parse_args()
 
     transformer = IncrementalPCA(n_components=1)
@@ -27,19 +21,10 @@ if __name__ == "__main__":
         os.path.join(args.directory, "**", f"*.{args.ext}"), recursive=True
     ):
         with rio.open(path) as f:
-            x = f.read()
+            x = f.read().astype(np.float32)
+            x /= args.scale
             x = np.transpose(x, (1, 2, 0))
             x = x.reshape((-1, x.shape[-1]))
             transformer.partial_fit(x)
 
     print("pca:", transformer.components_)
-
-    # for path in glob.iglob(
-    #     os.path.join(args.directory, "**", f"*.{args.ext}"), recursive=True
-    # ):
-    #     with rio.open(path) as f:
-    #         x = f.read()
-    #         gray = x * np.expand_dims(transformer.components_.flatten(), axis=(1, 2))
-    #         gray = np.sum(gray, axis=0)
-    #         plt.imshow(gray, cmap="gray")
-    #         plt.show()
