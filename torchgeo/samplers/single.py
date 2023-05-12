d30cb4995dd3ddb208fed5842c14db15853553b2
Adam J. Stewart <ajstewart426@gmail.com>
2023-05-12 10:59:22 -0500

    GridGeoSampler: don't change stride of last patch

--- torchgeo/samplers/single.py
+++ torchgeo/samplers/single.py
@@ -169,9 +169,6 @@ class GridGeoSampler(GeoSampler):
     The overlap between each chip (``chip_size - stride``) should be approximately equal
     to the `receptive field <https://distill.pub/2019/computing-receptive-fields/>`_ of
     the CNN.
-
-    Note that the stride of the final set of chips in each row/column may be adjusted so
-    that the entire :term:`tile` is sampled without exceeding the bounds of the dataset.
     """
 
     def __init__(
@@ -242,17 +239,11 @@ class GridGeoSampler(GeoSampler):
             for i in range(rows):
                 miny = bounds.miny + i * self.stride[0]
                 maxy = miny + self.size[0]
-                if maxy > bounds.maxy:
-                    maxy = bounds.maxy
-                    miny = bounds.maxy - self.size[0]
 
                 # For each column...
                 for j in range(cols):
                     minx = bounds.minx + j * self.stride[1]
                     maxx = minx + self.size[1]
-                    if maxx > bounds.maxx:
-                        maxx = bounds.maxx
-                        minx = bounds.maxx - self.size[1]
 
                     yield BoundingBox(minx, maxx, miny, maxy, mint, maxt)
 
