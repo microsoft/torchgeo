777b68376f56638e77d74ca73af733f4559c5cec
Adam J. Stewart <ajstewart426@gmail.com>
2023-05-14 11:01:31 -0500

    Add script to compress dataset files (#1326)
    * Add script to compress dataset files
    
    * Fix type annotation
    
    * Add script for L5 L1

---
+++ experiments/ssl4eo/compress_l8_l2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# User-specific parameters
+ROOT_DIR=data
+SRC_DIR="$ROOT_DIR/ssl4eo-l8-l2"
+DST_DIR="$ROOT_DIR/ssl4eo-l8-l2-v2"
+NUM_WORKERS=40
+
+# Satellite-specific parameters
+# https://www.usgs.gov/faqs/how-do-i-use-scale-factor-landsat-level-2-science-products
+# https://developers.google.com/earth-engine/datasets/catalog/LANDSAT_LC08_C02_T1_L2
+R_MIN=$(echo "(0   + 0.2) / 0.0000275" | bc -l)
+R_MAX=$(echo "(0.3 + 0.2) / 0.0000275" | bc -l)
+
+MIN=$R_MIN
+MAX=$R_MAX
+
+# Generic parameters
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+time python3 "$SCRIPT_DIR/compress_dataset.py" \
+    "$SRC_DIR" \
+    "$DST_DIR" \
+    --min ${MIN[@]} \
+    --max ${MAX[@]} \
+    --num-workers $NUM_WORKERS
