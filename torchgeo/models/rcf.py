cc84cadf510290066a71bdef6223013ce4375802
Caleb Robinson <calebrob6@gmail.com>
2023-05-15 20:44:04 +0000

    Initial commit

--- torchgeo/models/rcf.py
+++ torchgeo/models/rcf.py
@@ -3,12 +3,14 @@
 
 """Implementation of a random convolutional feature projection model."""
 
-from typing import Optional
+from typing import Any, Optional
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import Tensor
 from torch.nn.modules import Module
+from torch.utils.data import Dataset
 
 
 class RCF(Module):
@@ -104,3 +106,98 @@ class RCF(Module):
             assert len(x1a.shape) == 2
             output = torch.cat((x1a, x1b), dim=1)
             return output
+
+
+class MOSAIKS(RCF):
+    def _normalize(
+        self, patches: np.ndarray, min_divisor: float = 1e-8, zca_bias: float = 0.001
+    ) -> np.ndarray:
+        """Does ZCA whitening on a set of input patches.
+
+        Copied fromhttps://github.com/Global-Policy-Lab/mosaiks-paper/blob/7efb09ed455505562d6bb04c2aaa242ef59f0a82/code/mosaiks/featurization.py#L120
+
+        Args:
+            patches: a numpy array of size (N, C, H, W)
+            min_divisor: a small number to guard against division by zero
+            zca_bias: bias term for ZCA whitening
+
+        Returns
+            a numpy array of size (N, C, H, W) containing the normalized patches
+        """
+        if patches.dtype == "uint8":
+            patches = patches.astype("float32")
+            patches /= 255.0
+
+        n_patches = patches.shape[0]
+        orig_shape = patches.shape
+        patches = patches.reshape(patches.shape[0], -1)
+
+        # Zero mean every feature
+        patches = patches - np.mean(patches, axis=1, keepdims=True)
+
+        # Normalize
+        patch_norms = np.linalg.norm(patches, axis=1)
+
+        # Get rid of really small norms
+        patch_norms[np.where(patch_norms < min_divisor)] = 1
+
+        # Make features unit norm
+        patches = patches / patch_norms[:, np.newaxis]
+
+        patchesCovMat = 1.0 / n_patches * patches.T.dot(patches)
+
+        (E, V) = np.linalg.eig(patchesCovMat)
+
+        E += zca_bias
+        sqrt_zca_eigs = np.sqrt(E)
+        inv_sqrt_zca_eigs = np.diag(np.power(sqrt_zca_eigs, -1))
+        global_ZCA = V.dot(inv_sqrt_zca_eigs).dot(V.T)
+        patches_normalized: np.ndarray = (patches).dot(global_ZCA).dot(global_ZCA.T)
+
+        return patches_normalized.reshape(orig_shape).astype("float32")
+
+    def __init__(
+        self,
+        dataset: Dataset[dict[str, Any]],
+        in_channels: int = 4,
+        features: int = 16,
+        kernel_size: int = 3,
+        bias: float = -1.0,
+        seed: Optional[int] = None,
+    ):
+        """Initializes the MOSAIKS model.
+
+        This is the main model used in Multi-task Observation using Satellite Imagery
+        & Kitchen Sinks (MOSAIKS) method proposed in
+        https://www.nature.com/articles/s41467-021-24638-z.
+
+        Args:
+            dataset: a torch dataset that returns dictionaries with an "image" key
+            in_channels: number of input channels
+            features: number of features to compute, must be divisible by 2
+            kernel_size: size of the kernel used to compute the RCFs
+            bias: bias of the convolutional layer
+            seed: random seed used to initialize the convolutional layer
+
+        .. versionadded:: 0.5.0
+        """
+        super().__init__(in_channels, features, kernel_size, bias, seed)
+
+        # sample dataset patches
+        generator = np.random.default_rng(seed=seed)
+        num_patches = features // 2
+        num_channels, height, width = dataset[0]["image"].shape
+        assert num_channels == in_channels
+
+        patches = np.zeros(
+            (num_patches, num_channels, kernel_size, kernel_size), dtype=np.float32
+        )
+        for i in range(num_patches):
+            idx = generator.integers(0, len(dataset))
+            img = dataset[idx]["image"]
+            y = generator.integers(0, height - kernel_size)
+            x = generator.integers(0, width - kernel_size)
+            patches[i] = img[:, y : y + kernel_size, x : x + kernel_size]
+
+        patches = self._normalize(patches)
+        self.weights = torch.tensor(patches)
