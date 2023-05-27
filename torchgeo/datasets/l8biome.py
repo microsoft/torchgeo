b80a4ad7c19baec171cc247179a6d23d9c696b99
Adam J. Stewart <ajstewart426@gmail.com>
2023-05-27 17:05:15 -0500

    L7 Irish/L8 Biome: don't interpolate cmap

--- torchgeo/datasets/l8biome.py
+++ torchgeo/datasets/l8biome.py
@@ -250,17 +250,18 @@ class L8Biome(RasterDataset):
             predictions = sample["prediction"].numpy().astype("uint8").squeeze()
             num_panels += 1
 
+        kwargs = {"cmap": "gray", "vmin": 0, "vmax": 4, "interpolation": "none"}
         fig, axs = plt.subplots(1, num_panels, figsize=(num_panels * 4, 5))
         axs[0].imshow(image)
         axs[0].axis("off")
-        axs[1].imshow(mask, vmin=0, vmax=4, cmap="gray")
+        axs[1].imshow(mask, **kwargs)
         axs[1].axis("off")
         if show_titles:
             axs[0].set_title("Image")
             axs[1].set_title("Mask")
 
         if showing_predictions:
-            axs[2].imshow(predictions, vmin=0, vmax=4, cmap="gray")
+            axs[2].imshow(predictions, **kwargs)
             axs[2].axis("off")
             if show_titles:
                 axs[2].set_title("Predictions")
