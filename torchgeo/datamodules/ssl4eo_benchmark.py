3d436d305705d54e737f6ca119463f0e29a89f12
Nils Lehmann <35272119+nilsleh@users.noreply.github.com>
2023-06-04 17:03:18 +0200

    Define data augmentations for benchmark tasks (#1382)
    * define data augmentations for benchmark tasks
    
    * updated probs
    
    * merge main and fix tests
    
    * revert unnecessary squeeze
    
    * final augmentations
    
    * more fixes
    
    * Fix tests
    
    * Better workaround
    
    * Fix type hints
    
    ---------
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- torchgeo/datamodules/ssl4eo_benchmark.py
+++ torchgeo/datamodules/ssl4eo_benchmark.py
@@ -3,11 +3,13 @@
 
 """SSL4EO datamodule."""
 
-from typing import Any
+from typing import Any, Union
 
 import kornia.augmentation as K
+from kornia.constants import DataKey, Resample
 
 from ..datasets import SSL4EOLBenchmark
+from ..samplers.utils import _to_tuple
 from ..transforms import AugmentationSequential
 from .geo import NonGeoDataModule
 
@@ -18,33 +20,43 @@ class SSL4EOLBenchmarkDataModule(NonGeoDataModule):
     .. versionadded:: 0.5
     """
 
-    crop_size = 224
-
     def __init__(
-        self, batch_size: int = 64, num_workers: int = 0, **kwargs: Any
+        self,
+        batch_size: int = 64,
+        patch_size: Union[int, tuple[int, int]] = 224,
+        num_workers: int = 0,
+        **kwargs: Any,
     ) -> None:
         """Initialize a new SSL4EOLBenchmarkDataModule instance.
 
         Args:
             batch_size: Size of each mini-batch.
+            patch_size: Size of each patch, either ``size`` or ``(height, width)``.
             num_workers: Number of workers for parallel data loading.
             **kwargs: Additional keyword arguments passed to
                 :class:`~torchgeo.datasets.SSL4EOLBenchmark`.
         """
         super().__init__(SSL4EOLBenchmark, batch_size, num_workers, **kwargs)
 
+        self.patch_size = _to_tuple(patch_size)
+
         self.train_aug = AugmentationSequential(
             K.Normalize(mean=self.mean, std=self.std),
-            K.CenterCrop(self.crop_size),
+            K.RandomResizedCrop(_to_tuple(self.patch_size), scale=(0.6, 1.0)),
+            K.RandomVerticalFlip(p=0.5),
+            K.RandomHorizontalFlip(p=0.5),
             data_keys=["image", "mask"],
+            extra_args={
+                DataKey.MASK: {"resample": Resample.NEAREST, "align_corners": None}
+            },
         )
         self.val_aug = AugmentationSequential(
             K.Normalize(mean=self.mean, std=self.std),
-            K.CenterCrop(self.crop_size),
+            K.CenterCrop(self.patch_size),
             data_keys=["image", "mask"],
         )
         self.test_aug = AugmentationSequential(
             K.Normalize(mean=self.mean, std=self.std),
-            K.CenterCrop(self.crop_size),
+            K.CenterCrop(self.patch_size),
             data_keys=["image", "mask"],
         )
