5985f47d2b538bbffc9704b53bf8d47aad6778b7
Nils Lehmann <nils.lehmann24@gmail.com>
2023-06-06 15:19:33 +0200

    adapt segmentation

--- torchgeo/trainers/segmentation.py
+++ torchgeo/trainers/segmentation.py
@@ -36,12 +36,21 @@ class SemanticSegmentationTask(LightningModule):  # type: ignore[misc]
         """Configures the task based on kwargs parameters passed to the constructor."""
         weights = self.hyperparams["weights"]
 
+        if self.hyperparams["backbone"].startswith("tu-vit"):
+            encoder_depth = 4
+            decoder_channels = (256, 128, 64, 32, 32)
+        else:
+            encoder_depth = 5
+            decoder_channels = (256, 128, 64, 32, 16)
+
         if self.hyperparams["model"] == "unet":
             self.model = smp.Unet(
                 encoder_name=self.hyperparams["backbone"],
                 encoder_weights="imagenet" if weights is True else None,
                 in_channels=self.hyperparams["in_channels"],
                 classes=self.hyperparams["num_classes"],
+                encoder_depth=encoder_depth,
+                decoder_channels=decoder_channels,
             )
         elif self.hyperparams["model"] == "deeplabv3+":
             self.model = smp.DeepLabV3Plus(
@@ -49,6 +58,8 @@ class SemanticSegmentationTask(LightningModule):  # type: ignore[misc]
                 encoder_weights="imagenet" if weights is True else None,
                 in_channels=self.hyperparams["in_channels"],
                 classes=self.hyperparams["num_classes"],
+                encoder_depth=encoder_depth,
+                decoder_channels=decoder_channels,
             )
         elif self.hyperparams["model"] == "fcn":
             self.model = FCN(
