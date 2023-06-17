f65948d7fff11333b965951c70487a0a6dac002d
Adam J. Stewart <ajstewart426@gmail.com>
2023-06-17 10:18:18 -0500

    Placate black

--- experiments/ssl4eo/delete_excess.py
+++ experiments/ssl4eo/delete_excess.py
@@ -15,11 +15,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("root", help="directory to search for scenes")
     parser.add_argument("--num-workers", type=int, default=10, help="number of threads")
-    parser.add_argument("--length", type=int, default=250000, help="number of scenes to keep")
+    parser.add_argument(
+        "--length", type=int, default=250000, help="number of scenes to keep"
+    )
     args = parser.parse_args()
 
     paths = sorted(glob.glob(os.path.join(args.root, "*")))
-    paths = paths[args.length:]
+    paths = paths[args.length :]
 
     if args.num_workers > 0:
         thread_map(shutil.rmtree, paths, max_workers=args.num_workers)
