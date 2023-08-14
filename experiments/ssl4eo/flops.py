de5c2d74f47fea93cfb57574fe2b6012dfeda907
Adam J. Stewart <ajstewart426@gmail.com>
2023-08-14 13:26:39 -0400

    Calculate memory requirements too

--- experiments/ssl4eo/flops.py
+++ experiments/ssl4eo/flops.py
@@ -16,8 +16,16 @@ input_shape = (batch_size, in_channels, patch_size, patch_size)
 
 for model in models:
     print(f"Model: {model}")
+
+    m = timm.create_model(model, num_classes=num_classes, in_chans=in_channels)
+
+    # Calculate memory requirements of model
+    mem_params = sum([p.nelement() * p.element_size() for p in m.parameters()])
+    mem_bufs = sum([b.nelement() * b.element_size() for b in m.buffers()])
+    mem = (mem_params + mem_bufs) / 2**20
+    print(f"Memory: {mem:.2f} MB")
+
     with get_accelerator().device(0):
-        m = timm.create_model(model, num_classes=num_classes, in_chans=in_channels)
         get_model_profile(
             model=m, input_shape=input_shape, detailed=False, module_depth=0
         )
