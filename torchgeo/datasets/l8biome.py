623c49b3bbc8307ae1c2c3fbd42863ea69069fd8
Adam J. Stewart <ajstewart426@gmail.com>
2023-08-18 09:45:49 -0400

    L7 Irish/L8 Biome: fix default CRS

--- torchgeo/datasets/l8biome.py
+++ torchgeo/datasets/l8biome.py
@@ -90,7 +90,7 @@ class L8Biome(RasterDataset):
     def __init__(
         self,
         root: str = "data",
-        crs: Optional[CRS] = None,
+        crs: Optional[CRS] = CRS.from_epsg(3857),
         res: Optional[float] = None,
         bands: Sequence[str] = all_bands,
         transforms: Optional[Callable[[dict[str, Any]], dict[str, Any]]] = None,
@@ -103,7 +103,7 @@ class L8Biome(RasterDataset):
         Args:
             root: root directory where dataset can be found
             crs: :term:`coordinate reference system (CRS)` to warp to
-                (defaults to the CRS of the first file found)
+                (defaults to EPSG:3857)
             res: resolution of the dataset in units of CRS
                 (defaults to the resolution of the first file found)
             bands: bands to return (defaults to all bands)
