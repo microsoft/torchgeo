09666ccaf3d6b9d9db91596aff5460f6fff974e6
Adrian Tofting <adrian@vake.ai>
2023-10-03 11:03:50 +0200

    Add url to reference for apache vsi syntax

--- torchgeo/datasets/utils.py
+++ torchgeo/datasets/utils.py
@@ -746,8 +746,9 @@ def path_is_vsi(path: str) -> bool:
 
     vsi can for instance be Cloud Storage Blobs or zip-archives.
     They will start with a prefix indicating this.
-    For examples of these, see reference:
+    For examples of these, see references for the two accepted syntaxes.
     https://gdal.org/user/virtual_file_systems.html
+    https://rasterio.readthedocs.io/en/latest/topics/datasets.html
 
     Args:
         path: string representing a directory or file
