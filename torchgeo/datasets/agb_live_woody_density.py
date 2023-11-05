6a9679ac8acf06358d3cd055b0128f5017baaa05
Nils Lehmann <nils.lehmann24@gmail.com>
2023-11-05 15:18:47 +0100

    fix link

--- torchgeo/datasets/agb_live_woody_density.py
+++ torchgeo/datasets/agb_live_woody_density.py
@@ -45,8 +45,8 @@ class AbovegroundLiveWoodyBiomassDensity(RasterDataset):
     is_image = False
 
     url = (
-        "https://opendata.arcgis.com/api/v3/datasets/3e8736c8866b458687"
-        "e00d40c9f00bce_0/downloads/data?format=geojson&spatialRefId=4326"
+        "https://opendata.arcgis.com/api/v3/datasets/e4bdbe8d6d8d4e32ace7d3"
+        "6a4aec7b93_0/downloads/data?format=geojson&spatialRefId=4326"
     )
 
     base_filename = "Aboveground_Live_Woody_Biomass_Density.geojson"
@@ -123,7 +123,7 @@ class AbovegroundLiveWoodyBiomassDensity(RasterDataset):
 
         for item in content["features"]:
             download_url(
-                item["properties"]["download"],
+                item["properties"]["Mg_px_1_download"],
                 self.paths,
                 item["properties"]["tile_id"] + ".tif",
             )
