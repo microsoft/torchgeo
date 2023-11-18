15809645833d8df702b5755083fd6dc7122e491a
Jingtong <115182031+cookie-kyu@users.noreply.github.com>
2023-11-17 19:55:54 -0800

    Update torchgeo/datasets/south_america_soybean.py
    Co-authored-by: Yi-Chia Chang <61452667+yichiac@users.noreply.github.com>

--- torchgeo/datasets/south_america_soybean.py
+++ torchgeo/datasets/south_america_soybean.py
@@ -108,8 +108,6 @@ This dataset produced annual 30-m soybean maps of South America from 2001 to 202
         self.paths = paths
         self.download = download
         self.checksum = checksum
-        self.ordinal_map = torch.zeros(max(self.cmap.keys()) + 1, dtype=self.dtype)
-        self.ordinal_cmap = torch.zeros((len(self.classes), 4), dtype=torch.uint8)
     
         self._verify()
 
