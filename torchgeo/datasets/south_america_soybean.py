3eb2e33855cf47944b817083ea2314cb452dcf64
Jingtong <115182031+cookie-kyu@users.noreply.github.com>
2023-11-17 19:50:35 -0800

    Update torchgeo/datasets/south_america_soybean.py
    Co-authored-by: Yi-Chia Chang <61452667+yichiac@users.noreply.github.com>

--- torchgeo/datasets/south_america_soybean.py
+++ torchgeo/datasets/south_america_soybean.py
@@ -163,11 +163,7 @@ class SouthAmericaSoybean(RasterDataset):
 
         # Check if the user requested to download the dataset
         if not self.download:
-            raise RuntimeError(
-                f"Dataset not found in `root={self.paths}` and `download=False`, "
-                "either specify a different `root` directory or use `download=True` "
-                "to automatically download the dataset."
-            )
+            raise DatasetNotFoundError(self)
 
         # Download the dataset
         self._download()
