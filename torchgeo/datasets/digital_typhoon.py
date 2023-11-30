c31f73add22ce375c8de0cbf2e4aa3fd0ea910f9
nilsleh <nils.lehmann24@gmail.com>
2023-11-30 15:57:27 +0000

    implement sequence sampling

--- torchgeo/datasets/digital_typhoon.py
+++ torchgeo/datasets/digital_typhoon.py
@@ -53,6 +53,7 @@ class DigitalTyphoonAnalysis(NonGeoDataset):
         root: str = "data",
         task: str = "regression",
         features: Sequence[str] = ["wind"],
+        sequence_length: int = 3,
         transforms: Optional[Callable[[dict[str, Tensor]], dict[str, Tensor]]] = None,
         download: bool = False,
         checksum: bool = False,
@@ -77,6 +78,7 @@ class DigitalTyphoonAnalysis(NonGeoDataset):
         self.transforms = transforms
         self.download = download
         self.checksum = checksum
+        self.sequence_length = sequence_length
 
         assert task in self.valid_tasks, f"Please choose one of {self.valid_tasks}"
         self.task = task
@@ -84,10 +86,59 @@ class DigitalTyphoonAnalysis(NonGeoDataset):
         assert set(features).issubset(set(self.valid_features))
         self.features = features
 
-        # get all the hf file paths and save them to a dictionary with corresponding id
         self.aux_df = pd.read_csv(os.path.join(root, self.aux_file_name))
+        self.aux_df["datetime"] = pd.to_datetime(
+            self.aux_df[["year", "month", "day", "hour"]]
+        )
 
-        # processing based on different tasks here?
+        self.aux_df = self.aux_df.sort_values(["year", "month", "day", "hour"])
+        self.aux_df["seq_id"] = self.aux_df.groupby(["id"]).cumcount()
+
+        # Compute the hour difference between consecutive images per typhoon id
+        self.aux_df["hour_diff"] = (
+            self.aux_df.sort_values(["id", "datetime"])
+            .groupby("id")["datetime"]
+            .diff()
+            .dt.total_seconds()
+            / 3600
+        )
+
+        # 0 hour difference is for the last time step of each typhoon sequence and want to keep only images that have max 1 hour difference
+        self.aux_df = self.aux_df[self.aux_df["hour_diff"] <= 1]
+        # Filter out all ids that only have less than sequence_length entries
+        self.aux_df = self.aux_df.groupby("id").filter(
+            lambda x: len(x) >= self.sequence_length
+        )
+
+        def get_subsequences(df: pd.DataFrame, k: int) -> list[dict[str, list[int]]]:
+            """Generate all possible subsequences of length k for a given group.
+
+            Args:
+                df: grouped dataframe of a single typhoon
+                k: length of the subsequences to generate
+
+            Returns:
+                list of all possible subsequences of length k for a given typhoon id
+            """
+            # generate possible subsquences of length k for each group
+            subsequences = [
+                {"id": df["id"].iloc[0], "seq_id": list(range(i, i + k))}
+                for i in range(len(df) - k + 1)
+            ]
+            return [
+                subseq
+                for subseq in subsequences
+                if set(subseq["seq_id"]).issubset(df["seq_id"])
+            ]
+
+        # self.sample_sequences = self.aux_df.groupby('id').apply(get_subsequences, k=self.sequence_length).tolist()
+        self.sample_sequences: list[dict[str, list[int]]] = [
+            item
+            for sublist in self.aux_df.groupby("id")
+            .apply(get_subsequences, k=self.sequence_length)
+            .tolist()
+            for item in sublist
+        ]
 
     def __getitem__(self, index: int) -> dict[str, Any]:
         """Return an index within the dataset.
@@ -98,22 +149,20 @@ class DigitalTyphoonAnalysis(NonGeoDataset):
         Returns:
             data, labels, and metadata at that index
         """
-        sample_entry = self.aux_df.iloc[index]
+        sample_entry = self.sample_sequences[index]
+        sample_df = self.aux_df[
+            (self.aux_df["id"] == sample_entry["id"])
+            & (self.aux_df["seq_id"].isin(sample_entry["seq_id"]))
+        ]
 
-        sample = {
-            "image": self._load_image(
-                os.path.join(
-                    self.root,
-                    "image",
-                    str(sample_entry["id"]),
-                    sample_entry["image_path"],
-                )
-            )
-        }
+        sample = {"image": self._load_image(sample_df)}
+        # load features of the last image in the sequence
         sample.update(
             self._load_features(
-                os.path.join(self.root, "metadata", str(sample_entry["id"]) + ".csv"),
-                sample_entry["image_path"],
+                os.path.join(
+                    self.root, "metadata", str(sample_df.iloc[-1]["id"]) + ".csv"
+                ),
+                sample_df.iloc[-1]["image_path"],
             )
         )
 
@@ -128,20 +177,33 @@ class DigitalTyphoonAnalysis(NonGeoDataset):
         Returns:
             length of the dataset
         """
-        return len(self.aux_df)
+        return len(self.sample_sequences)
 
-    def _load_image(self, filepath: str) -> Tensor:
+    def _load_image(self, sample_df: pd.DataFrame) -> Tensor:
         """Load a single image.
 
         Args:
-            filepath: path of the image file to load
+            sample_df: df holding all information necessary to load the
+                consecutive images in the sequence
 
         Returns:
-            the image
+            concatenation of all images in the sequence over channel dimension
         """
-        with h5py.File(filepath, "r") as h5f:
-            # tensor with added channel dimension
-            tensor = torch.from_numpy(h5f["Infrared"][:]).unsqueeze(0)
+
+        def load_image_tensor(id: str, filepath: str) -> Tensor:
+            full_path = os.path.join(self.root, "image", id, filepath)
+            with h5py.File(full_path, "r") as h5f:
+                # tensor with added channel dimension
+                tensor = torch.from_numpy(h5f["Infrared"][:]).unsqueeze(0)
+            return tensor
+
+        # tensor of shape [sequence_length, height, width]
+        tensor = torch.cat(
+            [
+                load_image_tensor(str(id), filepath)
+                for id, filepath in zip(sample_df["id"], sample_df["image_path"])
+            ]
+        )
         return tensor
 
     def _load_features(self, filepath: str, image_path: str) -> dict[str, Any]:
