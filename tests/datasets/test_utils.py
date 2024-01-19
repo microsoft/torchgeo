4a87d7101be4c470cec760a744cece4e2aa2a0fa
Favyen Bastani <favyenb@allenai.org>
2024-01-19 15:44:26 -0800

    type annotation fix

--- tests/datasets/test_utils.py
+++ tests/datasets/test_utils.py
@@ -664,7 +664,7 @@ def test_percentile_normalization() -> None:
     "array_dtype",
     [np.uint8, np.uint16, np.uint32, np.int8, np.int16, np.int32, np.int64],
 )
-def test_array_to_tensor(array_dtype: np.dtype) -> None:
+def test_array_to_tensor(array_dtype: np.typing.DTypeLike) -> None:
     array: "np.typing.NDArray[Any]" = np.zeros((2,), dtype=array_dtype)
     array[0] = np.iinfo(array.dtype).min
     array[1] = np.iinfo(array.dtype).max
