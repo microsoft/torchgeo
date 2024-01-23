4ddaf7087ee63bc7a5f9b41e7e720214f4eddfc5
cookie-kyu <jingtongwang149@gmail.com>
2024-01-22 22:31:47 -0600

    Fix comments

--- torchgeo/datasets/south_america_soybean.py
+++ torchgeo/datasets/south_america_soybean.py
@@ -1,3 +1,8 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""South America Soybean Dataset"""
+
 import glob
 import os
 from collections.abc import Iterable
@@ -15,7 +20,7 @@ from .utils import BoundingBox, DatasetNotFoundError, download_url, extract_arch
 class SouthAmericaSoybean(RasterDataset):
     """South America Soybean Dataset
 
-This dataset produced annual 30-m soybean maps of South America from 2001 to 2021.
+    This dataset produced annual 30-m soybean maps of South America from 2001 to 2021.
 
     Link: https://www.nature.com/articles/s41893-021-00729-z
 
@@ -30,47 +35,18 @@ This dataset produced annual 30-m soybean maps of South America from 2001 to 202
 
     If you use this dataset in your research, please use the corresponding citation:
 
-* https://doi.org/10.1038/s41893-021-00729-z 
+    * https://doi.org/10.1038/s41893-021-00729-z 
 
-.. versionadded:: 0.6
-
+    .. versionadded:: 0.6
     """
     filename_glob = "South_America_Soybean_*.*"
     filename_regex = r"South_America_Soybean_(?P<year>\d{4})"
     zipfile_glob = "SouthAmericaSoybean.zip"
 
-
     date_format = "%Y"
     is_image = False
-
     url = "https://glad.umd.edu/projects/AnnualClassMapsV1/SouthAmerica_Soybean_"
-
     md5 = "7f1d06a57cc6c4ae6be3b3fb9464ddeb"
-    md5s = {
-        2001: "2914b0af7590a0ca4dfa9ccefc99020f", 
-        2002: "8a4a9dcea54b3ec7de07657b9f2c0893",
-        2003: "cad5ed461ff4ab45c90177841aaecad2",
-        2004: "f9882ca9c70e054e50172835cb75a8c3",
-        2005: "89faae27f9b5afbd06935a465e5fe414",
-        2006: "eabaa525414ecbff89301d3d5c706f0b",
-        2007: "bb8549b6674163fe20ffd47ec4ce8903",
-        2008: "96fc3f737ab3ce9bcd16cbf7761427e2",
-        2009: "341387c1bb42a15140c80702e4cca02d",
-        2010: "9264532d36ffa93493735a6e44caef0d",
-        2011: "b73352ebea3d5658959e9044ec526143",
-        2012: "9f3a71097c9836fcff18a13b9ba608b2",
-        2013: "0263e19b3cae6fdaba4e3b450cef985e",
-        2014: "824ff91c62a4ba9f4ccfd281729830e5",
-        2015: "6beb96a61fe0e9ce8c06263e500dde8f",
-        2016: "770c558f6ac40550d0e264da5e44b3e",
-        2017: "4d0487ac1105d171e5f506f1766ea777",
-        2018: "503c2d0a803c2a2629ebbbd9558a3013",
-        2019: "441836493bbcd5e123cff579a58f5a4f",
-        2020: "0709dec807f576c9707c8c7e183db31",
-        2021: "edff3ada13a1a9910d1fe844d28ae4f",
-
-    }
-
 
     def __init__(
         self,
@@ -84,21 +60,19 @@ This dataset produced annual 30-m soybean maps of South America from 2001 to 202
     ) -> None:
         """Initialize a new Dataset instance.
         Args:
-            root: root directory where dataset can be found
+            paths: one or more root directories to search or files to load
             crs: :term:`coordinate reference system (CRS)` to warp to
                 (defaults to the CRS of the first file found)
             res: resolution of the dataset in units of CRS
                 (defaults to the resolution of the first file found)
-            years: list of years to use
             transforms: a function/transform that takes an input sample
                 and returns a transformed version
             cache: if True, cache file handle to speed up repeated sampling
             download: if True, download dataset and store it in the root directory
             checksum: if True, check the MD5 after downloading files (may be slow)
+
         Raises:
-            FileNotFoundError: if no files are found in ``root``
             RuntimeError: if ``download=False`` but dataset is missing or checksum fails
-            AssertionError: if ``year`` is invalid
         """
         self.paths = paths
         self.download = download
@@ -121,10 +95,7 @@ This dataset produced annual 30-m soybean maps of South America from 2001 to 202
         return sample
 
     def _verify(self) -> None:
-        """Verify the integrity of the dataset.
-        Raises:
-            RuntimeError: if ``download=False`` but dataset is missing or checksum fails
-        """
+        """Verify the integrity of the dataset."""
         # Check if the extracted files already exist
         if self.files:
             return
@@ -134,7 +105,6 @@ This dataset produced annual 30-m soybean maps of South America from 2001 to 202
             self._extract()
             return
 
-
         # Check if the user requested to download the dataset
         if not self.download:
             raise DatasetNotFoundError(self)
@@ -142,6 +112,7 @@ This dataset produced annual 30-m soybean maps of South America from 2001 to 202
         # Download the dataset
         self._download()
         self._extract()
+
     def _download(self) -> None:
         """Download the dataset."""
         filename = "SouthAmericaSoybean.zip"
@@ -149,6 +120,7 @@ This dataset produced annual 30-m soybean maps of South America from 2001 to 202
         download_url(
             self.url, self.paths, filename, md5=self.md5 if self.checksum else None
         )
+
     def _extract(self) -> None:
         """Extract the dataset."""
         assert isinstance(self.paths, str)
@@ -156,7 +128,6 @@ This dataset produced annual 30-m soybean maps of South America from 2001 to 202
         pathname = os.path.join(self.paths, "**", self.zipfile_glob)
     
         extract_archive(glob.glob(pathname, recursive=True)[0], self.paths)
-
 
     def plot(
         self,
@@ -200,6 +171,4 @@ This dataset produced annual 30-m soybean maps of South America from 2001 to 202
             plt.suptitle(suptitle)
 
         return fig
-
-
-
+    
