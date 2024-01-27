10f516db863620c8e58d3c2aa01179f2e32ca1cc
Adam J. Stewart <ajstewart426@gmail.com>
2024-01-27 11:58:40 +0100

    Update TorchGeo

--- torchgeo/samplers/utils.py
+++ torchgeo/samplers/utils.py
@@ -12,13 +12,11 @@ from ..datasets import BoundingBox
 
 
 @overload
-def _to_tuple(value: Union[tuple[int, int], int]) -> tuple[int, int]:
-    ...
+def _to_tuple(value: Union[tuple[int, int], int]) -> tuple[int, int]: ...
 
 
 @overload
-def _to_tuple(value: Union[tuple[float, float], float]) -> tuple[float, float]:
-    ...
+def _to_tuple(value: Union[tuple[float, float], float]) -> tuple[float, float]: ...
 
 
 def _to_tuple(value: Union[tuple[float, float], float]) -> tuple[float, float]:
