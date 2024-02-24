18b4a4fde6e70779fde330cd91bfc0cc34d5f0aa
Adam J. Stewart <ajstewart426@gmail.com>
2024-02-24 10:45:58 +0100

    Add type hints

--- torchgeo/models/dofa.py
+++ torchgeo/models/dofa.py
@@ -12,6 +12,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 from timm.models.vision_transformer import Block
+from torch import Tensor
 from torchvision.models._api import Weights, WeightsEnum
 
 from ..transforms import AugmentationSequential
@@ -19,11 +20,20 @@ from ..transforms import AugmentationSequential
 __all__ = ["DOFABase16_Weights"]
 
 
-def get_1d_sincos_pos_embed_from_grid_torch(embed_dim, pos):
-    """
-    embed_dim: output dimension for each position
-    pos: a list of positions to be encoded: size (M,)
-    out: (M, D)
+def get_1d_sincos_pos_embed_from_grid_torch(embed_dim: int, pos: Tensor) -> Tensor:
+    """Compute the 1D sine/cosine position embeddings in a grid.
+
+    .. versionadded:: 0.6
+
+    Args:
+        embed_dim: Output dimension D for each position. Must be even.
+        pos: A list of positions to be encoded, of size (M,).
+
+    Returns:
+        Position embeddings of size (M, D).
+
+    Raises:
+        AssertionError: If *embed_dim* is not even.
     """
     assert embed_dim % 2 == 0
     omega = torch.arange(embed_dim // 2, dtype=torch.float32, device=pos.device)
@@ -41,8 +51,29 @@ def get_1d_sincos_pos_embed_from_grid_torch(embed_dim, pos):
 
 
 class TransformerWeightGenerator(nn.Module):
-    def __init__(self, input_dim, output_dim, embed_dim, num_heads=4, num_layers=1):
-        super(TransformerWeightGenerator, self).__init__()
+    """Dynamic weight generator for DOFA.
+
+    .. versionadded:: 0.6
+    """
+
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        embed_dim: int,
+        num_heads: int = 4,
+        num_layers: int = 1,
+    ) -> None:
+        """Initialize a new TransformerWeightGenerator instance.
+
+        Args:
+            input_dim: Input dimensions.
+            output_dim: Output dimensions.
+            embed_dim: Embedding dimensions.
+            num_heads: Number of heads.
+            num_layers: Number of layers.
+        """
+        super().__init__()
         encoder_layer = nn.TransformerEncoderLayer(
             d_model=input_dim,
             nhead=num_heads,
@@ -67,33 +98,55 @@ class TransformerWeightGenerator(nn.Module):
         torch.nn.init.normal_(self.weight_tokens, std=0.02)
         torch.nn.init.normal_(self.bias_token, std=0.02)
 
-    def forward(self, x):
-        # x should have shape [seq_len, batch, input_dim]
+    def forward(self, x: Tensor) -> tuple[Tensor, Tensor]:
+        """Forward pass of the model.
+
+        Args:
+            x: Input mini-batch of size (seq_len, batch, input_dim).
+
+        Returns:
+            Weight and bias.
+        """
         pos_wave = x
         x = torch.cat([self.weight_tokens, pos_wave], dim=0)
         x = torch.cat([x, self.bias_token], dim=0)
         transformer_output = self.transformer_encoder(x)
         weights = self.fc_weight(transformer_output[self.wt_num : -1] + pos_wave)
-        bias = self.fc_bias(
-            transformer_output[-1]
-        )  # Using the last output to generate bias
+        # Using the last output to generate bias
+        bias = self.fc_bias(transformer_output[-1])
         return weights, bias
 
 
 class FCResLayer(nn.Module):
-    def __init__(self, linear_size=128):
-        super(FCResLayer, self).__init__()
+    """Fully-connected residual layer.
+
+    .. versionadded:: 0.6
+    """
+
+    def __init__(self, linear_size: int = 128) -> None:
+        """Initialize a new FCResLayer instance.
+
+        Args:
+            linear_size: Size of linear layer.
+        """
+        super().__init__()
         self.l_size = linear_size
         self.nonlin1 = nn.ReLU(inplace=True)
         self.nonlin2 = nn.ReLU(inplace=True)
-        # self.dropout1 = nn.Dropout()
         self.w1 = nn.Linear(self.l_size, self.l_size)
         self.w2 = nn.Linear(self.l_size, self.l_size)
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
+        """Forward pass of the model.
+
+        Args:
+            x: Input mini-batch.
+
+        Returns:
+            Output of the model.
+        """
         y = self.w1(x)
         y = self.nonlin1(y)
-        # y = self.dropout1(y)
         y = self.w2(y)
         y = self.nonlin2(y)
         out = x + y
@@ -101,21 +154,32 @@ class FCResLayer(nn.Module):
 
 
 class Dynamic_MLP_OFA(nn.Module):
-    """
-    Input: channels of wavelength (normalized): List -> List
-           kernel size of the depth-wise convolution: kernel_size, default 3x3
-           wv_planes
-           inplanes
+    """Dynamic Multilayer Perceptron for DOFA.
+
+    .. versionadded:: 0.6
     """
 
-    def __init__(self, wv_planes, inter_dim=128, kernel_size=3, embed_dim=1024):
+    def __init__(
+        self,
+        wv_planes: int,
+        inter_dim: int = 128,
+        kernel_size: int = 3,
+        embed_dim: int = 1024,
+    ) -> None:
+        """Initialize a new Dynamic_MLP_OFA instance.
+
+        Args:
+            wv_planes: Dimensions of dynamic weight generator.
+            inter_dim: Internal dimensions.
+            kernel_size: Kernel size of the depth-wise convolution.
+            embed_dim: Embedding dimensions.
+        """
         super().__init__()
-        self.kernel_size = kernel_size
         self.wv_planes = wv_planes
-        self.embed_dim = embed_dim
-        self.kernel_size = kernel_size
-        self._num_kernel = self.kernel_size * self.kernel_size * self.embed_dim
         self.inter_dim = inter_dim
+        self.kernel_size = kernel_size
+        self.embed_dim = embed_dim
+        self._num_kernel = self.kernel_size * self.kernel_size * self.embed_dim
         self.patch_size = (kernel_size, kernel_size)
         self.num_patches = -1
 
@@ -128,30 +192,49 @@ class Dynamic_MLP_OFA(nn.Module):
 
         self._init_weights()
 
-    def _get_weights(self, waves):
+    def _get_weights(self, waves: Tensor) -> Tensor:
+        """Generate dynamic weights.
+
+        Args:
+            waves: Input wavelengths.
+
+        Returns:
+            Dynamic weights.
+        """
         dynamic_weights = self.weight_generator(waves)
 
         return dynamic_weights
 
-    def weight_init(self, m):
+    def weight_init(self, m: object) -> None:
+        """Initialize weights of a single layer.
+
+        Args:
+            m: A single layer.
+        """
         if isinstance(m, nn.Linear):
             init.xavier_uniform_(m.weight)
             m.bias.data.fill_(0.01)
 
-    def _init_weights(self):
-        """
-        initialize the base weights and dynamic mlp weights
-        """
+    def _init_weights(self) -> None:
+        """Initialize the base weights and dynamic MLP weights."""
         self.weight_generator.apply(self.weight_init)
         self.fclayer.apply(self.weight_init)
 
-    def forward(self, img_feat, wvs):
+    def forward(self, img_feat: Tensor, wvs: Tensor) -> tuple[Tensor, Tensor]:
+        """Forward pass of the model.
+
+        Args:
+            img_feat: Input mini-batch.
+            wvs: Wavelengths.
+
+        Return:
+            Output mini-batch and wavelengths.
+        """
         inplanes = wvs.size(0)
         # wv_feats: 9,128 -> 9, 3x3x3
         waves = get_1d_sincos_pos_embed_from_grid_torch(self.wv_planes, wvs * 1000)
         waves = self.fclayer(waves)
         weight, bias = self._get_weights(waves)  # 3x3x3
-        # bias = None
 
         dynamic_weight = weight.view(
             self.embed_dim, inplanes, self.kernel_size, self.kernel_size
@@ -172,22 +255,40 @@ class Dynamic_MLP_OFA(nn.Module):
 
 
 class OFAViT(nn.Module):
-    """Masked Autoencoder with VisionTransformer backbone"""
+    """Masked Autoencoder with VisionTransformer backbone.
+
+    .. versionadded:: 0.6
+    """
 
     def __init__(
         self,
-        img_size=224,
-        patch_size=16,
-        drop_rate=0.0,
-        embed_dim=1024,
-        depth=24,
-        num_heads=16,
-        wv_planes=128,
-        num_classes=45,
-        global_pool=True,
-        mlp_ratio=4.0,
-        norm_layer=nn.LayerNorm,
-    ):
+        img_size: int = 224,
+        patch_size: int = 16,
+        drop_rate: float = 0.0,
+        embed_dim: int = 1024,
+        depth: int = 24,
+        num_heads: int = 16,
+        wv_planes: int = 128,
+        num_classes: int = 45,
+        global_pool: bool = True,
+        mlp_ratio: float = 4.0,
+        norm_layer: nn.Module = nn.LayerNorm,
+    ) -> None:
+        """Initialize a new OFAViT instance.
+
+        Args:
+            img_size: Input image size.
+            patch_size: Patch size.
+            drop_rate: Head dropout rate.
+            embed_dim: Transformer embedding dimension.
+            depth: Depth of transformer.
+            num_heads: Number of attention heads.
+            wv_planes: Number of wavelengths.
+            num_classes: Number of classes for classification head.
+            global_pool: Whether or not to perform global pooling.
+            mlp_ratio: Ratio of MLP hidden dim to embedding dim.
+            norm_layer: Normalization layer.
+        """
         super().__init__()
 
         self.wv_planes = wv_planes
@@ -229,7 +330,16 @@ class OFAViT(nn.Module):
             nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
         )
 
-    def forward_features(self, x, wave_list):
+    def forward_features(self, x: Tensor, wave_list: list[float]) -> Tensor:
+        """Forward pass of the feature embedding layer.
+
+        Args:
+            x: Input mini-batch.
+            wave_list: List of wavelengths.
+
+        Returns:
+            Output mini-batch.
+        """
         # embed patches
         wavelist = torch.tensor(wave_list, device=x.device).float()
         self.waves = wavelist
@@ -254,17 +364,36 @@ class OFAViT(nn.Module):
             outcome = x[:, 0]
         return outcome
 
-    def forward_head(self, x, pre_logits=False):
+    def forward_head(self, x: Tensor, pre_logits: bool = False) -> Tensor:
+        """Forward pass of the attention head.
+
+        Args:
+            x: Input mini-batch.
+            pre_logits: Whether or not to return the layer before logits are computed.
+
+        Returns:
+            Output mini-batch.
+        """
         x = self.head_drop(x)
         return x if pre_logits else self.head(x)
 
-    def forward(self, x, wave_list):
+    def forward(self, x: Tensor, wave_list: list[float]) -> Tensor:
+        """Forward pass of the model.
+
+        Args:
+            x: Input mini-batch.
+            wave_list: List of wavelengths.
+
+        Returns:
+            Output mini-batch.
+        """
         x = self.forward_features(x, wave_list)
         x = self.forward_head(x)
         return x
 
 
 # https://github.com/ShadowXZT/DOFA-Net/blob/master/normalize_dataset.py
+# Normalization is sensor-dependent and is therefore left out
 _dofa_transforms = AugmentationSequential(K.CenterCrop((224, 224)), data_keys=["image"])
 
 # https://github.com/pytorch/vision/pull/6883
