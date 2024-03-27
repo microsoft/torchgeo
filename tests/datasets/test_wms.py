31fc997a44114b4371901ab0b622a98e11e6092e
Ian Turton <ijurton@gmail.com>
2024-03-27 16:53:04 +0000

    QA fixes

--- tests/datasets/test_wms.py
+++ tests/datasets/test_wms.py
@@ -9,25 +9,26 @@ from torchgeo.datasets import WMSDataset
 SERVICE_URL = "https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r-t.cgi?"
 
 
-class TestWMSDataset:
+def service_ok(url: str, timeout: int = 5) -> bool:
+    try:
+        resp = requests.head(url, allow_redirects=True, timeout=timeout)
+        ok = bool(resp.ok)
+    except requests.exceptions.ReadTimeout:
+        ok = False
+    except requests.exceptions.ConnectTimeout:
+        ok = False
+    except Exception:
+        ok = False
+    return ok
 
-    def service_ok(url, timeout=5):
-        try:
-            resp = requests.head(url, allow_redirects=True, timeout=timeout)
-            ok = resp.ok
-        except requests.exceptions.ReadTimeout:
-            ok = False
-        except requests.exceptions.ConnectTimeout:
-            ok = False
-        except Exception:
-            ok = False
-        return ok
+
+class TestWMSDataset:
 
     @pytest.mark.online
     @pytest.mark.skipif(
         not service_ok(SERVICE_URL), reason="WMS service is unreachable"
     )
-    def test_wms_no_layer(self):
+    def test_wms_no_layer(self) -> None:
         """MESONET GetMap 1.1.1"""
         wms = WMSDataset(SERVICE_URL, 10.0)
         assert "nexrad_base_reflect" in wms.layers()
@@ -39,7 +40,7 @@ class TestWMSDataset:
         assert 50 == wms.index.bounds[3]
         assert "image/png" == wms._format
 
-    def test_wms_layer(self):
+    def test_wms_layer(self) -> None:
         """MESONET GetMap 1.1.1"""
         wms = WMSDataset(SERVICE_URL, 10.0, layer="nexrad_base_reflect", crs=4326)
         assert 4326 == wms.crs.to_epsg()
@@ -49,7 +50,7 @@ class TestWMSDataset:
         assert 50 == wms.index.bounds[3]
         assert "image/png" == wms._format
 
-    def test_wms_layer_nocrs(self):
+    def test_wms_layer_nocrs(self) -> None:
         """MESONET GetMap 1.1.1"""
         wms = WMSDataset(SERVICE_URL, 10.0, layer="nexrad_base_reflect")
         assert 4326 == wms.crs.to_epsg()
--- torchgeo/datasets/wms.py
+++ torchgeo/datasets/wms.py
@@ -2,10 +2,12 @@
 # Licensed under the MIT License.
 # Author: Ian Turton, Glasgow University ian.turton@gla.ac.uk
 
+"""A simple class to fetch WMS images."""
 from io import BytesIO
-from typing import Any
+from typing import Any, Callable, Optional, Union
 
 import torchvision.transforms as transforms
+from owslib.map.wms111 import ContentMetadata
 from owslib.wms import WebMapService
 from PIL import Image
 from rasterio.coords import BoundingBox
@@ -17,19 +19,39 @@ from torchgeo.datasets import GeoDataset
 
 
 class WMSDataset(GeoDataset):
-    """
-    Allow models to fetch images from a WMS (at a good resolution)
-    """
+    """Allow models to fetch images from a WMS (at a good resolution)."""
 
-    _url = None
-    _wms = None
+    _url: str = ""
+    _wms: WebMapService = None
 
-    _layers = []
-    _layer = None
-    _layer_name = ""
+    _layers: list[ContentMetadata] = []
+    _layer: ContentMetadata = None
+    _layer_name: str = ""
     is_image = True
 
-    def __init__(self, url, res, layer=None, transforms=None, crs=None):
+    def __init__(
+        self,
+        url: str,
+        res: float,
+        layer: Optional[str] = None,
+        transforms: Optional[Callable[[dict[str, Any]], dict[str, Any]]] = None,
+        crs: Optional[Union[str, int]] = None,
+    ) -> None:
+        """Initialize the data set instance.
+
+        Args:
+            url: String pointing to the WMS Server (no need for
+                request=getcapabilities).
+            res: The best resolution to make requests at
+                (should probably match your other datasets).
+            layer: An optional string with the name of the
+                layer you want to fetch.
+            transforms:  a function/transform that takes an input sample
+                 and returns a transformed version
+            crs: An optional string or integer code that is a
+                valid EPSG code (without the 'EPSG:')
+
+        """
         super().__init__(transforms)
         self._url = url
         self._res = res
@@ -39,10 +61,17 @@ class WMSDataset(GeoDataset):
         self._format = self._wms.getOperationByName("GetMap").formatOptions[0]
         self._layers = list(self._wms.contents)
 
-        if layer in self._layers:
+        if layer is not None and layer in self._layers:
             self.layer(layer, crs)
 
-    def layer(self, layer, crs=None):
+    def layer(self, layer: str, crs: Optional[Union[str, int]] = None) -> None:
+        """Set the layer to be fetched.
+
+        Args:
+            layer: A string with the name of the layer you want to fetch.
+            crs: An optional string or integer code that is a valid EPSG
+                code (without the 'EPSG:')
+        """
         self._layer = self._wms[layer]
         self._layer_name = layer
         coords = self._wms[layer].boundingBox
@@ -71,10 +100,12 @@ class WMSDataset(GeoDataset):
                 except CRSError:
                     pass
 
-    def getlayer(self):
+    def getlayer(self) -> ContentMetadata:
+        """Return the selected layer object."""
         return self._layer
 
-    def layers(self):
+    def layers(self) -> list[str]:
+        """Return a list of availiable layers."""
         return self._layers
 
     def __getitem__(self, query: BoundingBox) -> dict[str, Any]:
