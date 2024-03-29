777e0f4005ee39efbde91be00cfe580f6073b4a7
Adam J. Stewart <ajstewart426@gmail.com>
2024-03-29 14:02:03 +0100

    Fix intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ autodoc_typehints_description_target = "documented"
 # sphinx.ext.intersphinx
 intersphinx_mapping = {
     "azure-storage-blob": (
-        "https://azuresdkdocs.blob.core.windows.net/$web/python/azure-storage-blob/stable/",  # noqa: E501
+        "https://azuresdkdocs.blob.core.windows.net/$web/python/azure-storage-blob/latest/",  # noqa: E501
         None,
     ),
     "kornia": ("https://kornia.readthedocs.io/en/stable/", None),
