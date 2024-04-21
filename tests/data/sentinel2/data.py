b1c4597f4b82e258a217c7334a8154677576e22f
Yi-Chia Chang <yichia3@illinois.edu>
2024-04-21 16:34:41 -0500

    new s2 images

--- tests/data/sentinel2/data.py
+++ tests/data/sentinel2/data.py
@@ -11,140 +11,45 @@ from rasterio import Affine
 from rasterio.crs import CRS
 
 SIZE = 128
+current_x_offset = 0
 
 np.random.seed(0)
 
-FILENAME_HIERARCHY = dict[str, "FILENAME_HIERARCHY"] | list[str]
+FILENAME_HIERARCHY = dict[str, dict | list[str]]
 
-filenames: FILENAME_HIERARCHY = {
-    # USGS Earth Explorer
-    "S2A_MSIL1C_20220412T162841_N0400_R083_T16TFM_20220412T202300.SAFE": {
-        "GRANULE": {
-            "L1C_T16TFM_A035544_20220412T163959": {
-                "IMG_DATA": [
-                    "T16TFM_20220412T162841_B01.jp2",
-                    "T16TFM_20220412T162841_B02.jp2",
-                    "T16TFM_20220412T162841_B03.jp2",
-                    "T16TFM_20220412T162841_B04.jp2",
-                    "T16TFM_20220412T162841_B05.jp2",
-                    "T16TFM_20220412T162841_B06.jp2",
-                    "T16TFM_20220412T162841_B07.jp2",
-                    "T16TFM_20220412T162841_B08.jp2",
-                    "T16TFM_20220412T162841_B09.jp2",
-                    "T16TFM_20220412T162841_B10.jp2",
-                    "T16TFM_20220412T162841_B11.jp2",
-                    "T16TFM_20220412T162841_B12.jp2",
-                    "T16TFM_20220412T162841_B8A.jp2",
-                    "T16TFM_20220412T162841_TCI.jp2",
-                    "T16TFM_20190412T162841_B01.jp2",
-                    "T16TFM_20190412T162841_B02.jp2",
-                    "T16TFM_20190412T162841_B03.jp2",
-                    "T16TFM_20190412T162841_B04.jp2",
-                    "T16TFM_20190412T162841_B05.jp2",
-                    "T16TFM_20190412T162841_B06.jp2",
-                    "T16TFM_20190412T162841_B07.jp2",
-                    "T16TFM_20190412T162841_B08.jp2",
-                    "T16TFM_20190412T162841_B09.jp2",
-                    "T16TFM_20190412T162841_B10.jp2",
-                    "T16TFM_20190412T162841_B11.jp2",
-                    "T16TFM_20190412T162841_B12.jp2",
-                    "T16TFM_20190412T162841_B8A.jp2",
-                    "T16TFM_20190412T162841_TCI.jp2",
-                ]
-            }
-        }
-    },
-    # Copernicus Open Access Hub
-    "S2A_MSIL2A_20220414T110751_N0400_R108_T26EMU_20220414T165533.SAFE": {
-        "GRANULE": {
-            "L2A_T26EMU_A035569_20220414T110747": {
-                "IMG_DATA": {
-                    "R10m": [
-                        "T26EMU_20220414T110751_AOT_10m.jp2",
-                        "T26EMU_20220414T110751_B02_10m.jp2",
-                        "T26EMU_20220414T110751_B03_10m.jp2",
-                        "T26EMU_20220414T110751_B04_10m.jp2",
-                        "T26EMU_20220414T110751_B08_10m.jp2",
-                        "T26EMU_20220414T110751_TCI_10m.jp2",
-                        "T26EMU_20220414T110751_WVP_10m.jp2",
-                        "T26EMU_20190414T110751_AOT_10m.jp2",
-                        "T26EMU_20190414T110751_B02_10m.jp2",
-                        "T26EMU_20190414T110751_B03_10m.jp2",
-                        "T26EMU_20190414T110751_B04_10m.jp2",
-                        "T26EMU_20190414T110751_B08_10m.jp2",
-                        "T26EMU_20190414T110751_TCI_10m.jp2",
-                        "T26EMU_20190414T110751_WVP_10m.jp2",
-                    ],
-                    "R20m": [
-                        "T26EMU_20220414T110751_AOT_20m.jp2",
-                        "T26EMU_20220414T110751_B01_20m.jp2",
-                        "T26EMU_20220414T110751_B02_20m.jp2",
-                        "T26EMU_20220414T110751_B03_20m.jp2",
-                        "T26EMU_20220414T110751_B04_20m.jp2",
-                        "T26EMU_20220414T110751_B05_20m.jp2",
-                        "T26EMU_20220414T110751_B06_20m.jp2",
-                        "T26EMU_20220414T110751_B07_20m.jp2",
-                        "T26EMU_20220414T110751_B11_20m.jp2",
-                        "T26EMU_20220414T110751_B12_20m.jp2",
-                        "T26EMU_20220414T110751_B8A_20m.jp2",
-                        "T26EMU_20220414T110751_SCL_20m.jp2",
-                        "T26EMU_20220414T110751_TCI_20m.jp2",
-                        "T26EMU_20220414T110751_WVP_20m.jp2",
-                        "T26EMU_20190414T110751_AOT_20m.jp2",
-                        "T26EMU_20190414T110751_B01_20m.jp2",
-                        "T26EMU_20190414T110751_B02_20m.jp2",
-                        "T26EMU_20190414T110751_B03_20m.jp2",
-                        "T26EMU_20190414T110751_B04_20m.jp2",
-                        "T26EMU_20190414T110751_B05_20m.jp2",
-                        "T26EMU_20190414T110751_B06_20m.jp2",
-                        "T26EMU_20190414T110751_B07_20m.jp2",
-                        "T26EMU_20190414T110751_B11_20m.jp2",
-                        "T26EMU_20190414T110751_B12_20m.jp2",
-                        "T26EMU_20190414T110751_B8A_20m.jp2",
-                        "T26EMU_20190414T110751_SCL_20m.jp2",
-                        "T26EMU_20190414T110751_TCI_20m.jp2",
-                        "T26EMU_20190414T110751_WVP_20m.jp2",
-                    ],
-                    "R60m": [
-                        "T26EMU_20220414T110751_AOT_60m.jp2",
-                        "T26EMU_20220414T110751_B01_60m.jp2",
-                        "T26EMU_20220414T110751_B02_60m.jp2",
-                        "T26EMU_20220414T110751_B03_60m.jp2",
-                        "T26EMU_20220414T110751_B04_60m.jp2",
-                        "T26EMU_20220414T110751_B05_60m.jp2",
-                        "T26EMU_20220414T110751_B06_60m.jp2",
-                        "T26EMU_20220414T110751_B07_60m.jp2",
-                        "T26EMU_20220414T110751_B09_60m.jp2",
-                        "T26EMU_20220414T110751_B11_60m.jp2",
-                        "T26EMU_20220414T110751_B12_60m.jp2",
-                        "T26EMU_20220414T110751_B8A_60m.jp2",
-                        "T26EMU_20220414T110751_SCL_60m.jp2",
-                        "T26EMU_20220414T110751_TCI_60m.jp2",
-                        "T26EMU_20220414T110751_WVP_60m.jp2",
-                        "T26EMU_20190414T110751_AOT_60m.jp2",
-                        "T26EMU_20190414T110751_B01_60m.jp2",
-                        "T26EMU_20190414T110751_B02_60m.jp2",
-                        "T26EMU_20190414T110751_B03_60m.jp2",
-                        "T26EMU_20190414T110751_B04_60m.jp2",
-                        "T26EMU_20190414T110751_B05_60m.jp2",
-                        "T26EMU_20190414T110751_B06_60m.jp2",
-                        "T26EMU_20190414T110751_B07_60m.jp2",
-                        "T26EMU_20190414T110751_B09_60m.jp2",
-                        "T26EMU_20190414T110751_B11_60m.jp2",
-                        "T26EMU_20190414T110751_B12_60m.jp2",
-                        "T26EMU_20190414T110751_B8A_60m.jp2",
-                        "T26EMU_20190414T110751_SCL_60m.jp2",
-                        "T26EMU_20190414T110751_TCI_60m.jp2",
-                        "T26EMU_20190414T110751_WVP_60m.jp2",
-                    ],
-                }
-            }
-        }
-    },
-}
+filenames: FILENAME_HIERARCHY = {}
+
+base_path = "S2A_MSIL1C_20220412T162841_N0400_R083_{tile_id}_20220412T202300.SAFE"
+granule_path = "L1C_{tile_id}_A035544_20220412T163959"
+bands = [
+    "B01.jp2",
+    "B02.jp2",
+    "B03.jp2",
+    "B04.jp2",
+    "B05.jp2",
+    "B06.jp2",
+    "B07.jp2",
+    "B08.jp2",
+    "B09.jp2",
+    "B10.jp2",
+    "B11.jp2",
+    "B12.jp2",
+    "B8A.jp2",
+    "TCI.jp2",
+]
+
+for i in range(16, 26):
+    tile_id = f"T{i}TFM"
+    safe_file = base_path.format(tile_id=tile_id)
+    granule = granule_path.format(tile_id=tile_id)
+    img_data = [f"{tile_id}_20220412T162841_{band}" for band in bands] + [
+        f"{tile_id}_20190412T162841_{band}" for band in bands
+    ]
+
+    filenames[safe_file] = {"GRANULE": {granule: {"IMG_DATA": img_data}}}
 
 
-def create_file(path: str, dtype: str, num_channels: int) -> None:
+def create_file(path: str, dtype: str, num_channels: int, x_offset: int) -> None:
     res = 10
     root, _ = os.path.splitext(path)
     if root.endswith("m"):
@@ -155,7 +60,7 @@ def create_file(path: str, dtype: str, num_channels: int) -> None:
     profile["dtype"] = dtype
     profile["count"] = num_channels
     profile["crs"] = CRS.from_epsg(32616)
-    profile["transform"] = Affine(res, 0.0, 399960.0, 0.0, -res, 4500000.0)
+    profile["transform"] = Affine(res, 0.0, 399960.0 + x_offset, 0.0, -res, 4500000.0)
     profile["height"] = round(SIZE * 10 / res)
     profile["width"] = round(SIZE * 10 / res)
 
@@ -172,6 +77,8 @@ def create_file(path: str, dtype: str, num_channels: int) -> None:
 
 
 def create_directory(directory: str, hierarchy: FILENAME_HIERARCHY) -> None:
+    global current_x_offset
+
     if isinstance(hierarchy, dict):
         # Recursive case
         for key, value in hierarchy.items():
@@ -180,9 +87,11 @@ def create_directory(directory: str, hierarchy: FILENAME_HIERARCHY) -> None:
             create_directory(path, value)
     else:
         # Base case
+        print(current_x_offset)
         for value in hierarchy:
             path = os.path.join(directory, value)
-            create_file(path, dtype="uint16", num_channels=1)
+            create_file(path, dtype="uint16", num_channels=1, x_offset=current_x_offset)
+        current_x_offset += SIZE
 
 
 if __name__ == "__main__":
