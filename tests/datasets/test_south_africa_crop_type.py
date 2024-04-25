0abaa8ef821398b9210dbaa717c0670ee8b2bf2f
Adam J. Stewart <ajstewart426@gmail.com>
2024-04-25 19:25:46 +0200

    100% test coverage

--- tests/datasets/test_south_africa_crop_type.py
+++ tests/datasets/test_south_africa_crop_type.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import pytest
 import torch
 import torch.nn as nn
+from _pytest.fixtures import SubRequest
 from rasterio.crs import CRS
 
 from torchgeo.datasets import (
@@ -21,11 +22,12 @@ from torchgeo.datasets import (
 
 
 class TestSouthAfricaCropType:
-    @pytest.fixture
-    def dataset(self) -> SouthAfricaCropType:
+    @pytest.fixture(params=[SouthAfricaCropType.s1_bands, SouthAfricaCropType.s2_bands])
+    def dataset(self, request: SubRequest) -> SouthAfricaCropType:
         path = os.path.join("tests", "data", "south_africa_crop_type")
+        bands = request.param
         transforms = nn.Identity()
-        return SouthAfricaCropType(paths=path, transforms=transforms)
+        return SouthAfricaCropType(path, bands=bands, transforms=transforms)
 
     def test_getitem(self, dataset: SouthAfricaCropType) -> None:
         x = dataset[dataset.bounds]
@@ -49,15 +51,19 @@ class TestSouthAfricaCropType:
         with pytest.raises(DatasetNotFoundError, match="Dataset not found"):
             SouthAfricaCropType(str(tmp_path))
 
-    def test_plot(self, dataset: SouthAfricaCropType) -> None:
-        x = dataset[dataset.bounds]
-        dataset.plot(x, suptitle="Test")
+    def test_plot(self) -> None:
+        path = os.path.join("tests", "data", "south_africa_crop_type")
+        ds = SouthAfricaCropType(path)
+        x = ds[ds.bounds]
+        ds.plot(x, suptitle="Test")
         plt.close()
 
-    def test_plot_prediction(self, dataset: SouthAfricaCropType) -> None:
-        x = dataset[dataset.bounds]
+    def test_plot_prediction(self) -> None:
+        path = os.path.join("tests", "data", "south_africa_crop_type")
+        ds = SouthAfricaCropType(path)
+        x = ds[ds.bounds]
         x["prediction"] = x["mask"].clone()
-        dataset.plot(x, suptitle="Prediction")
+        ds.plot(x, suptitle="Prediction")
         plt.close()
 
     def test_invalid_query(self, dataset: SouthAfricaCropType) -> None:
@@ -67,11 +73,12 @@ class TestSouthAfricaCropType:
         ):
             dataset[query]
 
-    def test_rgb_bands_absent_plot(self, dataset: SouthAfricaCropType) -> None:
+    def test_rgb_bands_absent_plot(self) -> None:
+        path = os.path.join("tests", "data", "south_africa_crop_type")
         with pytest.raises(
             RGBBandsMissingError, match="Dataset does not contain some of the RGB bands"
         ):
-            ds = SouthAfricaCropType(dataset.paths, bands=["B01", "B02", "B05"])
+            ds = SouthAfricaCropType(path, bands=["B01", "B02", "B05"])
             x = ds[ds.bounds]
             ds.plot(x, suptitle="Test")
             plt.close()
