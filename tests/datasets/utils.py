13a37d448e2bdff835868c880407467c881a1cf0
Adam J. Stewart <ajstewart426@gmail.com>
2024-05-04 12:21:40 +0200

    Simpler implementation

--- tests/datasets/utils.py
+++ tests/datasets/utils.py
@@ -1,8 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-# https://github.com/pytest-dev/pytest/issues/7469#issuecomment-2094101076
-from _pytest.outcomes import Skipped
+import pytest
 
 
 def importandskip(modname: str, reason: str | None = None) -> None:
@@ -12,14 +11,11 @@ def importandskip(modname: str, reason: str | None = None) -> None:
         modname: The name of the module to import.
         reason: If given, this reason is shown as the message when the module can
             be imported.
-
-    Raises:
-        Skipped: If *modname* can be imported.
     """
     try:
         __import__(modname)
         if reason is None:
             reason = f'could import {modname!r}'
-        raise Skipped(reason, allow_module_level=True) from None
+        pytest.skip(reason)
     except ImportError:
         pass
