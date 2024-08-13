875f8f2d8f10b8127f1e4eee25697d8776476bd1
Adam J. Stewart <ajstewart426@gmail.com>
2024-08-13 19:25:25 +0200

    Set PATHEXT in conftest.py

--- tests/datasets/conftest.py
+++ tests/datasets/conftest.py
@@ -13,4 +13,5 @@ from torchgeo.datasets.utils import Executable, which
 def azcopy(monkeypatch: MonkeyPatch) -> Executable:
     path = os.path.dirname(os.path.realpath(__file__))
     monkeypatch.setenv('PATH', path, prepend=os.pathsep)
+    monkeypatch.setenv('PATHEXT', '.py', prepend=os.pathsep)
     return which('azcopy')
