6f041ee25a34a939d760c45bd3b59c47efd407d1
Adrian Tofting <11158599+adriantre@users.noreply.github.com>
2024-08-13 21:26:19 +0200

    Add test for https/curl files

--- tests/datasets/test_geo.py
+++ tests/datasets/test_geo.py
@@ -215,109 +215,6 @@ class TestGeoDataset:
         )
 
 
-class TestZippedDataset:
-    @pytest.mark.parametrize(
-        'temp_archive', [os.path.join('tests', 'data', 'vector')], indirect=True
-    )
-    def test_zipped_file(self, temp_archive: tuple[str, str]) -> None:
-        _, dir_zipped = temp_archive
-        filename = 'vector_2024.geojson'
-
-        # Cannot use os.path.join here, as dir_zipped has .zip extension.
-        # Using '!' as separator between archive and file as per Apache Commons VFS
-        # https://rasterio.readthedocs.io/en/latest/topics/vsi.html
-        specific_file_zipped = f'{dir_zipped}!{filename}'
-
-        files_found = CustomGeoDataset(paths=f'zip://{specific_file_zipped}').files
-        assert len(files_found) == 1
-        file = str(files_found[0])
-        assert file.endswith(filename)
-
-    @pytest.mark.parametrize(
-        'temp_archive',
-        [
-            os.path.join(
-                'tests',
-                'data',
-                'sentinel2',
-                'S2A_MSIL2A_20220414T110751_N0400_R108_T26EMU_20220414T165533.SAFE',
-            )
-        ],
-        indirect=True,
-    )
-    def test_zipped_specific_file_nested_dir(
-        self, temp_archive: tuple[str, str]
-    ) -> None:
-        dir_not_zipped, dir_zipped = temp_archive
-
-        # Listing, instead of hardcoding filepath
-        filename_glob = Sentinel2.filename_glob.format('B02')
-        specific_file_not_zipped = list_directory_recursive(
-            dir_not_zipped, filename_glob
-        )[0]
-        filepath_within_dir = specific_file_not_zipped.replace(dir_not_zipped, '')
-
-        specific_file_zipped = f'zip://{dir_zipped}!{filepath_within_dir}'
-
-        files_found = CustomGeoDataset(paths=specific_file_zipped).files
-        assert len(files_found) == 1
-        file = str(files_found[0])
-        assert file.endswith(filepath_within_dir)
-
-    @pytest.mark.parametrize(
-        'temp_archive',
-        [
-            os.path.join(
-                'tests',
-                'data',
-                'sentinel2',
-                'S2A_MSIL2A_20220414T110751_N0400_R108_T26EMU_20220414T165533.SAFE',
-            )
-        ],
-        indirect=True,
-    )
-    def test_zipped_sentinel2_dataset_specific_file_not_exist(
-        self, temp_archive: tuple[str, str]
-    ) -> None:
-        dir_not_zipped, dir_zipped = temp_archive
-        file_zipped_not_existing = os.path.join(dir_zipped, 'non_existing_file.tif')
-        with pytest.warns(UserWarning, match='Path was ignored.'):
-            assert (
-                len(CustomGeoDataset(paths=f'zip://{file_zipped_not_existing}').files)
-                == 0
-            )
-
-    @pytest.mark.parametrize(
-        'temp_archive',
-        [
-            os.path.join(
-                'tests',
-                'data',
-                'sentinel2',
-                'S2A_MSIL2A_20220414T110751_N0400_R108_T26EMU_20220414T165533.SAFE',
-            )
-        ],
-        indirect=True,
-    )
-    def test_zipped_sentinel2_dataset_root(self, temp_archive: tuple[str, str]) -> None:
-        dir_not_zipped, dir_zipped = temp_archive
-        bands = ['B04', 'B03', 'B02']
-        transforms = nn.Identity()
-        cache = False
-
-        files_not_zipped = Sentinel2(
-            paths=dir_not_zipped, bands=bands, transforms=transforms, cache=cache
-        ).files
-
-        files_zipped = Sentinel2(
-            paths=f'zip://{dir_zipped}', bands=bands, transforms=transforms, cache=cache
-        ).files
-
-        basenames_not_zipped = [Path(path).stem for path in files_not_zipped]
-        basenames_zipped = [Path(path).stem for path in files_zipped]
-        assert basenames_zipped == basenames_not_zipped
-
-
 class TestRasterDataset:
     @pytest.fixture(params=zip([['R', 'G', 'B'], None], [True, False]))
     def naip(self, request: SubRequest) -> NAIP:
@@ -444,6 +341,132 @@ class TestRasterDataset:
 
         with pytest.raises(AssertionError, match=msg):
             CustomSentinelDataset(root, bands=bands, transforms=transforms, cache=cache)
+
+
+class TestVirtualFilesystems:
+    @pytest.mark.parametrize(
+        'temp_archive', [os.path.join('tests', 'data', 'vector')], indirect=True
+    )
+    def test_zipped_file(self, temp_archive: tuple[str, str]) -> None:
+        _, dir_zipped = temp_archive
+        filename = 'vector_2024.geojson'
+
+        # Cannot use os.path.join here, as dir_zipped has .zip extension.
+        # Using '!' as separator between archive and file as per Apache Commons VFS
+        # https://rasterio.readthedocs.io/en/latest/topics/vsi.html
+        specific_file_zipped = f'{dir_zipped}!{filename}'
+
+        files_found = CustomGeoDataset(paths=f'zip://{specific_file_zipped}').files
+        assert len(files_found) == 1
+        file = str(files_found[0])
+        assert file.endswith(filename)
+
+    @pytest.mark.parametrize(
+        'temp_archive',
+        [
+            os.path.join(
+                'tests',
+                'data',
+                'sentinel2',
+                'S2A_MSIL2A_20220414T110751_N0400_R108_T26EMU_20220414T165533.SAFE',
+            )
+        ],
+        indirect=True,
+    )
+    def test_zipped_specific_file_dir(self, temp_archive: tuple[str, str]) -> None:
+        dir_not_zipped, dir_zipped = temp_archive
+
+        # Listing, instead of hardcoding filepath
+        filename_glob = Sentinel2.filename_glob.format('B02')
+        specific_file_not_zipped = list_directory_recursive(
+            dir_not_zipped, filename_glob
+        )[0]
+        filepath_within_dir = specific_file_not_zipped.replace(dir_not_zipped, '')
+
+        specific_file_zipped = f'zip://{dir_zipped}!{filepath_within_dir}'
+
+        files_found = CustomGeoDataset(paths=specific_file_zipped).files
+        assert len(files_found) == 1
+        file = str(files_found[0])
+        assert file.endswith(filepath_within_dir)
+
+    @pytest.mark.parametrize(
+        'temp_archive',
+        [
+            os.path.join(
+                'tests',
+                'data',
+                'sentinel2',
+                'S2A_MSIL2A_20220414T110751_N0400_R108_T26EMU_20220414T165533.SAFE',
+            )
+        ],
+        indirect=True,
+    )
+    def test_zipped_path_non_existing_file(self, temp_archive: tuple[str, str]) -> None:
+        dir_not_zipped, dir_zipped = temp_archive
+        file_zipped_not_existing = os.path.join(dir_zipped, 'non_existing_file.tif')
+        with pytest.warns(UserWarning, match='Path was ignored.'):
+            assert (
+                len(CustomGeoDataset(paths=f'zip://{file_zipped_not_existing}').files)
+                == 0
+            )
+
+    @pytest.mark.parametrize(
+        'temp_archive',
+        [
+            os.path.join(
+                'tests',
+                'data',
+                'sentinel2',
+                'S2A_MSIL2A_20220414T110751_N0400_R108_T26EMU_20220414T165533.SAFE',
+            )
+        ],
+        indirect=True,
+    )
+    def test_zipped_directory(self, temp_archive: tuple[str, str]) -> None:
+        dir_not_zipped, dir_zipped = temp_archive
+        bands = Sentinel2.rgb_bands
+        transforms = nn.Identity()
+        cache = False
+
+        files_not_zipped = Sentinel2(
+            paths=dir_not_zipped, bands=bands, transforms=transforms, cache=cache
+        ).files
+
+        files_zipped = Sentinel2(
+            paths=f'zip://{dir_zipped}', bands=bands, transforms=transforms, cache=cache
+        ).files
+
+        basenames_not_zipped = [Path(path).stem for path in files_not_zipped]
+        basenames_zipped = [Path(path).stem for path in files_zipped]
+        assert basenames_zipped == basenames_not_zipped
+
+    def test_http_files(self) -> None:
+        url = 'https://github.com/microsoft/torchgeo/raw/main'
+        dir = os.path.join(
+            'tests',
+            'data',
+            'sentinel2',
+            'S2A_MSIL2A_20220414T110751_N0400_R108_T26EMU_20220414T165533.SAFE',
+            'GRANULE',
+            'L2A_T26EMU_A035569_20220414T110747',
+            'IMG_DATA',
+            'R10m',
+        )
+        files = [
+            'T26EMU_20220414T110751_B04_10m.jp2',
+            'T26EMU_20190414T110751_B03_10m.jp2',
+        ]
+        paths = [os.path.join(url, dir, file) for file in files]
+        bands = Sentinel2.rgb_bands
+        transforms = nn.Identity()
+        cache = False
+
+        files_found = Sentinel2(
+            paths=paths, bands=bands, transforms=transforms, cache=cache
+        ).files
+
+        assert len(files_found) == 1
 
 
 class TestVectorDataset:
