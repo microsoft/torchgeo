dbbcec7edb21c9d7b34840d20e1e6f7966aa3bf0
Adrian Tofting <11158599+adriantre@users.noreply.github.com>
2024-08-13 21:33:50 +0200

    Define should_warn outside if

--- torchgeo/datasets/geo.py
+++ torchgeo/datasets/geo.py
@@ -307,17 +307,16 @@ class GeoDataset(Dataset[dict[str, Any]], abc.ABC):
         # Using set to remove any duplicates if directories are overlapping
         files: set[Path] = set()
         for path in paths:
+            file_not_found = True
             if os.path.isfile(path):
                 if fnmatch.fnmatch(str(path), os.path.join('*', self.filename_glob)):
                     files.add(path)
-                else:
-                    should_warn = True
+                    file_not_found = False
             elif files_found := set(list_directory_recursive(path, self.filename_glob)):
                 files |= files_found
-            else:
-                should_warn = True
+                file_not_found = False
 
-            if 'should_warn' in locals():
+            if file_not_found and not hasattr(self, 'download'):
                 warnings.warn(
                     f"Could not find any relevant files for provided path '{path}'. "
                     f'Path was ignored.',
