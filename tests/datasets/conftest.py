839b912e05de1fe8048ca1ab2860b132dad19fb2
Adam J. Stewart <ajstewart426@gmail.com>
2024-08-15 10:46:25 +0200

    Try .bat file

--- tests/datasets/conftest.py
+++ tests/datasets/conftest.py
@@ -13,5 +13,4 @@ from torchgeo.datasets.utils import Executable, which
 def azcopy(monkeypatch: MonkeyPatch) -> Executable:
     path = os.path.dirname(os.path.realpath(__file__))
     monkeypatch.setenv('PATH', path, prepend=os.pathsep)
-    monkeypatch.setenv('PATHEXT', '.py', prepend=os.pathsep)
     return which('azcopy')
