f1c011f7e27131164cf51f8ea5d86cbd2f0ed511
Adam J. Stewart <ajstewart426@gmail.com>
2024-08-20 16:53:22 +0200

    Fix remote tests

--- torchgeo/models/swin.py
+++ torchgeo/models/swin.py
@@ -296,6 +296,12 @@ def swin_v2_t(
     model: SwinTransformer = torchvision.models.swin_v2_t(weights=None, *args, **kwargs)
 
     if weights:
+        num_channels = weights.meta['in_chans']
+        out_channels = model.features[0][0].out_channels
+        # https://github.com/allenai/satlaspretrain_models/blob/main/satlaspretrain_models/models/backbones.py#L27
+        model.features[0][0] = torch.nn.Conv2d(
+            num_channels, out_channels, kernel_size=(4, 4), stride=(4, 4)
+        )
         missing_keys, unexpected_keys = model.load_state_dict(
             weights.get_state_dict(progress=True), strict=False
         )
@@ -329,6 +335,12 @@ def swin_v2_b(
     model: SwinTransformer = torchvision.models.swin_v2_b(weights=None, *args, **kwargs)
 
     if weights:
+        num_channels = weights.meta['in_chans']
+        out_channels = model.features[0][0].out_channels
+        # https://github.com/allenai/satlaspretrain_models/blob/main/satlaspretrain_models/models/backbones.py#L27
+        model.features[0][0] = torch.nn.Conv2d(
+            num_channels, out_channels, kernel_size=(4, 4), stride=(4, 4)
+        )
         missing_keys, unexpected_keys = model.load_state_dict(
             weights.get_state_dict(progress=True), strict=False
         )
