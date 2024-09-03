6576b400197774a8d8f611567c734b2539b5f136
Adam J. Stewart <ajstewart426@gmail.com>
2024-09-03 16:36:44 +0200

    GeoDataset: allow a mix of str and pathlib paths (#2270)
    * GeoDataset: allow a mix of str and pathlib paths
    
    * Update type hints
    
    * Simpler str conversion

--- torchgeo/datasets/geo.py
+++ torchgeo/datasets/geo.py
@@ -291,7 +291,7 @@ class GeoDataset(Dataset[dict[str, Any]], abc.ABC):
         self._res = new_res
 
     @property
-    def files(self) -> list[Path]:
+    def files(self) -> list[str]:
         """A list of all files in the dataset.
 
         Returns:
@@ -306,7 +306,7 @@ class GeoDataset(Dataset[dict[str, Any]], abc.ABC):
             paths = self.paths
 
         # Using set to remove any duplicates if directories are overlapping
-        files: set[Path] = set()
+        files: set[str] = set()
         for path in paths:
             if os.path.isdir(path):
                 pathname = os.path.join(path, '**', self.filename_glob)
@@ -314,7 +314,7 @@ class GeoDataset(Dataset[dict[str, Any]], abc.ABC):
             elif (os.path.isfile(path) or path_is_vsi(path)) and fnmatch.fnmatch(
                 str(path), f'*{self.filename_glob}'
             ):
-                files.add(path)
+                files.add(str(path))
             elif not hasattr(self, 'download'):
                 warnings.warn(
                     f"Could not find any relevant files for provided path '{path}'. "
