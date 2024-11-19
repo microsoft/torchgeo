0f375fff4207a8eeb243dafc53aef4e5fccb823d
Th-Yash <thakoryashpal755@gmail.com>
2024-11-19 14:29:39 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,14 +30,6 @@ version = '.'.join(torchgeo.__version__.split('.')[:2])
 release = torchgeo.__version__
 
 
-html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
-
-# Tell Jinja2 templates the build is running on Read the Docs
-if os.environ.get("READTHEDOCS", "") == "True":
-    if "html_context" not in globals():
-        html_context = {}
-    html_context["READTHEDOCS"] = True
-    
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
