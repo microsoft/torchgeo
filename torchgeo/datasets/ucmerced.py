398d26bb01d75e601e1205b0786304e06ca2574e
Adam J. Stewart <ajstewart426@gmail.com>
2024-12-03 21:28:40 +0100

    UC Merced: redistribute split files on Hugging Face (#2433)

--- torchgeo/datasets/ucmerced.py
+++ torchgeo/datasets/ucmerced.py
@@ -66,17 +66,17 @@ class UCMerced(NonGeoClassificationDataset):
     * https://dl.acm.org/doi/10.1145/1869790.1869829
     """
 
-    url = 'https://hf.co/datasets/torchgeo/ucmerced/resolve/d0af6e2eeea2322af86078068bd83337148a2149/UCMerced_LandUse.zip'
+    url = 'https://hf.co/datasets/torchgeo/ucmerced/resolve/7c5ef3454d9b1cccfa7ccde0c01fc8f00a45909a/'
     filename = 'UCMerced_LandUse.zip'
     md5 = '5b7ec56793786b6dc8a908e8854ac0e4'
 
     base_dir = os.path.join('UCMerced_LandUse', 'Images')
 
     splits = ('train', 'val', 'test')
-    split_urls: ClassVar[dict[str, str]] = {
-        'train': 'https://storage.googleapis.com/remote_sensing_representations/uc_merced-train.txt',
-        'val': 'https://storage.googleapis.com/remote_sensing_representations/uc_merced-val.txt',
-        'test': 'https://storage.googleapis.com/remote_sensing_representations/uc_merced-test.txt',
+    split_filenames: ClassVar[dict[str, str]] = {
+        'train': 'uc_merced-train.txt',
+        'val': 'uc_merced-val.txt',
+        'test': 'uc_merced-test.txt',
     }
     split_md5s: ClassVar[dict[str, str]] = {
         'train': 'f2fb12eb2210cfb53f93f063a35ff374',
@@ -113,7 +113,7 @@ class UCMerced(NonGeoClassificationDataset):
         self._verify()
 
         valid_fns = set()
-        with open(os.path.join(self.root, f'uc_merced-{split}.txt')) as f:
+        with open(os.path.join(self.root, self.split_filenames[split])) as f:
             for fn in f:
                 valid_fns.add(fn.strip())
 
@@ -173,16 +173,12 @@ class UCMerced(NonGeoClassificationDataset):
     def _download(self) -> None:
         """Download the dataset."""
         download_url(
-            self.url,
-            self.root,
-            filename=self.filename,
-            md5=self.md5 if self.checksum else None,
+            self.url + self.filename, self.root, md5=self.md5 if self.checksum else None
         )
         for split in self.splits:
             download_url(
-                self.split_urls[split],
+                self.url + self.split_filenames[split],
                 self.root,
-                filename=f'uc_merced-{split}.txt',
                 md5=self.split_md5s[split] if self.checksum else None,
             )
 
