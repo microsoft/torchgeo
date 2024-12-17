befe1f24707dcead50f213da31eed33cc1ada701
preethatr07 <preethatr2000@gmail.com>
2024-12-17 19:08:17 +0100

    Update inaturalist.py with plot class

--- torchgeo/datasets/inaturalist.py
+++ torchgeo/datasets/inaturalist.py
@@ -11,6 +11,11 @@ from typing import Any
 import pandas as pd
 from rasterio.crs import CRS
 
+import matplotlib.pyplot as plt
+import geopandas as gpd
+from matplotlib import cm
+import numpy as np
+
 from .errors import DatasetNotFoundError
 from .geo import GeoDataset
 from .utils import BoundingBox, Path, disambiguate_timestamp
@@ -110,3 +115,85 @@ class INaturalist(GeoDataset):
         sample = {'crs': self.crs, 'bounds': bboxes}
 
         return sample
+
+
+
+    def plot(self, query: BoundingBox = None, time_range: tuple = None, cmap: str = 'viridis') -> None:
+        """ Plot the observations in a map when given a geographical bounding box as well as time frame which is optional.
+
+        Args:
+            query: Optional case for bounding box which is meant to cut the observations (minx,maxx,miny,maxy,mint,maxt).
+            time_range: Optional filtering in terms of exact date and location for (start_time, end_time).
+            cmap: A color map for the time sequence.
+        """
+        # Step 1: If a query and/or time_range is provided, filter the dataset based on this criteria. 
+        data = self._filter_data(query, time_range)
+
+        # Step 2: Prepare a GeoDataFrame for geospatial visualization by geographic reference.
+        gdf = gpd.GeoDataFrame(
+            data,
+            geometry=gpd.points_from_xy(data['longitude'], data['latitude']),
+            crs=self._crs
+        )
+
+        
+        # Step 3: Illustrate the observations that were made.
+        fig, ax = plt.subplots(figsize=(10, 8))
+        gdf.plot(ax=ax, color=self._get_color_by_time(data, cmap=cmap), markersize=10, alpha=0.7)
+
+
+        # Step 4: Incorporate cartographic refinements (for example, coastlines, gridlines) to the map.
+        ax.set_title('iNaturalist Observations', fontsize=16)
+        ax.set_xlabel('Longitude')
+        ax.set_ylabel('Latitude')
+        ax.grid(True)
+
+        # Insert a colorbar to show the progression of time 
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=data['observed_on'].min(), vmax=data['observed_on'].max()))
+        sm.set_array([])
+        plt.colorbar(sm, ax=ax, label='Time of Observation')
+        plt.show()
+
+    def _filter_data(self, query: BoundingBox, time_range: tuple) -> pd.DataFrame:
+        """This is a Helper function that helps to filter the dataset based on the bounding box provided by the query and a time range."""      
+        # First stage filter on bounding box
+        if query:
+            data = self._filter_by_bbox(query)
+        else:
+            data = self._load_data()
+
+        # Now we filter on time range
+        if time_range:
+            data = data[(data['observed_on'] >= time_range[0]) & (data['observed_on'] <= time_range[1])]
+        return data
+    
+
+    def _get_color_by_time(self, data: pd.DataFrame, cmap: str) -> np.ndarray:
+        """Creates a mapping of time with color."""
+        norm = plt.Normalize(vmin=data['observed_on'].min(), vmax=data['observed_on'].max())
+        colormap = cm.get_cmap(cmap)
+        return colormap(norm(data['observed_on']))
+    
+
+    def _filter_by_bbox(self, query: BoundingBox) -> pd.DataFrame:
+        """Helper function that broadens filters with bounding box type parameters to filter the data frame."""
+        minx, maxx, miny, maxy, _, _ = query
+        data = self._load_data()
+        return data[(data['longitude'] >= minx) & (data['longitude'] <= maxx) & 
+                    (data['latitude'] >= miny) & (data['latitude'] <= maxy)]
+
+    def _load_data(self) -> pd.DataFrame:
+        """Tries to get dataset from the CSV file."""      
+        files = glob.glob(os.path.join(self.root, '**.csv'))
+        if not files:
+            raise DatasetNotFoundError(self)            
+        
+        data = pd.read_csv(
+            files[0],
+            engine='c',
+            usecols=['observed_on', 'latitude', 'longitude'],
+        )
+        return data
+
+
+
