5d9e671e63d6768c2c03530785db72e84c8009bb
lns-lns <go57cig@mytum.de>
2025-03-18 11:53:29 +0100

    Update reforestree.py

--- torchgeo/datamodules/reforestree.py
+++ torchgeo/datamodules/reforestree.py
@@ -1,6 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-
 """ReforesTree datamodule."""
 from typing import Any
 
@@ -12,7 +11,6 @@ from ..datasets import ReforesTree
 from ..samplers.utils import _to_tuple
 from ..transforms.transforms import _RandomNCrop
 from .geo import NonGeoDataModule
-
 
 class ReforesTreeDataModule(NonGeoDataModule):
     """LightningDataModule implementation for the ReforesTree dataset.
