e259fca072f3583d1fb6d363fb458aa6a14fdfd6
lns-lns <go57cig@mytum.de>
2025-03-18 12:00:40 +0100

    Update reforestree.py

--- torchgeo/datamodules/reforestree.py
+++ torchgeo/datamodules/reforestree.py
@@ -12,7 +12,6 @@ from ..samplers.utils import _to_tuple
 from ..transforms.transforms import _RandomNCrop
 from .geo import NonGeoDataModule
 
-
 class ReforesTreeDataModule(NonGeoDataModule):
     """LightningDataModule implementation for the ReforesTree dataset.
     
