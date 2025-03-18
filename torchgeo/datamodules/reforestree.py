d1e472f120c0e7a122d876df93f37bd0c64b9156
lns-lns <go57cig@mytum.de>
2025-03-18 13:29:08 +0100

    Update reforestree.py

--- torchgeo/datamodules/reforestree.py
+++ torchgeo/datamodules/reforestree.py
@@ -1,4 +1,3 @@
-
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 """ReforesTree datamodule."""
@@ -20,7 +19,7 @@ class ReforesTreeDataModule(NonGeoDataModule):
     .. versionadded:: 0.7
     
     """
-
+    
     def __init__(
         self, 
         batch_size: int = 64, 
