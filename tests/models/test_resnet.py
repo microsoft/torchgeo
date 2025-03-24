4020b993a53fcaa211171a8cd77d745dc1b0c9ba
Luca Colomba <lccol338@gmail.com>
2025-03-24 22:29:06 +0100

    introduced features_only fixture and removed duplicated code

--- tests/models/test_resnet.py
+++ tests/models/test_resnet.py
@@ -25,34 +25,22 @@ class TestResNet18:
     def weights(self, request: SubRequest) -> WeightsEnum:
         return request.param
 
+    @pytest.fixture(params=[True, False])
+    def features_only(self, request: SubRequest) -> bool:
+        return bool(request.param)
+
     @pytest.fixture
     def mocked_weights(
         self,
         tmp_path: Path,
         monkeypatch: MonkeyPatch,
         weights: WeightsEnum,
-        load_state_dict_from_url: None,
-    ) -> WeightsEnum:
-        path = tmp_path / f'{weights}.pth'
-        model = timm.create_model('resnet18', in_chans=weights.meta['in_chans'])
-        torch.save(model.state_dict(), path)
-        try:
-            monkeypatch.setattr(weights.value, 'url', str(path))
-        except AttributeError:
-            monkeypatch.setattr(weights, 'url', str(path))
-        return weights
-
-    @pytest.fixture
-    def mocked_weights_features_only(
-        self,
-        tmp_path: Path,
-        monkeypatch: MonkeyPatch,
-        weights: WeightsEnum,
+        features_only: bool,
         load_state_dict_from_url: None,
     ) -> WeightsEnum:
         path = tmp_path / f'{weights}.pth'
         model = timm.create_model(
-            'resnet18', in_chans=weights.meta['in_chans'], features_only=True
+            'resnet18', in_chans=weights.meta['in_chans'], features_only=features_only
         )
         torch.save(model.state_dict(), path)
         try:
@@ -64,13 +52,10 @@ class TestResNet18:
     def test_resnet(self) -> None:
         resnet18()
 
-    def test_resnet_weights(self, mocked_weights: WeightsEnum) -> None:
-        resnet18(weights=mocked_weights)
-
-    def test_resnet_weights_features_only(
-        self, mocked_weights_features_only: WeightsEnum
+    def test_resnet_weights(
+        self, mocked_weights: WeightsEnum, features_only: bool
     ) -> None:
-        resnet18(weights=mocked_weights_features_only, features_only=True)
+        resnet18(weights=mocked_weights, features_only=features_only)
 
     def test_bands(self, mocked_weights: WeightsEnum) -> None:
         if 'bands' in mocked_weights.meta:
@@ -93,34 +78,22 @@ class TestResNet50:
     def weights(self, request: SubRequest) -> WeightsEnum:
         return request.param
 
+    @pytest.fixture(params=[True, False])
+    def features_only(self, request: SubRequest) -> bool:
+        return bool(request.param)
+
     @pytest.fixture
     def mocked_weights(
         self,
         tmp_path: Path,
         monkeypatch: MonkeyPatch,
         weights: WeightsEnum,
-        load_state_dict_from_url: None,
-    ) -> WeightsEnum:
-        path = tmp_path / f'{weights}.pth'
-        model = timm.create_model('resnet50', in_chans=weights.meta['in_chans'])
-        torch.save(model.state_dict(), path)
-        try:
-            monkeypatch.setattr(weights.value, 'url', str(path))
-        except AttributeError:
-            monkeypatch.setattr(weights, 'url', str(path))
-        return weights
-
-    @pytest.fixture
-    def mocked_weights_features_only(
-        self,
-        tmp_path: Path,
-        monkeypatch: MonkeyPatch,
-        weights: WeightsEnum,
+        features_only: bool,
         load_state_dict_from_url: None,
     ) -> WeightsEnum:
         path = tmp_path / f'{weights}.pth'
         model = timm.create_model(
-            'resnet50', in_chans=weights.meta['in_chans'], features_only=True
+            'resnet50', in_chans=weights.meta['in_chans'], features_only=features_only
         )
         torch.save(model.state_dict(), path)
         try:
@@ -132,13 +105,10 @@ class TestResNet50:
     def test_resnet(self) -> None:
         resnet50()
 
-    def test_resnet_weights(self, mocked_weights: WeightsEnum) -> None:
-        resnet50(weights=mocked_weights)
-
-    def test_resnet_weights_features_only(
-        self, mocked_weights_features_only: WeightsEnum
+    def test_resnet_weights(
+        self, mocked_weights: WeightsEnum, features_only: bool
     ) -> None:
-        resnet50(weights=mocked_weights_features_only, features_only=True)
+        resnet50(weights=mocked_weights, features_only=features_only)
 
     def test_bands(self, mocked_weights: WeightsEnum) -> None:
         if 'bands' in mocked_weights.meta:
@@ -161,34 +131,22 @@ class TestResNet152:
     def weights(self, request: SubRequest) -> WeightsEnum:
         return request.param
 
+    @pytest.fixture(params=[True, False])
+    def features_only(self, request: SubRequest) -> bool:
+        return bool(request.param)
+
     @pytest.fixture
     def mocked_weights(
         self,
         tmp_path: Path,
         monkeypatch: MonkeyPatch,
         weights: WeightsEnum,
-        load_state_dict_from_url: None,
-    ) -> WeightsEnum:
-        path = tmp_path / f'{weights}.pth'
-        model = timm.create_model('resnet152', in_chans=weights.meta['in_chans'])
-        torch.save(model.state_dict(), path)
-        try:
-            monkeypatch.setattr(weights.value, 'url', str(path))
-        except AttributeError:
-            monkeypatch.setattr(weights, 'url', str(path))
-        return weights
-
-    @pytest.fixture
-    def mocked_weights_features_only(
-        self,
-        tmp_path: Path,
-        monkeypatch: MonkeyPatch,
-        weights: WeightsEnum,
+        features_only: bool,
         load_state_dict_from_url: None,
     ) -> WeightsEnum:
         path = tmp_path / f'{weights}.pth'
         model = timm.create_model(
-            'resnet152', in_chans=weights.meta['in_chans'], features_only=True
+            'resnet152', in_chans=weights.meta['in_chans'], features_only=features_only
         )
         torch.save(model.state_dict(), path)
         try:
@@ -200,13 +158,10 @@ class TestResNet152:
     def test_resnet(self) -> None:
         resnet152()
 
-    def test_resnet_weights(self, mocked_weights: WeightsEnum) -> None:
-        resnet152(weights=mocked_weights)
-
-    def test_resnet_weights_features_only(
-        self, mocked_weights_features_only: WeightsEnum
+    def test_resnet_weights(
+        self, mocked_weights: WeightsEnum, features_only: bool
     ) -> None:
-        resnet152(weights=mocked_weights_features_only, features_only=True)
+        resnet152(weights=mocked_weights, features_only=features_only)
 
     def test_bands(self, mocked_weights: WeightsEnum) -> None:
         if 'bands' in mocked_weights.meta:
--- tests/models/test_vit.py
+++ tests/models/test_vit.py
@@ -18,41 +18,29 @@ class TestViTSmall16:
     def weights(self, request: SubRequest) -> WeightsEnum:
         return request.param
 
+    @pytest.fixture(params=[True, False])
+    def features_only(self, request: SubRequest) -> bool:
+        if request.param:
+            # features_only arg in ViT supported only from timm 1.0.3
+            pytest.importorskip('timm', minversion='1.0.3')
+        return bool(request.param)
+
     @pytest.fixture
     def mocked_weights(
         self,
         tmp_path: Path,
         monkeypatch: MonkeyPatch,
         weights: WeightsEnum,
+        features_only: bool,
         load_state_dict_from_url: None,
     ) -> WeightsEnum:
         path = tmp_path / f'{weights}.pth'
         model = timm.create_model(
-            weights.meta['model'], in_chans=weights.meta['in_chans']
+            weights.meta['model'],
+            in_chans=weights.meta['in_chans'],
+            features_only=features_only,
         )
         torch.save(model.state_dict(), path)
-        try:
-            monkeypatch.setattr(weights.value, 'url', str(path))
-        except AttributeError:
-            monkeypatch.setattr(weights, 'url', str(path))
-        return weights
-
-    @pytest.fixture
-    def mocked_weights_features_only(
-        self,
-        tmp_path: Path,
-        monkeypatch: MonkeyPatch,
-        weights: WeightsEnum,
-        load_state_dict_from_url: None,
-    ) -> WeightsEnum:
-        # features_only args implemented in timm 1.0.3
-        pytest.importorskip('timm', minversion='1.0.3')
-        path = tmp_path / f'{weights}.pth'
-        model = timm.create_model(
-            weights.meta['model'], in_chans=weights.meta['in_chans'], features_only=True
-        )
-        # the actual ViT is stored in model attribute
-        torch.save(model.model.state_dict(), path)
         try:
             monkeypatch.setattr(weights.value, 'url', str(path))
         except AttributeError:
@@ -62,13 +50,10 @@ class TestViTSmall16:
     def test_vit(self) -> None:
         vit_small_patch16_224()
 
-    def test_vit_weights(self, mocked_weights: WeightsEnum) -> None:
-        vit_small_patch16_224(weights=mocked_weights)
-
-    def test_vit_weights_features_only(
-        self, mocked_weights_features_only: WeightsEnum
+    def test_vit_weights(
+        self, mocked_weights: WeightsEnum, features_only: bool
     ) -> None:
-        vit_small_patch16_224(weights=mocked_weights_features_only, features_only=True)
+        vit_small_patch16_224(weights=mocked_weights, features_only=features_only)
 
     def test_bands(self, mocked_weights: WeightsEnum) -> None:
         if 'bands' in mocked_weights.meta:
