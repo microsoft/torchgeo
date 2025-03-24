ba8a5d28ade170bee653451cf30d0aab9827ecc7
Luca Colomba <lccol338@gmail.com>
2025-03-25 00:03:04 +0100

    added check on timm minversion

--- tests/models/conftest.py
+++ tests/models/conftest.py
@@ -4,4 +4,6 @@ from _pytest.fixtures import SubRequest
 
 @pytest.fixture(params=[True, False])
 def features_only(request: SubRequest) -> bool:
+    if request.param:
+        pytest.importorskip('timm', minversion='1.0.3')
     return bool(request.param)
