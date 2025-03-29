d47784c0e745055d9deb6c1da0b20c464bea4cce
Adam J. Stewart <ajstewart426@gmail.com>
2025-03-29 18:26:26 +0100

    Skip minimum tests

--- tests/models/conftest.py
+++ tests/models/conftest.py
@@ -7,7 +7,6 @@ from _pytest.fixtures import SubRequest
 
 @pytest.fixture(params=[True, False])
 def features_only(request: SubRequest) -> bool:
-    if request.param:
-        # features_only arg in ViT supported only from timm 1.0.3
-        pytest.importorskip('timm', minversion='1.0.3')
+    # features_only arg in ViT supported only from timm 1.0.3
+    pytest.importorskip('timm', minversion='1.0.3')
     return bool(request.param)
