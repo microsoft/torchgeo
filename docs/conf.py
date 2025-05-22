aff1d8954bc6e7666b7001a6fbbffefaf3789363
Yi-Chia Chang <yichia3@illinois.edu>
2025-05-22 14:48:50 -0400

    fix doc test

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,7 @@ nitpick_ignore = [
     # Undocumented classes
     ('py:class', 'fiona.model.Feature'),
     ('py:class', 'kornia.augmentation._2d.intensity.base.IntensityAugmentationBase2D'),
+    ('py:class', 'kornia.augmentation._3d.geometric.base.GeometricAugmentationBase3D'),
     ('py:class', 'kornia.augmentation.base._AugmentationBase'),
     ('py:class', 'lightning.pytorch.utilities.types.LRSchedulerConfig'),
     ('py:class', 'lightning.pytorch.utilities.types.OptimizerConfig'),
