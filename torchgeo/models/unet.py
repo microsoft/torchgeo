401f4ca793bad893bf576769702f52e7f6ce0c9a
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2025-05-24 17:40:25 +0000

    add ai4g flood model

--- torchgeo/models/unet.py
+++ torchgeo/models/unet.py
@@ -8,17 +8,111 @@ from typing import Any
 import kornia.augmentation as K
 import segmentation_models_pytorch as smp
 import torch
+from kornia.constants import Resample
+from kornia.contrib import Lambda
 from segmentation_models_pytorch import Unet
+from torch import Tensor
 from torchvision.models._api import Weights, WeightsEnum
+
+import torchgeo.transforms.transforms as T
 
 # Specified in https://github.com/fieldsoftheworld/ftw-baselines
 # First 4 S2 bands are for image t1 and last 4 bands are for image t2
-_ftw_sentinel2_bands = ['B4', 'B3', 'B2', 'B8A', 'B4', 'B3', 'B2', 'B8A']
+_ftw_sentinel2_bands = ('B4', 'B3', 'B2', 'B8A', 'B4', 'B3', 'B2', 'B8A')
 
 # https://github.com/fieldsoftheworld/ftw-baselines/blob/main/src/ftw/datamodules.py
 # Normalization by 3k (for S2 uint16 input)
 _ftw_transforms = K.AugmentationSequential(
     K.Normalize(mean=torch.tensor(0.0), std=torch.tensor(3000.0)), data_keys=None
+)
+
+# Specified in https://github.com/fieldsoftheworld/ftw-baselines
+# First 4 S2 bands are for image t1 and last 4 bands are for image t2
+_ai4g_flood_sentinel1_bands = ('VV', 'VH', 'VV', 'VH')
+
+
+# https://github.com/microsoft/ai4g-flood/blob/main/src/run_flood_detection_downloaded_images.py#L54
+class Sentinel1ChangeMap(torch.nn.Module):
+    """Extracts change map from Sentinel-1 pre and post imagery.
+
+    .. versionadded:: 0.8
+    """
+
+    def __init__(
+        self,
+        vv_threshold: int = 100,
+        vh_threshold: int = 90,
+        vv_min_threshold: int = 75,
+        vh_min_threshold: int = 70,
+        delta_amplitude: float = 10,
+    ) -> None:
+        """Initializes the Sentinel1ChangeMap transform.
+
+        Args:
+            vv_threshold: Threshold for VV band to detect change.
+            vh_threshold: Threshold for VH band to detect change.
+            vv_min_threshold: Minimum threshold for VV band to consider valid data.
+            vh_min_threshold: Minimum threshold for VH band to consider valid data.
+            delta_amplitude: Minimum change in amplitude to consider as a change.
+        """
+        super().__init__()
+        self.vv_threshold = vv_threshold
+        self.vh_threshold = vh_threshold
+        self.vv_min_threshold = vv_min_threshold
+        self.vh_min_threshold = vh_min_threshold
+        self.delta_amplitude = delta_amplitude
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Extracts change map from Sentinel-1 pre and post imagery.
+
+        Args:
+            x: Input tensor of shape (N, 4, H, W) where N is the batch size,
+                4 is the number of channels (VV pre, VH pre, VV post, VH post),
+                and H, W are the height and width of the images.
+
+        Returns:
+            A tensor of shape (N, 2, H, W) containing the change maps for VV and VH bands.
+            The values are 1 for change detected and 0 for no change.
+        """
+        vv_pre, vh_pre, vv_post, vh_post = x[:, 0], x[:, 1], x[:, 2], x[:, 3]
+        vv_change = (
+            (vv_post < self.vv_threshold)
+            & (vv_pre > self.vv_threshold)
+            & ((vv_pre - vv_post) > self.delta_amplitude)
+        ).to(torch.int)
+        vh_change = (
+            (vh_post < self.vh_threshold)
+            & (vh_pre > self.vh_threshold)
+            & ((vh_pre - vh_post) > self.delta_amplitude)
+        ).to(torch.int)
+
+        zero_idx = (
+            (vv_post < self.vv_min_threshold)
+            | (vv_pre < self.vv_min_threshold)
+            | (vh_post < self.vh_min_threshold)
+            | (vh_pre < self.vh_min_threshold)
+        )
+        vv_change[zero_idx] = 0
+        vh_change[zero_idx] = 0
+        return torch.stack([vv_change, vh_change], dim=1).to(torch.float)
+
+
+_ai4g_flood_transforms = K.AugmentationSequential(
+    # Convert to decibel scale and shift to [0, 255] range
+    K.ImageSequential(Lambda(lambda x: 2 * 10 * torch.log10(x) + 135)),
+    T._Clamp(p=1, min=0, max=255),
+    # Extract change map from pre and post images
+    K.ImageSequential(
+        Sentinel1ChangeMap(
+            vv_threshold=100,
+            vh_threshold=90,
+            vv_min_threshold=75,
+            vh_min_threshold=70,
+            delta_amplitude=10,
+        )
+    ),
+    K.Resize(size=(128, 128), resample=Resample.NEAREST),
+    data_keys=None,
 )
 
 # https://github.com/pytorch/vision/pull/6883
@@ -94,6 +188,21 @@ class Unet_Weights(WeightsEnum):  # type: ignore[misc]
             'repo': 'https://github.com/fieldsoftheworld/ftw-baselines',
             'bands': _ftw_sentinel2_bands,
             'license': 'non-commercial',
+        },
+    )
+    SENTINEL1_AI4G_FLOOD = Weights(
+        url='https://huggingface.co/torchgeo/ai4g_flood/resolve/672bfb53b61a91114941ac9e4338ebc96dff6ec7/unet_mobilenetv2_sentinel1_ai4g_flood-d95df7aa.pth',
+        transforms=_ai4g_flood_transforms,
+        meta={
+            'dataset': 'AI4G Global Flood Extent',
+            'in_chans': 4,
+            'num_classes': 2,
+            'model': 'U-Net',
+            'encoder': 'mobilenet_v2',
+            'publication': 'https://arxiv.org/abs/2411.01411',
+            'repo': 'https://github.com/microsoft/ai4g-flood',
+            'bands': _ai4g_flood_sentinel1_bands,
+            'license': 'MIT',
         },
     )
 
