b6f89f6f88928629583c51391510a942e8ad5494
Adam J. Stewart <ajstewart426@gmail.com>
2025-06-10 11:20:06 +0200

    EnviroAtlas: make sliceable

--- tests/datasets/test_enviroatlas.py
+++ tests/datasets/test_enviroatlas.py
@@ -15,7 +15,6 @@ from pyproj import CRS
 from pytest import MonkeyPatch
 
 from torchgeo.datasets import (
-    BoundingBox,
     DatasetNotFoundError,
     EnviroAtlas,
     IntersectionDataset,
@@ -91,11 +90,10 @@ class TestEnviroAtlas:
             EnviroAtlas(tmp_path, checksum=True)
 
     def test_out_of_bounds_query(self, dataset: EnviroAtlas) -> None:
-        query = BoundingBox(0, 0, 0, 0, pd.Timestamp.min, pd.Timestamp.min)
         with pytest.raises(
             IndexError, match='query: .* not found in index with bounds:'
         ):
-            dataset[query]
+            dataset[0:0, 0:0, pd.Timestamp.min : pd.Timestamp.min]
 
     def test_multiple_hits_query(self, dataset: EnviroAtlas) -> None:
         ds = EnviroAtlas(
