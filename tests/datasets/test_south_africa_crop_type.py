d44ef17282965e5cec86da8fa462048a32e15f5e
Adam J. Stewart <ajstewart426@gmail.com>
2025-06-14 10:49:09 +0200

    South Africa Crop Type: make sliceable

--- tests/datasets/test_south_africa_crop_type.py
+++ tests/datasets/test_south_africa_crop_type.py
@@ -14,7 +14,6 @@ from pyproj import CRS
 from pytest import MonkeyPatch
 
 from torchgeo.datasets import (
-    BoundingBox,
     DatasetNotFoundError,
     IntersectionDataset,
     RGBBandsMissingError,
@@ -82,11 +81,10 @@ class TestSouthAfricaCropType:
         plt.close()
 
     def test_invalid_query(self, dataset: SouthAfricaCropType) -> None:
-        query = BoundingBox(0, 0, 0, 0, pd.Timestamp.min, pd.Timestamp.min)
         with pytest.raises(
             IndexError, match='query: .* not found in index with bounds:'
         ):
-            dataset[query]
+            dataset[0:0, 0:0, pd.Timestamp.min : pd.Timestamp.min]
 
     def test_rgb_bands_absent_plot(self) -> None:
         path = os.path.join('tests', 'data', 'south_africa_crop_type')
