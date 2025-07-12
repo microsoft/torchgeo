f35c0a2a9222094973a7fc8767202d8870e9203a
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2025-07-12 20:47:38 +0000

    add pretrained core-dino

--- torchgeo/models/yolo.py
+++ torchgeo/models/yolo.py
@@ -29,7 +29,7 @@ class YOLO_Weights(WeightsEnum):  # type: ignore[misc]
     """
 
     DELINEATE_ANYTHING = Weights(
-        url='https://huggingface.co/torchgeo/delineate-anything-s/resolve/60bea7b2f81568d16d5c75e4b5b06289e1d7efaf/delineate_anything_rgb_yolo11x-88ede029.pt',
+        url='https://hf.co/torchgeo/delineate-anything-s/resolve/60bea7b2f81568d16d5c75e4b5b06289e1d7efaf/delineate_anything_rgb_yolo11x-88ede029.pt',
         transforms=_delineate_anything_transforms,
         meta={
             'dataset': 'FBIS-22M',
@@ -48,7 +48,7 @@ class YOLO_Weights(WeightsEnum):  # type: ignore[misc]
         },
     )
     DELINEATE_ANYTHING_SMALL = Weights(
-        url='https://huggingface.co/torchgeo/delineate-anything-s/resolve/69cd440b0c5bd450ced145e68294aa9393ddae05/delineate_anything_s_rgb_yolo11n-b879d643.pt',
+        url='https://hf.co/torchgeo/delineate-anything-s/resolve/69cd440b0c5bd450ced145e68294aa9393ddae05/delineate_anything_s_rgb_yolo11n-b879d643.pt',
         transforms=_delineate_anything_transforms,
         meta={
             'dataset': 'FBIS-22M',
@@ -64,6 +64,25 @@ class YOLO_Weights(WeightsEnum):  # type: ignore[misc]
             'repo': 'https://github.com/Lavreniuk/Delineate-Anything',
             'resolution': None,
             'license': 'AGPL-3.0',
+        },
+    )
+    CORE_DINO = Weights(
+        url='https://hf.co/torchgeo/core-dino/resolve/59427e13d114cbbf02f4745e1bea7570be3e2057/core_dino_rgb_yolo11x-80ca836f.pt',
+        transforms=nn.Identity(),  # transform is handled within ultralytics.YOLO model
+        meta={
+            'dataset': 'core-five',
+            'in_chans': 3,
+            'num_classes': None,
+            'classes': None,
+            'model': 'yolo11x',
+            'task': 'bbox',
+            'encoder': None,
+            'input_shape': (3, -1, -1),  # trained for dynamic input shape
+            'bands': ['R', 'G', 'B'],
+            'publication': None,
+            'repo': 'https://huggingface.co/gajeshladhar/core-dino',
+            'resolution': None,
+            'license': 'CC-BY-NC-3.0',
         },
     )
 
@@ -88,7 +107,9 @@ def yolo(weights: YOLO_Weights | None = None, *args: Any, **kwargs: Any) -> nn.M
 
     if weights:
         kwargs['model'] = weights.url
-        kwargs['task'] = weights.meta['task']
+
+        if 'task' not in kwargs:
+            kwargs['task'] = weights.meta['task']
 
     model = ultralytics.YOLO(*args, **kwargs)
     return cast(nn.Module, model)
