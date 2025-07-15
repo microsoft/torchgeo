fd170aefd4e9929a55cacbb167dee58a4e37627a
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2025-07-15 07:48:39 -0500

    YOLO Marine Vessel Detection (#2884)

--- torchgeo/models/yolo.py
+++ torchgeo/models/yolo.py
@@ -6,6 +6,7 @@
 from typing import Any, cast
 
 import kornia.augmentation as K
+import torch
 import torch.nn as nn
 from torchvision.models._api import Weights, WeightsEnum
 
@@ -16,6 +17,15 @@ from ..datasets.utils import lazy_import
 # normalization method for each source.
 _delineate_anything_transforms = K.AugmentationSequential(
     K.Resize(size=(512, 512)), data_keys=None
+)
+
+# Model is trained on 320x320 Sentinel-2 L1C TCI uint8 patches
+# then resized to 640x640
+# https://hf.co/mayrajeo/marine-vessel-yolo#direct-use
+_marine_vessel_detection_transforms = K.AugmentationSequential(
+    K.Resize(size=(640, 640)),
+    K.Normalize(mean=torch.tensor(0), std=torch.tensor(255)),
+    data_keys=None,
 )
 
 
@@ -29,7 +39,7 @@ class YOLO_Weights(WeightsEnum):  # type: ignore[misc]
     """
 
     DELINEATE_ANYTHING = Weights(
-        url='https://hf.co/torchgeo/delineate-anything-s/resolve/60bea7b2f81568d16d5c75e4b5b06289e1d7efaf/delineate_anything_rgb_yolo11x-88ede029.pt',
+        url='https://hf.co/torchgeo/delineate-anything/resolve/60bea7b2f81568d16d5c75e4b5b06289e1d7efaf/delineate_anything_rgb_yolo11x-88ede029.pt',
         transforms=_delineate_anything_transforms,
         meta={
             'dataset': 'FBIS-22M',
@@ -83,6 +93,25 @@ class YOLO_Weights(WeightsEnum):  # type: ignore[misc]
             'repo': 'https://huggingface.co/gajeshladhar/core-dino',
             'resolution': None,
             'license': 'CC-BY-NC-3.0',
+        },
+    )
+    SENTINEL2_RGB_MARINE_VESSEL_DETECTION = Weights(
+        url='https://hf.co/torchgeo/yolo11s_marine_vessel_detection/resolve/f57c8537eef80e8fb4b1ad85e02db1d6de3f3e40/yolo11s_sentinel2_rgb_marine_vessel_detection-952cb83c.pt',
+        transforms=_marine_vessel_detection_transforms,
+        meta={
+            'dataset': 'Finnish Coast Sentinel-2 Marine Vessel Detection',
+            'in_chans': 3,
+            'num_classes': 1,
+            'classes': ('boat',),
+            'model': 'yolo11s',
+            'task': 'detect',
+            'encoder': None,
+            'input_shape': (3, 320, 320),
+            'bands': ['R', 'G', 'B'],
+            'publication': 'https://doi.org/10.1016/j.rse.2025.114791',
+            'repo': 'https://hf.co/mayrajeo/marine-vessel-yolo',
+            'resolution': 10,
+            'license': 'AGPL-3.0',
         },
     )
 
