162d0352bcbbb9a3d6518b036d39a91a5c2e5fb4
Giacomo Caria <giacomo@chloris.earth>
2025-07-18 11:54:10 -0400

    change year copyright

--- docs/conf.py
+++ docs/conf.py
@@ -17,16 +17,16 @@ import pytorch_sphinx_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 import torchgeo
 
 # -- Project information -----------------------------------------------------
 
-project = 'torchgeo'
-copyright = '2021, Microsoft Corporation'
+project = "torchgeo"
+copyright = "2025, Microsoft Corporation"
 author = torchgeo.__author__
-version = '.'.join(torchgeo.__version__.split('.')[:2])
+version = ".".join(torchgeo.__version__.split(".")[:2])
 release = torchgeo.__version__
 
 
@@ -36,40 +36,40 @@ release = torchgeo.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    'nbsphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
 ]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build']
+exclude_patterns = ["_build"]
 
 # Sphinx 4.0+ required for autodoc_typehints_description_traget
-needs_sphinx = '4.0'
+needs_sphinx = "4.0"
 
 nitpicky = True
 nitpick_ignore = [
     # Undocumented classes
-    ('py:class', 'fiona.model.Feature'),
-    ('py:class', 'kornia.augmentation._2d.intensity.base.IntensityAugmentationBase2D'),
-    ('py:class', 'kornia.augmentation._3d.geometric.base.GeometricAugmentationBase3D'),
-    ('py:class', 'kornia.augmentation.base._AugmentationBase'),
-    ('py:class', 'lightning.pytorch.utilities.types.LRSchedulerConfig'),
-    ('py:class', 'lightning.pytorch.utilities.types.OptimizerConfig'),
-    ('py:class', 'lightning.pytorch.utilities.types.OptimizerLRSchedulerConfig'),
-    ('py:class', 'segmentation_models_pytorch.base.model.SegmentationModel'),
-    ('py:class', 'timm.models.resnet.ResNet'),
-    ('py:class', 'timm.models.vision_transformer.VisionTransformer'),
-    ('py:class', 'torch.optim.lr_scheduler.LRScheduler'),
-    ('py:class', 'torchvision.models._api.WeightsEnum'),
-    ('py:class', 'torchvision.models.resnet.ResNet'),
-    ('py:class', 'torchvision.models.swin_transformer.SwinTransformer'),
+    ("py:class", "fiona.model.Feature"),
+    ("py:class", "kornia.augmentation._2d.intensity.base.IntensityAugmentationBase2D"),
+    ("py:class", "kornia.augmentation._3d.geometric.base.GeometricAugmentationBase3D"),
+    ("py:class", "kornia.augmentation.base._AugmentationBase"),
+    ("py:class", "lightning.pytorch.utilities.types.LRSchedulerConfig"),
+    ("py:class", "lightning.pytorch.utilities.types.OptimizerConfig"),
+    ("py:class", "lightning.pytorch.utilities.types.OptimizerLRSchedulerConfig"),
+    ("py:class", "segmentation_models_pytorch.base.model.SegmentationModel"),
+    ("py:class", "timm.models.resnet.ResNet"),
+    ("py:class", "timm.models.vision_transformer.VisionTransformer"),
+    ("py:class", "torch.optim.lr_scheduler.LRScheduler"),
+    ("py:class", "torchvision.models._api.WeightsEnum"),
+    ("py:class", "torchvision.models.resnet.ResNet"),
+    ("py:class", "torchvision.models.swin_transformer.SwinTransformer"),
 ]
 
 
@@ -77,65 +77,65 @@ nitpick_ignore = [
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'pytorch_sphinx_theme'
+html_theme = "pytorch_sphinx_theme"
 html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'collapse_navigation': False,
-    'display_version': True,
-    'logo_only': True,
-    'pytorch_project': 'docs',
-    'navigation_with_keys': True,
-    'analytics_id': 'UA-209075005-1',
+    "collapse_navigation": False,
+    "display_version": True,
+    "logo_only": True,
+    "pytorch_project": "docs",
+    "navigation_with_keys": True,
+    "analytics_id": "UA-209075005-1",
 }
 
-html_favicon = os.path.join('..', 'logo', 'favicon.ico')
+html_favicon = os.path.join("..", "logo", "favicon.ico")
 
-html_static_path = ['_static']
-html_css_files = ['badge-height.css', 'notebook-prompt.css', 'table-scroll.css']
+html_static_path = ["_static"]
+html_css_files = ["badge-height.css", "notebook-prompt.css", "table-scroll.css"]
 
 # -- Extension configuration -------------------------------------------------
 
 # sphinx.ext.autodoc
 autodoc_default_options = {
-    'members': True,
-    'special-members': True,
-    'show-inheritance': True,
+    "members": True,
+    "special-members": True,
+    "show-inheritance": True,
 }
-autodoc_member_order = 'bysource'
-autodoc_typehints = 'description'
-autodoc_typehints_description_target = 'documented'
+autodoc_member_order = "bysource"
+autodoc_typehints = "description"
+autodoc_typehints_description_target = "documented"
 
 # sphinx.ext.intersphinx
 intersphinx_mapping = {
-    'einops': ('https://einops.rocks/', None),
-    'kornia': ('https://kornia.readthedocs.io/en/stable/', None),
-    'lightning': ('https://lightning.ai/docs/pytorch/stable/', None),
-    'matplotlib': ('https://matplotlib.org/stable/', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'pandas': ('https://pandas.pydata.org/docs/', None),
-    'pyproj': ('https://pyproj4.github.io/pyproj/stable/', None),
-    'python': ('https://docs.python.org/3', None),
-    'rasterio': ('https://rasterio.readthedocs.io/en/stable/', None),
-    'segmentation_models_pytorch': ('https://smp.readthedocs.io/en/stable/', None),
-    'shapely': ('https://shapely.readthedocs.io/en/stable/', None),
-    'sklearn': ('https://scikit-learn.org/stable/', None),
-    'timm': ('https://huggingface.co/docs/timm/main/en/', None),
-    'torch': ('https://docs.pytorch.org/docs/stable/', None),
-    'torchmetrics': ('https://lightning.ai/docs/torchmetrics/stable/', None),
-    'torchvision': ('https://docs.pytorch.org/vision/stable/', None),
-    'ultralytics': ('https://docs.ultralytics.com/', None),
+    "einops": ("https://einops.rocks/", None),
+    "kornia": ("https://kornia.readthedocs.io/en/stable/", None),
+    "lightning": ("https://lightning.ai/docs/pytorch/stable/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
+    "pyproj": ("https://pyproj4.github.io/pyproj/stable/", None),
+    "python": ("https://docs.python.org/3", None),
+    "rasterio": ("https://rasterio.readthedocs.io/en/stable/", None),
+    "segmentation_models_pytorch": ("https://smp.readthedocs.io/en/stable/", None),
+    "shapely": ("https://shapely.readthedocs.io/en/stable/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
+    "timm": ("https://huggingface.co/docs/timm/main/en/", None),
+    "torch": ("https://docs.pytorch.org/docs/stable/", None),
+    "torchmetrics": ("https://lightning.ai/docs/torchmetrics/stable/", None),
+    "torchvision": ("https://docs.pytorch.org/vision/stable/", None),
+    "ultralytics": ("https://docs.ultralytics.com/", None),
 }
 
 # nbsphinx
-nbsphinx_execute = 'never'
-with open(os.path.join('tutorials', 'prolog.rst.jinja')) as f:
+nbsphinx_execute = "never"
+with open(os.path.join("tutorials", "prolog.rst.jinja")) as f:
     nbsphinx_prolog = f.read()
 
 # Disables requirejs in nbsphinx to enable compatibility with the pytorch_sphinx_theme
 # See more information here https://github.com/spatialaudio/nbsphinx/issues/599
 # NOTE: This will likely break nbsphinx widgets
-nbsphinx_requirejs_path = ''
+nbsphinx_requirejs_path = ""
