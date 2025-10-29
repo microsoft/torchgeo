45866422d478e37217ba337b43c097658a4dbdce
Adam J. Stewart <ajstewart426@gmail.com>
2025-10-29 19:22:47 +0100

    Ruff

--- torchgeo/datamodules/geo.py
+++ torchgeo/datamodules/geo.py
@@ -287,7 +287,7 @@ class GeoDataModule(BaseDataModule):
             num_workers=self.num_workers,
             collate_fn=self.collate_fn,
             # drop_last is incompatible with batch sampler
-            drop_last=split == 'train' and batch_sampler is None,  
+            drop_last=split == 'train' and batch_sampler is None,
             persistent_workers=self.num_workers > 0,
         )
 
