70967d6cbb28e0f6e9f8acb8559a5ed3edd93825
Robin Cole <robmarkcole@gmail.com>
2025-11-11 12:12:54 +0000

    isort

--- torchgeo/datasets/copernicus/biomass_s3.py
+++ torchgeo/datasets/copernicus/biomass_s3.py
@@ -11,6 +11,7 @@ from typing import Literal
 import pandas as pd
 import torch
 import torch.nn.functional as F
+from torch import Tensor
 
 from ..utils import Path, Sample, stack_samples
 from .base import CopernicusBenchBase
