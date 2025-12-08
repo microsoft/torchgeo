a3c9078c6293769e23d879bc97868ee235824796
Robin Cole <robmarkcole@gmail.com>
2025-12-08 09:26:44 +0000

    put back data.py

---
+++ tests/data/ref_cloud_cover_detection_challenge_v1/data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Copyright (c) TorchGeo Contributors. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+import numpy as np
+import rasterio
+from rasterio import Affine
+from rasterio.crs import CRS
+
+SIZE = 2
+DTYPE = np.uint16
+
+np.random.seed(0)
+
+splits = {'train': 'public', 'test': 'private'}
+chip_ids = ['aaaa']
+all_bands = ['B02', 'B03', 'B04', 'B08']
+profile = {
+    'driver': 'GTiff',
+    'dtype': DTYPE,
+    'width': SIZE,
+    'height': SIZE,
+    'count': 1,
+    'crs': CRS.from_epsg(32753),
+    'transform': Affine(10.0, 0.0, 777760.0, 0.0, -10.0, 6735270.0),
+}
+Z = np.random.randint(np.iinfo(DTYPE).max, size=(SIZE, SIZE), dtype=DTYPE)
+
+for split, directory in splits.items():
+    for chip_id in chip_ids:
+        path = os.path.join(directory, f'{split}_features', chip_id)
+        os.makedirs(path, exist_ok=True)
+        for band in all_bands:
+            with rasterio.open(os.path.join(path, f'{band}.tif'), 'w', **profile) as f:
+                f.write(Z, 1)
+        path = os.path.join(directory, f'{split}_labels')
+        os.makedirs(path, exist_ok=True)
+        with rasterio.open(os.path.join(path, f'{chip_id}.tif'), 'w', **profile) as f:
+            f.write(Z, 1)
