99babc63cf1a7695238be43081e8cfc86e644587
Ashwin Nair <ash1995@gmail.com>
2025-12-16 00:59:51 +0400

    Update

--- docs/conf.py
+++ docs/conf.py
@@ -79,6 +79,8 @@ html_theme = 'pydata_sphinx_theme'
 
 # Define the version we use for matching in the version switcher.
 version_match = os.environ.get('READTHEDOCS_VERSION')
+json_url = 'https://torchgeo.readthedocs.io/en/latest/_static/switcher.json'
+
 # If READTHEDOCS_VERSION doesn't exist, we're not on RTD
 # If it is an integer, we're in a PR build and the version isn't correct.
 # If it's "latest" → change to "dev" (that's what we want the switcher to call it)
@@ -89,6 +91,8 @@ if not version_match or version_match.isdigit() or version_match == 'latest':
         # We want to keep the relative reference if we are in dev mode
         # but we want the whole url if we are effectively in a released version
         json_url = '_static/switcher.json'
+    else:
+        version_match = f'v{release}'
 elif version_match == 'stable':
     version_match = f'v{release}'
 
@@ -119,10 +123,7 @@ html_theme_options = {
         'image_light': os.path.join('..', 'logo', 'logo-color.svg'),
         'image_dark': os.path.join('..', 'logo', 'logo-color.svg'),
     },
-    'switcher': {
-        'json_url': 'https://torchgeo.readthedocs.io/en/latest/_static/switcher.json',
-        'version_match': version_match,
-    },
+    'switcher': {'json_url': json_url, 'version_match': version_match},
     'navbar_start': ['navbar-logo', 'version-switcher'],
     'navbar_center': ['navbar-nav'],
     'navbar_end': ['theme-switcher', 'navbar-icon-links'],
