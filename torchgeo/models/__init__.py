8a31c4331a6dcab5a144265e0286e9a3bd9061a2
Pratik0405 <pgangapurwala@gmail.com>
2026-01-31 12:58:00 +0100

    cleaned Ruff in _init_.py

--- torchgeo/models/__init__.py
+++ torchgeo/models/__init__.py
@@ -24,7 +24,7 @@ from .earthloc import EarthLoc, EarthLoc_Weights, earthloc
 from .farseg import FarSeg
 from .fcn import FCN
 from .fcsiam import FCSiamConc, FCSiamDiff
-from .galileo import GalileoWeights, galileo, GalileoEncoder
+from .galileo import GalileoEncoder, GalileoWeights, galileo
 from .ltae import LTAE
 from .panopticon import Panopticon, Panopticon_Weights, panopticon_vitb14
 from .rcf import MOSAIKS, RCF
