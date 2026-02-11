1a4e2f83767dc3880b90345274cc8d6b1dc89db2
Kshitij <36752999+kshitijrajsharma@users.noreply.github.com>
2026-02-11 18:18:34 +0100

    Update torchgeo/datasets/openaerialmap.py
    Co-authored-by: Ashwin Nair <ash1995@gmail.com>

--- torchgeo/datasets/openaerialmap.py
+++ torchgeo/datasets/openaerialmap.py
@@ -183,6 +183,7 @@ class OpenAerialMap(RasterDataset):
     If you use this dataset in your research, please cite OpenAerialMap:
 
     * https://openaerialmap.org/
+    .. versionadded:: 0.9
     """
 
     _stac_url: ClassVar[str] = 'https://api.imagery.hotosm.org/stac'
