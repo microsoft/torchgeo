f292fdc0c61a18e4fe29d1229d26b2425be2b809
JamesBrockUoB <james.brock@bristol.ac.uk>
2026-05-04 20:40:05 +0100

    ty check fix for testing invalid split

--- tests/datasets/test_forestchange.py
+++ tests/datasets/test_forestchange.py
@@ -225,9 +225,8 @@ class TestForestChange:
             ForestChange(root=tmp_path)
 
     def test_invalid_split(self, tmp_path: Path) -> None:
-        split: str = 'invalid'
         with pytest.raises(AssertionError):
-            ForestChange(root=tmp_path, split=split)
+            ForestChange(root=tmp_path, split=str('invalid'))
 
     def test_classes(self) -> None:
         assert ForestChange.classes == ('no_change', 'deforestation')
