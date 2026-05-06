4174d7dc9132cc2be98ea30438f9959e96664a80
hkristen <haraldkristen@posteo.at>
2026-05-06 11:13:32 +0200

    Add weighted blending utilities for tiled inference stitching

--- torchgeo/callbacks/__init__.py
+++ torchgeo/callbacks/__init__.py
@@ -3,6 +3,7 @@
 
 """TorchGeo callbacks."""
 
+from .blending import PatchMetadata, weighted_merge
 from .writer import GeoTIFFWriter
 
-__all__ = ['GeoTIFFWriter']
+__all__ = ['GeoTIFFWriter', 'PatchMetadata', 'weighted_merge']
