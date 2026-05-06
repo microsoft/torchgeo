aa8990576a43becc652aaba18d1ff66b4215d76e
hkristen <haraldkristen@posteo.at>
2026-05-06 10:51:23 +0200

    Skip case study notebooks from pytest collection

---
+++ docs/tutorials/conftest.py
@@ -0,0 +1,8 @@
+# Copyright (c) TorchGeo Contributors. All rights reserved.
+# Licensed under the MIT License.
+
+collect_ignore = [
+    'earth_surface_water.ipynb',
+    'earthquake_detection.ipynb',
+    'naip_road_segmentation.ipynb',
+]
