eb5b9b598c700311086899aa6d0d42df3e738b14
Adam J. Stewart <ajstewart426@gmail.com>
2026-05-07 17:54:36 +0200

    Add change_detection tutorial

--- docs/tutorials/conftest.py
+++ docs/tutorials/conftest.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 
 collect_ignore = [
+    'change_detection.ipynb',
     'earth_surface_water.ipynb',
     'earthquake_detection.ipynb',
     'naip_road_segmentation.ipynb',
