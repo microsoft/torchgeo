052fd88f211c057373330d24c471802b47fcf152
robin.cole@earthdaily.com <robmarkcole@gmail.com>
2026-05-18 15:21:06 +0100

    Update class path in pastis.yaml for spatiotemporal segmentation task and remove pastis from test cases

--- tests/trainers/test_segmentation.py
+++ tests/trainers/test_segmentation.py
@@ -67,7 +67,6 @@ class TestSemanticSegmentationTask:
             'loveda',
             'mmflood',
             'naipchesapeake',
-            'pastis',
             'pastis100',
             'potsdam2d',
             'sen12ms_all',
