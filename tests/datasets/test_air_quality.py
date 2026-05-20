889e6e6a5c41689a805c3649392b61893244525a
fschi <fschi@fschibook.localdomain>
2026-05-20 22:13:48 +0200

    update test params

--- tests/datasets/test_air_quality.py
+++ tests/datasets/test_air_quality.py
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) TorchGeo Contributors. All rights reserved.
 # Licensed under the MIT License.
 
 import os
@@ -23,14 +23,14 @@ class TestAirQuality:
 
     def test_getitem(self, dataset: AirQuality) -> None:
         item = dataset[0]
-        x = item['past_targets']
-        y = item['future_targets']
+        x = item['x_input']
+        y = item['y_target']
         assert isinstance(x, Tensor)
-        assert x.shape[1] == 15
-        assert x.shape[0] == dataset.num_past_steps
+        assert x.shape[1] == 12
+        assert x.shape[0] == dataset.num_input_steps
         assert isinstance(y, Tensor)
-        assert y.shape[1] == 15
-        assert y.shape[0] == dataset.num_future_steps
+        assert y.shape[1] == 12
+        assert y.shape[0] == dataset.num_target_steps
 
     def test_len(self, dataset: AirQuality) -> None:
         assert len(dataset) == 46
