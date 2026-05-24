d4e5018d9f1cc830fdd1e493c4b7d6e445a22854
Adam J. Stewart <ajstewart426@gmail.com>
2026-05-24 17:39:03 +0200

    Stress test all feature subset posibilities
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- tests/datasets/test_air_quality.py
+++ tests/datasets/test_air_quality.py
@@ -12,25 +12,33 @@ from torch import Tensor
 
 from torchgeo.datasets import AirQuality, DatasetNotFoundError
 
+GT = ['CO(GT)', 'NMHC(GT)', 'C6H6(GT)', 'NOx(GT)', 'NO2(GT)']
+
 
 class TestAirQuality:
-    @pytest.fixture
+    @pytest.fixture(params=[(None, None), (GT, None), (None, GT), (GT, GT)])
     def dataset(
         self, monkeypatch: MonkeyPatch, tmp_path: Path, request: SubRequest
     ) -> AirQuality:
         url = os.path.join('tests', 'data', 'air_quality', 'data.csv')
         monkeypatch.setattr(AirQuality, 'url', url)
-        return AirQuality(tmp_path, download=True)
+        input_features, target_features = request.param
+        return AirQuality(
+            tmp_path,
+            input_features=input_features,
+            target_features=target_features,
+            download=True,
+        )
 
     def test_getitem(self, dataset: AirQuality) -> None:
         item = dataset[0]
         x = item['input']
         y = item['target']
         assert isinstance(x, Tensor)
-        assert x.shape[1] == 17
+        assert x.shape[1] in {5, 17}
         assert x.shape[0] == dataset.input_steps
         assert isinstance(y, Tensor)
-        assert y.shape[1] == 17
+        assert y.shape[1] in {5, 17}
         assert y.shape[0] == dataset.target_steps
 
     def test_len(self, dataset: AirQuality) -> None:
@@ -44,8 +52,9 @@ class TestAirQuality:
         root = os.path.join('tests', 'data', 'air_quality')
         AirQuality(root)
 
-    def test_plot(self, dataset: AirQuality) -> None:
+    @pytest.mark.parametrize('features', [None, GT])
+    def test_plot(self, dataset: AirQuality, features: list[str] | None) -> None:
         sample = dataset[0]
         sample['prediction'] = sample['target']
-        dataset.plot(sample)
+        dataset.plot(sample, features=features)
         plt.close()
