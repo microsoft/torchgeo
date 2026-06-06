35a5a610b7acc87f3e890ff1eefc4faba50aa405
robin.cole@earthdaily.com <robmarkcole@gmail.com>
2026-06-06 14:19:12 +0100

    Add test cases for SpatioTemporalPixelwiseRegressionTask and configuration file

--- tests/tasks/test_spatiotemporal_regression.py
+++ tests/tasks/test_spatiotemporal_regression.py
@@ -1,16 +1,45 @@
 # Copyright (c) TorchGeo Contributors. All rights reserved.
 # Licensed under the MIT License.
 
+import os
 from typing import Literal
 
 import pytest
 import torch
 
 from torchgeo.datamodules import MisconfigurationException
+from torchgeo.main import main
 from torchgeo.tasks import SpatioTemporalRegression
 
 
 class TestSpatioTemporalRegression:
+    @pytest.mark.parametrize('name', ['copernicus_biomass_s3_ts'])
+    def test_trainer(self, name: str, fast_dev_run: bool) -> None:
+        config = os.path.join('tests', 'conf', name + '.yaml')
+
+        args = [
+            '--config',
+            config,
+            '--trainer.accelerator',
+            'cpu',
+            '--trainer.fast_dev_run',
+            str(fast_dev_run),
+            '--trainer.max_epochs',
+            '1',
+            '--trainer.log_every_n_steps',
+            '1',
+        ]
+
+        main(['fit', *args])
+        try:
+            main(['test', *args])
+        except MisconfigurationException:
+            pass
+        try:
+            main(['predict', *args])
+        except MisconfigurationException:
+            pass
+
     @pytest.mark.parametrize('loss', ['mse', 'mae'])
     def test_task(self, loss: Literal['mse', 'mae']) -> None:
         model = SpatioTemporalRegression(
