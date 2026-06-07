bd4b9006b8e0f465938e4eac151539c682029e1c
DarthRecaPortable <darthreca@gmail.com>
2026-06-07 09:53:40 +0000

    fix: classification test corrected

--- tests/tasks/test_classification.py
+++ tests/tasks/test_classification.py
@@ -18,9 +18,8 @@ from torchgeo.datamodules import (
     BigEarthNetDataModule,
     EuroSATDataModule,
     MisconfigurationException,
-    QuakeSetDataModule,
 )
-from torchgeo.datasets import BigEarthNet, EuroSAT, QuakeSet, RGBBandsMissingError
+from torchgeo.datasets import BigEarthNet, EuroSAT, RGBBandsMissingError
 from torchgeo.main import main
 from torchgeo.models import ResNet18_Weights
 from torchgeo.tasks import Classification
@@ -41,11 +40,6 @@ class ClassificationTestModel(Module):
         x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
-
-
-class PredictBinaryDataModule(QuakeSetDataModule):
-    def setup(self, stage: str) -> None:
-        self.predict_dataset = QuakeSet(split='test', **self.kwargs)
 
 
 class PredictMulticlassDataModule(EuroSATDataModule):
@@ -83,7 +77,6 @@ class TestClassification:
             'eurosat100',
             'eurosatspatial',
             'fire_risk',
-            'quakeset',
             'patternnet',
             'resisc45',
             'so2sat_all',
@@ -97,7 +90,7 @@ class TestClassification:
     def test_trainer(
         self, monkeypatch: MonkeyPatch, name: str, fast_dev_run: bool
     ) -> None:
-        if name.startswith('so2sat') or name == 'quakeset':
+        if name.startswith('so2sat'):
             pytest.importorskip('h5py', minversion='3.10')
 
         config = os.path.join('tests', 'conf', name + '.yaml')
