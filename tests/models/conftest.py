f290a491e0031cd9608500ae12b66a9eadac50e6
Isaac Corley <isaac.a.corley@gmail.com>
2026-08-11 16:06:47 -0500

    chore: narrow RCF dtype change

--- tests/models/conftest.py
+++ tests/models/conftest.py
@@ -1,23 +1,10 @@
 # Copyright (c) TorchGeo Contributors. All rights reserved.
 # Licensed under the MIT License.
 
-from collections.abc import Iterator
-
 import pytest
-import torch
 from _pytest.fixtures import SubRequest
 
 
 @pytest.fixture(params=[True, False])
 def features_only(request: SubRequest) -> bool:
     return bool(request.param)
-
-
-@pytest.fixture
-def use_float16() -> Iterator[None]:
-    dtype = torch.get_default_dtype()
-    torch.set_default_dtype(torch.float16)
-    try:
-        yield
-    finally:
-        torch.set_default_dtype(dtype)
