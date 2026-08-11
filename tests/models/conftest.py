2b7333befa3b9e909b439458aae68c6053168e87
Isaac Corley <isaac.a.corley@gmail.com>
2026-08-11 14:54:18 -0500

    fix: support bfloat16 in CROMA

--- tests/models/conftest.py
+++ tests/models/conftest.py
@@ -1,10 +1,23 @@
 # Copyright (c) TorchGeo Contributors. All rights reserved.
 # Licensed under the MIT License.
 
+from collections.abc import Iterator
+
 import pytest
+import torch
 from _pytest.fixtures import SubRequest
 
 
 @pytest.fixture(params=[True, False])
 def features_only(request: SubRequest) -> bool:
     return bool(request.param)
+
+
+@pytest.fixture
+def use_bfloat16() -> Iterator[None]:
+    dtype = torch.get_default_dtype()
+    torch.set_default_dtype(torch.bfloat16)
+    try:
+        yield
+    finally:
+        torch.set_default_dtype(dtype)
