1c0236200fbad12b085728c0b2f5a9b0f1ec1531
gatienc <gatien+dev@chenu.me>
2026-08-11 20:55:30 +0200

    FLAIR-HUB dataset (#3181)
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- torchgeo/datasets/__init__.py
+++ torchgeo/datasets/__init__.py
@@ -79,6 +79,7 @@ from .eurosat import EuroSAT, EuroSAT100, EuroSATSpatial
 from .everwatch import EverWatch
 from .fair1m import FAIR1M
 from .fire_risk import FireRisk
+from .flair import FLAIRHUB, FLAIRHUBBase, FLAIRHUBToy
 from .forestdamage import ForestDamage
 from .ftw import FieldsOfTheWorld
 from .gbif import GBIF
@@ -210,6 +211,7 @@ __all__ = (
     'ETCI2021',
     'EUDEM',
     'FAIR1M',
+    'FLAIRHUB',
     'GBIF',
     'GID15',
     'LEVIRCD',
@@ -298,6 +300,8 @@ __all__ = (
     'EuroSAT100',
     'EuroSATSpatial',
     'EverWatch',
+    'FLAIRHUBBase',
+    'FLAIRHUBToy',
     'FieldsOfTheWorld',
     'FireRisk',
     'ForestDamage',
