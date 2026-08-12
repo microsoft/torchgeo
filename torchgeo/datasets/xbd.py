763171ace04e84bf38868f1254717585b73828ad
Caleb Robinson <calebrob6@gmail.com>
2026-08-12 14:33:44 -0700

    docs

--- torchgeo/datasets/xbd.py
+++ torchgeo/datasets/xbd.py
@@ -430,7 +430,7 @@ class xBDDistShift(xBD):
         """Plot a sample from the dataset.
 
         Args:
-            sample: a sample returned by :meth:`__getitem__`
+            sample: a sample returned by :meth:`xBD.__getitem__`
             show_titles: flag indicating whether to show titles above each panel
             suptitle: optional string to use as a suptitle
             alpha: opacity with which to render predictions on top of the imagery
