db0d447b5255399bef190a35cdbfa5bd0753c31b
Filip Wolf <158554841+wolfilip@users.noreply.github.com>
2026-08-13 12:28:29 +0200

    add DEO model (#3629)
    * add deo model
    
    * add deo docs
    
    * add deo tests
    
    * update deo docs
    
    * update deo model and tests
    
    * add normalizations to deo model
    
    * add latest requested changes
    
    * add separate forward class for last layer features
    
    * add separate input encoders
    
    * minor fix
    
    * remove in_channels and test_deo fix
    
    * add check_hash to state dict
    
    ---------
    
    Co-authored-by: Robin Cole <robmarkcole@gmail.com>

--- torchgeo/models/api.py
+++ torchgeo/models/api.py
@@ -19,6 +19,7 @@ from torchvision.models._api import WeightsEnum
 from .aurora import Aurora_Weights, aurora_swin_unet
 from .copernicusfm import CopernicusFM_Base_Weights, copernicusfm_base
 from .croma import CROMABase_Weights, CROMALarge_Weights, croma_base, croma_large
+from .deo import DEO_Weights, deo_base
 from .dofa import (
     DOFABase16_Weights,
     DOFALarge16_Weights,
@@ -82,6 +83,7 @@ _model: dict[str, Callable[..., nn.Module]] = {
     'dofa_small_patch16_224': dofa_small_patch16_224,
     'earthloc': earthloc,
     'olmoearth_v1': olmoearth_v1,
+    'deo_base': deo_base,
     'panopticon_vitb14': panopticon_vitb14,
     'presto': presto,
     'resnet18': resnet18,
@@ -110,6 +112,7 @@ _model_weights: dict[str | Callable[..., nn.Module], type[WeightsEnum]] = {
     copernicusfm_base: CopernicusFM_Base_Weights,
     croma_base: CROMABase_Weights,
     croma_large: CROMALarge_Weights,
+    deo_base: DEO_Weights,
     dofa_base_patch16_224: DOFABase16_Weights,
     dofa_large_patch16_224: DOFALarge16_Weights,
     earthloc: EarthLoc_Weights,
@@ -139,6 +142,7 @@ _model_weights: dict[str | Callable[..., nn.Module], type[WeightsEnum]] = {
     'copernicusfm_base': CopernicusFM_Base_Weights,
     'croma_base': CROMABase_Weights,
     'croma_large': CROMALarge_Weights,
+    'deo_base': DEO_Weights,
     'dofa_base_patch16_224': DOFABase16_Weights,
     'dofa_large_patch16_224': DOFALarge16_Weights,
     'earthloc': EarthLoc_Weights,
