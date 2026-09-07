ffb7b549554cc15f303812fcd357ca2b60ef2847
yichiac <yichiachang1993@gmail.com>
2026-09-06 23:06:05 -0400

    add deprecation warning, docstring, overlap assert

--- torchgeo/datamodules/pastis.py
+++ torchgeo/datamodules/pastis.py
@@ -3,6 +3,7 @@
 
 """PASTIS datamodule."""
 
+import warnings
 from collections.abc import Sequence
 from functools import partial
 from typing import Any
@@ -30,6 +31,8 @@ class PASTISDataModule(NonGeoDataModule):
         val_folds: Sequence[int] = (4,),
         test_folds: Sequence[int] = (5,),
         padding_length: int = 61,
+        val_split_pct: float | None = None,
+        test_split_pct: float | None = None,
         **kwargs: Any,
     ) -> None:
         """Initialize a new PASTISDataModule instance.
@@ -37,16 +40,50 @@ class PASTISDataModule(NonGeoDataModule):
         Args:
             batch_size: Size of each mini-batch.
             num_workers: Number of workers for parallel data loading.
+            val_split_pct: Percentage of the dataset to use as a validation set (Deprecated).
+            test_split_pct: Percentage of the dataset to use as a test set (Deprecated).
             train_folds: List of fold indices for training split.
             val_folds: List of fold indices for validation split.
             test_folds: List of fold indices for test split.
             padding_length: Padding length of the time series.
             **kwargs: Additional keyword arguments passed to
                 :class:`~torchgeo.datasets.PASTIS`.
+
+        .. versionadded:: 0.11
+            The *train_folds*, *val_folds*, and *test_folds* parameters.
         """
+
+        if val_split_pct is not None or test_split_pct is not None:
+            warnings.warn(
+                'The val_split_pct and test_split_pct parameters are deprecated and have no effect.'
+                'To follow the official PASTIS folds, use train_folds, val_folds, and test_folds instead. ',
+                DeprecationWarning,
+            )
+
         super().__init__(
             self._dataset_cls, batch_size=batch_size, num_workers=num_workers, **kwargs
         )
+
+        valid_folds = set(range(1, 6))
+
+        for fold, idx in zip(
+            ['train', 'val', 'test'], [train_folds, val_folds, test_folds]
+        ):
+            bad_folds = set(idx) - valid_folds
+            assert not bad_folds, (
+                f'{fold}_folds have out-of-range indices, got {bad_folds}'
+            )
+
+        assert not (set(train_folds) & set(val_folds)), (
+            f'train/val overlap: {set(train_folds) & set(val_folds)}'
+        )
+        assert not (set(train_folds) & set(test_folds)), (
+            f'train/test overlap: {set(train_folds) & set(test_folds)}'
+        )
+        assert not (set(val_folds) & set(test_folds)), (
+            f'val/test overlap: {set(val_folds) & set(test_folds)}'
+        )
+
         self.train_folds = train_folds
         self.val_folds = val_folds
         self.test_folds = test_folds
