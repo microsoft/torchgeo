a86ed9a6e7119730a5aa5fac8598775a11452b30
Adam J. Stewart <ajstewart426@gmail.com>
2026-09-07 12:23:31 +0200

    Fix docs
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- torchgeo/datasets/chesapeake.py
+++ torchgeo/datasets/chesapeake.py
@@ -678,7 +678,7 @@ class ChesapeakeCVPR(UnionDataset):
         """Plot a sample from the dataset.
 
         Args:
-            sample: a sample returned by :meth:`__getitem__`
+            sample: a sample returned by :meth:`RasterDataset.__getitem__`
             show_titles: flag indicating whether to show titles above each panel
             suptitle: optional string to use as a suptitle
 
