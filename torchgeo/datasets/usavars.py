d199a57819de90ae7a518d8a047bdeb7e1d2d1d9
NB <148885206+borchertn@users.noreply.github.com>
2026-09-08 12:30:50 +0200

    Fix ruff

--- torchgeo/datasets/usavars.py
+++ torchgeo/datasets/usavars.py
@@ -236,10 +236,7 @@ class USAVars(NonGeoDataset):
         extract_archive(os.path.join(self.root, self.dirname + '.zip'))
 
     def plot(
-        self, 
-        sample: Sample, 
-        show_titles: bool = True, 
-        suptitle: str | None = None,
+        self, sample: Sample, show_titles: bool = True, suptitle: str | None = None
     ) -> Figure:
         """Plot a sample from the dataset.
 
